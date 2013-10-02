582a3c917319a19b987f29d8329b3c2e55116b1d
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-10-02 14:04:53 +0100

    Restructuring files

--- apps/CloverLeaf/Serial/initialise.cpp
+++ apps/CloverLeaf/Serial/initialise.cpp
@@ -34,8 +34,10 @@
 #include "data.h"
 #include "definitions.h"
 
-//Cloverleaf kernels
-#include "initialise_chunk_kernel.h"
+
+
+void read_input();
+void start();
 
 void initialise()
 {
@@ -85,48 +87,10 @@ void initialise()
   ops_fprintf(g_out," Initialising and generating\n");
   ops_fprintf(g_out,"\n");
 
+  read_input();
 
-  //initialize sizes using global values
-  int x_cells = grid->x_cells;
-  int y_cells = grid->y_cells;
-  int x_min = field->x_min;
-  int x_max = field->x_max;
-  int y_min = field->y_min;
-  int y_max = field->y_max;
+  start();
 
-  int self[] = {0,0};
-  ops_stencil sten1 = ops_decl_stencil( 2, 1, self, "self");
-
-  int rangex[] = {x_min-2, x_max+3, 0, 1};
-  ops_par_loop(initialise_chunk_kernel_x, "initialise_chunk_kernel_x", 2, rangex,
-               ops_arg_dat(vertexx, S2D_00, OPS_WRITE),
-               ops_arg_dat(xx, S2D_00, OPS_READ),
-               ops_arg_dat(vertexdx, S2D_00, OPS_WRITE));
-
-  int rangey[] = {0, 1, y_min-2, y_max+3};
-  ops_par_loop(initialise_chunk_kernel_y, "initialise_chunk_kernel_y", 2, rangey,
-               ops_arg_dat(vertexy, S2D_00, OPS_WRITE),
-               ops_arg_dat(yy, S2D_00, OPS_READ),
-               ops_arg_dat(vertexdy, S2D_00, OPS_WRITE));
-
-  rangex[0] = x_min-2; rangex[1] = x_max+2; rangex[2] = 0; rangex[3] = 1;
-  ops_par_loop(initialise_chunk_kernel_cellx, "initialise_chunk_kernel_cellx", 2, rangex,
-               ops_arg_dat(vertexx, S2D_00_P10, OPS_READ),
-               ops_arg_dat(cellx, S2D_00, OPS_WRITE),
-               ops_arg_dat(celldx, S2D_00, OPS_WRITE));
-
-  rangey[0] = 0; rangey[1] = 1; rangey[2] = y_min-2; rangey[3] = y_max+2;
-  ops_par_loop(initialise_chunk_kernel_celly, "initialise_chunk_kernel_celly", 2, rangey,
-               ops_arg_dat(vertexy, S2D_00_0P1, OPS_READ),
-               ops_arg_dat(celly, S2D_00, OPS_WRITE),
-               ops_arg_dat(celldy, S2D_00, OPS_WRITE));
-
-  int rangexy[] = {x_min-2,x_max+2,y_min-2,y_max+2};
-  ops_par_loop(initialise_volume_xarea_yarea, "initialise_volume_xarea_yarea", 2, rangexy,
-    ops_arg_dat(volume, S2D_00, OPS_WRITE),
-    ops_arg_dat(celldy, sten_self_stride2D_y, OPS_READ),
-    ops_arg_dat(xarea, S2D_00, OPS_WRITE),
-    ops_arg_dat(celldx, sten_self_stride2D_x, OPS_READ),
-    ops_arg_dat(yarea, S2D_00, OPS_WRITE));
-
+  ops_fprintf(g_out," Starting the calculation\n");
+  fclose(g_in);
 }
--- apps/CloverLeaf/Serial/read_input.cpp
+++ apps/CloverLeaf/Serial/read_input.cpp
@@ -35,87 +35,214 @@
 
 void read_input()
 {
-  int state, stat, state_max, n;
-  int number_of_states;
-  double dx, dy;
-  int grid_xmin = 0.0, grid_ymin = 0.0, grid_xmax = 100.0, grid_ymax = 100.0;
-  int grid_x_cells = 10, grid_y_cells = 10;
-  double end_time = 10.0;
-  int end_step = g_ibig;
-  int complete = 0;
+  //some defailt values before read input
 
-  double dtinit = 0.1;
-  double dtmax = 1.0;
-  double dtmin = 0.0000001;
-  double dtrise = 1.5;
-  double dtc_safe = 0.7;
-  double dtu_safe = 0.5;
-  double dtv_safe = 0.5;
-  double dtdiv_safe = 0.7;
+  test_problem = 0;
+  state_max = 0;
+  number_of_states = 0;
 
-  ops_fprintf(g_out," Reading input file\n");
-  ops_fprintf(g_out,"\n");
+  grid = (grid_type ) xmalloc(sizeof(grid_type_core));
+  grid->xmin = 0;
+  grid->ymin = 0;
+  grid->xmax = 100;
+  grid->ymax = 100;
+
+  grid->x_cells = 10;
+  grid->y_cells = 10;
+
+  end_time = 10.0;
+  end_step = g_ibig;
+  complete = FALSE;
+
+  visit_frequency=10;
+  summary_frequency=10;
+
+  dtinit = 0.1;
+  dtmax = 1.0;
+  dtmin = 0.0000001;
+  dtrise = 1.5;
+  dtc_safe = 0.7;
+  dtu_safe = 0.5;
+  dtv_safe = 0.5;
+  dtdiv_safe = 0.7;
+
+  use_vector_loops = TRUE;
 
   //
-  //For now hard code the default problem
+  //need to read in the following through I/O .. hard coded below
   //
-  dtinit=0.04;
-  dtmax=dtinit;
-  dtrise=1.5;
-  grid_x_cells=10;
-  grid_y_cells=2;
-  grid_xmin=0.0;
-  grid_ymin=0.0;
-  grid_xmax=10.0;
-  grid_ymax=2.0;
-  end_time=3.0;
 
-  state_max = 2;
-  number_of_states = state_max;
+  ops_printf(" Reading input file\n");
 
-  if(number_of_states < 1)
-  {
-    ops_printf(" read_input , No states defined\n");
+  #define LINESZ 1024
+  char buff[LINESZ];
+  FILE *fin = fopen ("clover.in", "r");
+  if (fin != NULL) {
+      while (fgets (buff, LINESZ, fin)) {
+          char* token = strtok(buff, " =");
+          while (token) {
+            if(strcmp(token,"*clover\n") != 0 && strcmp(token,"*endclover\n") != 0 ) {
+              //printf("token: %s ", token);
+              if(strcmp(token,"initial_timestep") == 0) {
+                token = strtok(NULL, " =");
+                dtinit = atof(token);
+                ops_printf("initial_timestep: %lf\n", dtinit);
+              }
+              else if(strcmp(token,"max_timestep") == 0) {
+                token = strtok(NULL, " =");
+                dtmax = atof(token);
+                ops_printf("max_timestep: %lf\n", dtmax);
+              }
+              else if(strcmp(token,"timestep_rise") == 0) {
+                token = strtok(NULL, " =");
+                dtrise = atof(token);
+                ops_printf("timestep_rise: %lf\n", dtrise);
+              }
+              else if(strcmp(token,"end_time") == 0) {
+                token = strtok(NULL, " =");
+                end_time = atof(token);
+                ops_printf("end_time: %lf\n", end_time);
+              }
+              else if(strcmp(token,"end_step") == 0) {
+                token = strtok(NULL, " =");
+                end_step = atof(token);
+                ops_printf("end_step: %lf\n", end_step);
+              }
+              else if(strcmp(token,"xmin") == 0) {
+                token = strtok(NULL, " =");
+                grid->xmin = atof(token);
+                ops_printf("xmin: %lf\n", grid->xmin);
+              }
+              else if(strcmp(token,"xmax") == 0) {
+                token = strtok(NULL, " =");
+                grid->xmax = atof(token);
+                ops_printf("xmax: %lf\n", grid->xmax);
+              }
+              else if(strcmp(token,"ymin") == 0) {
+                token = strtok(NULL, " =");
+                grid->ymin = atof(token);
+                ops_printf("ymin: %lf\n", grid->ymin);
+              }
+              else if(strcmp(token,"ymax") == 0) {
+                token = strtok(NULL, " =");
+                grid->ymax = atof(token);
+                ops_printf("ymax: %lf\n", grid->ymax);
+              }
+              else if(strcmp(token,"x_cells") == 0) {
+                token = strtok(NULL, " =");
+                grid->x_cells = atof(token);
+                ops_printf("x_cells: %d\n", grid->x_cells);
+              }
+              else if(strcmp(token,"y_cells") == 0) {
+                token = strtok(NULL, " =");
+                grid->y_cells = atof(token);
+                ops_printf("y_cells: %d\n", grid->y_cells);
+              }
+              else if(strcmp(token,"visit_frequency") == 0) {
+                token = strtok(NULL, " =");
+                visit_frequency = atoi(token);
+                ops_printf("visit_frequency: %d\n", visit_frequency);
+              }
+              else if(strcmp(token,"summary_frequency") == 0) {
+                token = strtok(NULL, " =");
+                summary_frequency = atoi(token);
+                ops_printf("summary_frequency: %d\n", summary_frequency);
+              }
+              else if(strcmp(token,"test_problem") == 0) {
+                token = strtok(NULL, " =");
+                test_problem = atoi(token);
+                ops_printf("test_problem: %d\n", test_problem);
+              }
+              else if(strcmp(token,"state") == 0) {
+                token = strtok(NULL, " =");
+                states =  (state_type *) xrealloc(states, sizeof(state_type) * number_of_states+1);
+                states[number_of_states] = (state_type ) xmalloc(sizeof(state_type_core));
+
+                token = strtok(NULL, " =");
+                while(token) {
+                  if(strcmp(token,"xvel") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->xvel = atof(token);
+                  }
+                  if(strcmp(token,"yvel") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->yvel = atof(token);
+                  }
+
+                  if(strcmp(token,"xmin") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->xmin = atof(token);
+                  }
+                  if(strcmp(token,"xmax") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->xmax = atof(token);
+                  }
+                  if(strcmp(token,"ymin") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->ymin = atof(token);
+                  }
+                  if(strcmp(token,"ymax") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->ymax = atof(token);
+                  }
+                  if(strcmp(token,"density") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->density = atof(token);
+                  }
+                  if(strcmp(token,"energy") == 0) {
+                    token = strtok(NULL, " =");
+                    states[number_of_states]->energy = atof(token);
+                  }
+                  if(strcmp(token,"geometry") == 0) {
+                    token = strtok(NULL, " =");
+                    if(strcmp(token,"rectangle") == 0)
+                      states[number_of_states]->geometry = g_rect;
+                    else if(strcmp(token,"circle") == 0)
+                      states[number_of_states]->geometry = g_circ;
+                    else if(strcmp(token,"point") == 0)
+                      states[number_of_states]->geometry = g_point;
+                  }
+
+                  token = strtok(NULL, " =");
+                }
+
+                  /*ops_printf("state: %d density %lf energy %lf geometry %d xmin %lf xmax %lf ymin %lf ymax %lf\n", number_of_states,
+                  states[number_of_states]->density, states[number_of_states]->energy,
+                  states[number_of_states]->geometry,
+                  states[number_of_states]->xmin, states[number_of_states]->xmax,
+                  states[number_of_states]->ymin, states[number_of_states]->ymax);*/
+                number_of_states++;
+              }
+            }
+            token = strtok(NULL, " =");
+          }
+      }
+      fclose (fin);
+  }
+
+  if(number_of_states == 0) {
+    ops_printf("read_input, No states defined.\n");
     exit(-1);
   }
 
-  states = (state_type *)xmalloc(number_of_states * sizeof(state_type));
+  ops_printf(" Input read finished\n");
+
+  field = (field_type ) xmalloc(sizeof(field_type_core));
+  field->x_min = 0;
+  field->y_min = 0;
+  field->x_max = grid->x_cells;
+  field->y_max = grid->y_cells;
+  field->left = 0;
+  field->bottom = 0;
+
+  float dx= (grid->xmax-grid->xmin)/(float)(grid->x_cells);
+  float dy= (grid->ymax-grid->ymin)/(float)(grid->y_cells);
+
   for(int i = 0; i < number_of_states; i++)
   {
-    state_type s = ( state_type ) xmalloc ( sizeof ( state_type_core ) );
-    s->defined = FALSE;
-    s->energy = 0.0;
-    s->density = 0.0;
-    s->xvel = 0.0;
-    s->yvel = 0.0;
-    states[i] = s;
+    states[i]->xmin = states[i]->xmin + (dx/100.00);
+    states[i]->ymin = states[i]->ymin + (dy/100.00);
+    states[i]->xmax = states[i]->xmax - (dx/100.00);
+    states[i]->ymax = states[i]->ymax - (dy/100.00);
   }
-
-  states[0]->density=0.2;
-  states[0]->energy=1.0;
-
-  states[1]->density=1.0;
-  states[1]->energy=2.5;
-  states[1]->geometry=g_rect;
-  states[1]->xmin=0.0;
-  states[1]->xmax=5.0;
-  states[1]->ymin=0.0;
-  states[1]->ymax=2.0;
-
-  ops_fprintf(g_out,"\n");
-  ops_fprintf(g_out," Input read finished.\n");
-  ops_fprintf(g_out,"\n");
-
-
-  dx=(grid_xmax-grid_xmin)/grid_x_cells;
-  dy=(grid_ymax-grid_ymin)/grid_y_cells;
-  for(int i = 1; i < number_of_states; i++)
-  {
-    states[i]->xmin=states[i]->xmin+(dx/100.0);
-    states[i]->ymin=states[i]->ymin+(dy/100.0);
-    states[i]->xmax=states[i]->xmax-(dx/100.0);
-    states[i]->ymax=states[i]->ymax-(dy/100.0);
-  }
-
-
 }
