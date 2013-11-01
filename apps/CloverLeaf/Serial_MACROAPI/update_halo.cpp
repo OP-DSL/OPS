a0baf0f4b1bee61110ec1634d886f33bcb078eb0
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-11-01 16:28:25 +0000

    Some kernels in update_halo converted .. more to do

--- apps/CloverLeaf/Serial_MACROAPI/update_halo.cpp
+++ apps/CloverLeaf/Serial_MACROAPI/update_halo.cpp
@@ -30,12 +30,130 @@
 
 // OPS header file
 #include "ops_seq_opt.h"
+#include "ops_seq_macro.h"
 
 #include "data.h"
 #include "definitions.h"
 
 //Cloverleaf kernels
 #include  "update_halo_kernel.h"
+
+void update_halo_kernel1_b2_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,3)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,3)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,3)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,3)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,3)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,3)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,3)];
+
+}
+
+void update_halo_kernel_b1_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,1)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,1)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,1)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,1)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,1)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,1)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,1)];
+
+}
+
+void update_halo_kernel_t2_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,-3)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,-3)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,-3)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,-3)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,-3)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,-3)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,-3)];
+
+}
+
+void update_halo_kernel_t1_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,-1)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,-1)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,-1)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,-1)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,-1)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,-1)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,-1)];
+
+}
+
+//////////
+
+void update_halo_kernel1_l2_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(3,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(3,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(3,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(3,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(3,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(3,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(3,0)];
+
+}
+
+void update_halo_kernel1_l1_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(1,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(1,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(1,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(1,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(1,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(1,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(1,0)];
+
+}
+
+void update_halo_kernel1_r2_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(-3,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(-3,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(-3,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(-3,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(-3,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(-3,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(-3,0)];
+
+}
+
+void update_halo_kernel1_r1_macro(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(-1,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(-1,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(-1,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(-1,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(-1,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(-1,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(-1,0)];
+
+}
+
+
+
 
 void update_halo(int* fields, int depth)
 {
@@ -54,7 +172,7 @@ void update_halo(int* fields, int depth)
 
   int rangexy_b2a[] = {x_min-depth,x_max+depth,y_min-2,y_min-1};
   if(depth ==2)
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel1", 2, rangexy_b2a,
+  ops_par_loop_macro(update_halo_kernel1_b2_macro, "update_halo_kernel1_macro", 2, rangexy_b2a,
               ops_arg_dat(density0, S2D_00_0P3, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_0P3, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_0P3, "double", OPS_READ),
@@ -64,7 +182,7 @@ void update_halo(int* fields, int depth)
               ops_arg_dat(soundspeed, S2D_00_0P3, "double", OPS_READ));
 
   int rangexy_b1a[] = {x_min-depth,x_max+depth,y_min-1,y_min};
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel1", 2, rangexy_b1a,
+  ops_par_loop_macro(update_halo_kernel_b1_macro, "update_halo_kernel1_macro", 2, rangexy_b1a,
               ops_arg_dat(density0, S2D_00_0P1, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_0P1, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_0P1, "double", OPS_READ),
@@ -75,7 +193,7 @@ void update_halo(int* fields, int depth)
 
   int rangexy_t2a[] = {x_min-depth,x_max+depth,y_max+1,y_max+2};
   if(depth ==2)
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel1", 2, rangexy_t2a,
+  ops_par_loop_macro(update_halo_kernel_t2_macro, "update_halo_kernel1", 2, rangexy_t2a,
               ops_arg_dat(density0, S2D_00_0M3, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_0M3, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_0M3, "double", OPS_READ),
@@ -85,7 +203,7 @@ void update_halo(int* fields, int depth)
               ops_arg_dat(soundspeed, S2D_00_0M3, "double", OPS_READ));
 
   int rangexy_t1a[] = {x_min-depth,x_max+depth,y_max,y_max+1};
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel1", 2, rangexy_t1a,
+  ops_par_loop_macro(update_halo_kernel_t1_macro, "update_halo_kernel1", 2, rangexy_t1a,
               ops_arg_dat(density0, S2D_00_0M1, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_0M1, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_0M1, "double", OPS_READ),
@@ -96,7 +214,7 @@ void update_halo(int* fields, int depth)
 
   int rangexy_l2a[] = {x_min-2,x_min-1,y_min-depth,y_max+depth};
   if(depth ==2)
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel", 2, rangexy_l2a,
+  ops_par_loop_macro(update_halo_kernel1_l2_macro, "update_halo_kernel", 2, rangexy_l2a,
               ops_arg_dat(density0, S2D_00_P30, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_P30, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_P30, "double", OPS_READ),
@@ -106,7 +224,7 @@ void update_halo(int* fields, int depth)
               ops_arg_dat(soundspeed, S2D_00_P30, "double", OPS_READ));
 
   int rangexy_l1a[] = {x_min-1,x_min,y_min-depth,y_max+depth};
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel", 2, rangexy_l1a,
+  ops_par_loop_macro(update_halo_kernel1_l1_macro, "update_halo_kernel", 2, rangexy_l1a,
               ops_arg_dat(density0, S2D_00_P10, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_P10, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_P10, "double", OPS_READ),
@@ -118,7 +236,7 @@ void update_halo(int* fields, int depth)
   int rangexy_r2a[] = {x_max+1,x_max+2,y_min-depth,y_max+depth};
 
   if(depth ==2)
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel", 2, rangexy_r2a,
+  ops_par_loop_macro(update_halo_kernel1_r2_macro, "update_halo_kernel", 2, rangexy_r2a,
               ops_arg_dat(density0, S2D_00_M30, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_M30, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_M30, "double", OPS_READ),
@@ -128,7 +246,7 @@ void update_halo(int* fields, int depth)
               ops_arg_dat(soundspeed, S2D_00_M30, "double", OPS_READ));
 
   int rangexy_r1a[] = {x_max,x_max+1,y_min-depth,y_max+depth};
-  ops_par_loop_opt(update_halo_kernel1, "update_halo_kernel", 2, rangexy_r1a,
+  ops_par_loop_macro(update_halo_kernel1_r1_macro, "update_halo_kernel", 2, rangexy_r1a,
               ops_arg_dat(density0, S2D_00_M10, "double", OPS_READ),
               ops_arg_dat(density1, S2D_00_M10, "double", OPS_READ),
               ops_arg_dat(energy0, S2D_00_M10, "double", OPS_READ),
