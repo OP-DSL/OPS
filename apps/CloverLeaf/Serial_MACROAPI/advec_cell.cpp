dfc54dd544d4f9d80c5231c6395b16a4c2fcd13b
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-11-01 11:42:50 +0000

    All kernels in advec_cell converted to MACRO API

--- apps/CloverLeaf/Serial_MACROAPI/advec_cell.cpp
+++ apps/CloverLeaf/Serial_MACROAPI/advec_cell.cpp
@@ -52,6 +52,77 @@ void advec_cell_kernel2_xdir_macro( double *pre_vol, double *post_vol, double *v
 }
 
 
+void advec_cell_kernel3_xdir_macro( double *vol_flux_x, double *pre_vol, int *xx,
+                              double *vertexdx,
+                              double *density1, double *energy1 ,
+                              double *mass_flux_x, double *ener_flux) {
+
+  double sigma, sigmat, sigmav, sigmam, sigma3, sigma4;
+  double diffuw, diffdw, limiter;
+  double one_by_six = 1.0/6.0;
+
+  int x_max=field->x_max;
+
+  int upwind,donor,downwind,dif;
+
+  //pre_vol accessed with: {0,0, 1,0, -1,0, -2,0};
+  //vertexdx accessed with: {0,0, 1,0, -1,0, -2,0};
+  //density1, energy1 accessed with: {0,0, 1,0, -1,0, -2,0};
+  //xx accessed with: {0,0 ,1,0}
+
+  if(vol_flux_x[OPS_ACC0(0,0)] > 0.0) {
+    upwind   = -2; //j-2
+    donor    = -1; //j-1
+    downwind = 0; //j
+    dif      = donor;
+  }
+  else if (xx[OPS_ACC2(1,0)] < x_max+2) {
+    upwind   = 1; //j+1
+    donor    = 0; //j
+    downwind = -1; //j-1
+    dif      = upwind;
+  } else { //*xx[OPS_ACC2(1,0)] >= x_max+2 , then need 0
+    upwind   = 0; //xmax+2
+    donor    = 0; //j
+    downwind = -1; //j-1
+    dif      = upwind;
+  }
+  //return;
+
+  sigmat = fabs(vol_flux_x[OPS_ACC0(0,0)])/pre_vol[OPS_ACC1(donor,0)];
+  sigma3 = (1.0 + sigmat)*(vertexdx[OPS_ACC3(0,0)]/vertexdx[OPS_ACC3(dif,0)]);
+  sigma4 = 2.0 - sigmat;
+
+  sigma = sigmat;
+  sigmav = sigmat;
+
+  diffuw = density1[OPS_ACC4(donor,0)] - density1[OPS_ACC4(upwind,0)];
+  diffdw = density1[OPS_ACC4(downwind,0)] - density1[OPS_ACC4(donor,0)];
+
+  if( (diffuw*diffdw) > 0.0)
+    limiter=(1.0 - sigmav) * SIGN(1.0 , diffdw) *
+    MIN( MIN(fabs(diffuw), fabs(diffdw)),
+    one_by_six * (sigma3*fabs(diffuw) + sigma4 * fabs(diffdw)));
+  else
+    limiter=0.0;
+
+  mass_flux_x[OPS_ACC6(0,0)] = (vol_flux_x[OPS_ACC0(0,0)]) * ( density1[OPS_ACC4(donor,0)] + limiter );
+
+  sigmam = fabs(mass_flux_x[OPS_ACC6(0,0)])/( density1[OPS_ACC4(donor,0)] * pre_vol[OPS_ACC1(donor,0)]);
+  diffuw = energy1[OPS_ACC5(donor,0)] - energy1[OPS_ACC5(upwind,0)];
+  diffdw = energy1[OPS_ACC5(downwind,0)] - energy1[OPS_ACC5(donor,0)];
+
+  if( (diffuw*diffdw) > 0.0)
+    limiter = (1.0 - sigmam) * SIGN(1.0,diffdw) *
+    MIN( MIN(fabs(diffuw), fabs(diffdw)),
+    one_by_six * (sigma3 * fabs(diffuw) + sigma4 * fabs(diffdw)));
+  else
+    limiter=0.0;
+
+  ener_flux[OPS_ACC7(0,0)] = mass_flux_x[OPS_ACC6(0,0)] * ( energy1[OPS_ACC0(donor,0)] + limiter );
+}
+
+
 void advec_cell_kernel4_xdir_macro( double *density1, double *energy1,
                          double *mass_flux_x, double *vol_flux_x,
                          double *pre_vol, double *post_vol,
@@ -84,6 +155,77 @@ void advec_cell_kernel2_ydir_macro( double *pre_vol, double *post_vol, double *v
   pre_vol[OPS_ACC0(0,0)] = volume[OPS_ACC2(0,0)] + vol_flux_y[OPS_ACC3(0,1)] - vol_flux_y[OPS_ACC3(0,0)];
   post_vol[OPS_ACC1(0,0)] = volume[OPS_ACC2(0,0)];
 
+}
+
+
+void advec_cell_kernel3_ydir_macro( double *vol_flux_y, double *pre_vol, int *yy,
+                              double *vertexdy,
+                              double *density1, double *energy1 ,
+                              double *mass_flux_y, double *ener_flux) {
+
+  double sigma, sigmat, sigmav, sigmam, sigma3, sigma4;
+  double diffuw, diffdw, limiter;
+  double one_by_six = 1.0/6.0;
+
+  int y_max=field->y_max;
+
+  int upwind,donor,downwind,dif;
+
+  //pre_vol accessed with: {0,0, 0,1, 0,-1, 0,-2};
+  //vertexdy accessed with: {0,0, 0,1, 0,-1, 0,-2};
+  //density1, energy1 accessed with: {0,0, 0,1, 0,-1, 0,-2};
+  //yy accessed with: {0,0 ,0,1}
+
+  if(vol_flux_y[OPS_ACC0(0,0)] > 0.0) {
+    upwind   = -2; //k-2
+    donor    = -1; //k-1
+    downwind = 0; //k
+    dif      = donor;
+  }
+  else if (yy[OPS_ACC2(0,1)] < y_max+2) {
+    upwind   = 1; //j+1
+    donor    = 0; //j
+    downwind = -1; //j-1
+    dif      = upwind;
+  } else { //*yy[OPS_ACC2(0,1)] >= y_max+2 , then need 0
+    upwind   = 0; //ymax+2
+    donor    = 0; //k
+    downwind = -1; //k-1
+    dif      = upwind;
+  }
+  //return;
+
+  sigmat = fabs(vol_flux_y[OPS_ACC0(0,0)])/pre_vol[OPS_ACC1(0,donor)];
+  sigma3 = (1.0 + sigmat)*(vertexdy[OPS_ACC3(0,0)]/vertexdy[OPS_ACC3(0,dif)]);
+  sigma4 = 2.0 - sigmat;
+
+  sigma = sigmat;
+  sigmav = sigmat;
+
+  diffuw = density1[OPS_ACC4(0,donor)] - density1[OPS_ACC4(0,upwind)];
+  diffdw = density1[OPS_ACC4(0,downwind)] - density1[OPS_ACC4(0,donor)];
+
+  if( (diffuw*diffdw) > 0.0)
+    limiter=(1.0 - sigmav) * SIGN(1.0 , diffdw) *
+    MIN( MIN(fabs(diffuw), fabs(diffdw)),
+    one_by_six * (sigma3*fabs(diffuw) + sigma4 * fabs(diffdw)));
+  else
+    limiter=0.0;
+
+  mass_flux_y[OPS_ACC6(0,0)] = (vol_flux_y[OPS_ACC0(0,0)]) * ( density1[OPS_ACC4(0,donor)] + limiter );
+
+  sigmam = fabs(mass_flux_y[OPS_ACC6(0,0)])/( density1[OPS_ACC4(0,donor)] * pre_vol[OPS_ACC1(0,donor)]);
+  diffuw = energy1[OPS_ACC5(0,donor)] - energy1[OPS_ACC5(0,upwind)];
+  diffdw = energy1[OPS_ACC5(0,downwind)] - energy1[OPS_ACC5(0,donor)];
+
+  if( (diffuw*diffdw) > 0.0)
+    limiter = (1.0 - sigmam) * SIGN(1.0,diffdw) *
+    MIN( MIN(fabs(diffuw), fabs(diffdw)),
+    one_by_six * (sigma3 * fabs(diffuw) + sigma4 * fabs(diffdw)));
+  else
+    limiter=0.0;
+
+  ener_flux[OPS_ACC7(0,0)] = mass_flux_y[OPS_ACC6(0,0)] * ( energy1[OPS_ACC0(0,donor)] + limiter );
 }
 
 void advec_cell_kernel4_ydir_macro( double *density1, double *energy1,
@@ -141,7 +283,7 @@ void advec_cell(int sweep_number, int dir)
     }
 
 
-    ops_par_loop_opt(advec_cell_kernel3_xdir, "advec_cell_kernel3_xdir", 2, rangexy_inner_plus2x,
+    ops_par_loop_macro(advec_cell_kernel3_xdir_macro, "advec_cell_kernel3_xdir_macro", 2, rangexy_inner_plus2x,
       ops_arg_dat(vol_flux_x, S2D_00, "double", OPS_READ),
       ops_arg_dat(work_array1, S2D_00_P10_M10_M20, "double", OPS_READ),
       ops_arg_dat(xx, s2D_00_P10_STRID2D_X, "int", OPS_READ),
@@ -192,7 +334,7 @@ void advec_cell(int sweep_number, int dir)
 
   }
 
-    ops_par_loop_opt(advec_cell_kernel3_ydir, "advec_cell_kernel3_ydir", 2, rangexy_inner_plus2y,
+    ops_par_loop_macro(advec_cell_kernel3_ydir_macro, "advec_cell_kernel3_ydir_macro", 2, rangexy_inner_plus2y,
       ops_arg_dat(vol_flux_y, S2D_00, "double", OPS_READ),
       ops_arg_dat(work_array1, S2D_00_0P1_0M1_0M2, "double", OPS_READ),
       ops_arg_dat(yy, S2D_00_0P1_STRID2D_Y, "int", OPS_READ),
