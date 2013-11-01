7cbb0e939f852302d84d7b4c171b504e189adfb4
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-11-01 12:20:08 +0000

    Fixed bug that gave different results

--- apps/CloverLeaf/Serial_MACROAPI/advec_cell.cpp
+++ apps/CloverLeaf/Serial_MACROAPI/advec_cell.cpp
@@ -143,9 +143,9 @@ void advec_cell_kernel4_xdir_macro( double *density1, double *energy1,
 void advec_cell_kernel1_ydir_macro( double *pre_vol, double *post_vol, double *volume,
                         double *vol_flux_x, double *vol_flux_y) {
 
-  pre_vol[OPS_ACC0(0,0)] = volume[OPS_ACC2(0,0)] + ( vol_flux_y[OPS_ACC3(0,1)] - vol_flux_y[OPS_ACC3(0,0)] +
-                           vol_flux_x[OPS_ACC4(1,0)] - vol_flux_x[OPS_ACC4(1,0)]);
-  post_vol[OPS_ACC1(0,0)] = pre_vol[OPS_ACC0(0,0)] - ( vol_flux_y[OPS_ACC3(0,1)] - vol_flux_y[OPS_ACC3(0,0)]);
+  pre_vol[OPS_ACC0(0,0)] = volume[OPS_ACC2(0,0)] + ( vol_flux_y[OPS_ACC4(0,1)] - vol_flux_y[OPS_ACC4(0,0)] +
+                           vol_flux_x[OPS_ACC3(1,0)] - vol_flux_x[OPS_ACC3(0,0)]);
+  post_vol[OPS_ACC1(0,0)] = pre_vol[OPS_ACC0(0,0)] - ( vol_flux_y[OPS_ACC4(0,1)] - vol_flux_y[OPS_ACC4(0,0)]);
 
 }
 
