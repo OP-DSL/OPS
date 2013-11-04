9f3b5057a7cd9f620d165afd1c982a4ce4a0eb39
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-11-04 16:01:47 +0000

    converted update_halo to MACRO API in genseq

--- apps/CloverLeaf/Serial_Gen_MACROAPI/revert_kernel.h
+++ apps/CloverLeaf/Serial_Gen_MACROAPI/revert_kernel.h
@@ -1,10 +1,10 @@
 #ifndef REVERT_KERNEL_H
 #define REVERT_KERNEL_H
 
-void revert_kernel( double **density0, double **density1,
-                double **energy0, double **energy1) {
+void revert_kernel( double *density0, double *density1,
+                double *energy0, double *energy1) {
 
-  **density1 = **density0;
-  **energy1 = **energy0;
+  density1[OPS_ACC1(0,0)] = density0[OPS_ACC0(0,0)];
+  energy1[OPS_ACC3(0,0)] = energy0[OPS_ACC2(0,0)];
 }
 #endif
--- apps/CloverLeaf/Serial_Gen_MACROAPI/update_halo_kernel.h
+++ apps/CloverLeaf/Serial_Gen_MACROAPI/update_halo_kernel.h
@@ -3,61 +3,273 @@
 
 #include "data.h"
 
-
-void update_halo_kernel1(double **density0, double **density1,
-                          double **energy0, double **energy1,
-                          double **pressure, double **viscosity,
-                          double **soundspeed ) {
-  if(fields[FIELD_DENSITY0] == 1) *density0[0] = *density0[1];
-  if(fields[FIELD_DENSITY1] == 1) *density1[0] = *density1[1];
-  if(fields[FIELD_ENERGY0] == 1) *energy0[0] = *energy0[1];
-  if(fields[FIELD_ENERGY1] == 1) *energy1[0] = *energy1[1];
-  if(fields[FIELD_PRESSURE] == 1) *pressure[0] = *pressure[1];
-  if(fields[FIELD_VISCOSITY] == 1) *viscosity[0] = *viscosity[1];
-  if(fields[FIELD_SOUNDSPEED] == 1) *soundspeed[0] = *soundspeed[1];
+void update_halo_kernel1_b2(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,3)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,3)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,3)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,3)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,3)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,3)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,3)];
 
 }
 
+void update_halo_kernel1_b1(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,1)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,1)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,1)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,1)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,1)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,1)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,1)];
 
-void update_halo_kernel2_xvel_plus(double **xvel0, double **xvel1){
-  if(fields[FIELD_XVEL0] == 1) *xvel0[0] = *xvel0[1];
-  if(fields[FIELD_XVEL1] == 1) *xvel1[0] = *xvel1[1];
-}
-void update_halo_kernel2_xvel_minus(double **xvel0, double **xvel1){
-  if(fields[FIELD_XVEL0] == 1) *xvel0[0] = -(*xvel0[1]);
-  if(fields[FIELD_XVEL1] == 1) *xvel1[0] = -(*xvel1[1]);
-}
-
-
-void update_halo_kernel2_yvel_plus( double **yvel0, double **yvel1 ){
-  if(fields[FIELD_YVEL0] == 1) *yvel0[0] = *yvel0[1];
-  if(fields[FIELD_YVEL1] == 1) *yvel1[0] = *yvel1[1];
-}
-void update_halo_kernel2_yvel_minus( double **yvel0, double **yvel1 ){
-  if(fields[FIELD_YVEL0] == 1) *yvel0[0] = -(*yvel0[1]);
-  if(fields[FIELD_YVEL1] == 1) *yvel1[0] = -(*yvel1[1]);
 }
 
+void update_halo_kernel1_t2(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,-3)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,-3)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,-3)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,-3)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,-3)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,-3)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,-3)];
 
-
-void update_halo_kernel3_plus(double **vol_flux_x, double **mass_flux_x){
-  if(fields[FIELD_VOL_FLUX_X] == 1) *vol_flux_x[0] = *vol_flux_x[1];
-  if(fields[FIELD_MASS_FLUX_X] == 1) *mass_flux_x[0] = *mass_flux_x[1];
-}
-void update_halo_kernel3_minus(double **vol_flux_x, double **mass_flux_x){
-  if(fields[FIELD_VOL_FLUX_X] == 1) *vol_flux_x[0] = -(*vol_flux_x[1]);
-  if(fields[FIELD_MASS_FLUX_X] == 1) *mass_flux_x[0] = -(*mass_flux_x[1]);
-}
-
-
-void update_halo_kernel4_plus(double **vol_flux_y, double **mass_flux_y){
-  if(fields[FIELD_VOL_FLUX_Y] == 1) *vol_flux_y[0] = *vol_flux_y[1];
-  if(fields[FIELD_MASS_FLUX_Y] == 1) *mass_flux_y[0] = *mass_flux_y[1];
-}
-void update_halo_kernel4_minus(double **vol_flux_y, double **mass_flux_y){
-  if(fields[FIELD_VOL_FLUX_Y] == 1) *vol_flux_y[0] = -(*vol_flux_y[1]);
-  if(fields[FIELD_MASS_FLUX_Y] == 1) *mass_flux_y[0] = -(*mass_flux_y[1]);
 }
 
+void update_halo_kernel1_t1(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(0,-1)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(0,-1)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(0,-1)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(0,-1)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(0,-1)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(0,-1)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(0,-1)];
 
+}
+
+//////////
+
+void update_halo_kernel1_l2(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(3,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(3,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(3,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(3,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(3,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(3,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(3,0)];
+
+}
+
+void update_halo_kernel1_l1(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(1,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(1,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(1,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(1,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(1,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(1,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(1,0)];
+
+}
+
+void update_halo_kernel1_r2(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(-3,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(-3,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(-3,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(-3,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(-3,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(-3,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(-3,0)];
+
+}
+
+void update_halo_kernel1_r1(double *density0, double *density1,
+                          double *energy0, double *energy1,
+                          double *pressure, double *viscosity,
+                          double *soundspeed ) {
+  if(fields[FIELD_DENSITY0] == 1) density0[OPS_ACC0(0,0)] = density0[OPS_ACC0(-1,0)];
+  if(fields[FIELD_DENSITY1] == 1) density1[OPS_ACC1(0,0)] = density1[OPS_ACC0(-1,0)];
+  if(fields[FIELD_ENERGY0] == 1) energy0[OPS_ACC2(0,0)] = energy0[OPS_ACC0(-1,0)];
+  if(fields[FIELD_ENERGY1] == 1) energy1[OPS_ACC3(0,0)] = energy1[OPS_ACC0(-1,0)];
+  if(fields[FIELD_PRESSURE] == 1) pressure[OPS_ACC4(0,0)] = pressure[OPS_ACC0(-1,0)];
+  if(fields[FIELD_VISCOSITY] == 1) viscosity[OPS_ACC5(0,0)] = viscosity[OPS_ACC0(-1,0)];
+  if(fields[FIELD_SOUNDSPEED] == 1) soundspeed[OPS_ACC6(0,0)] = soundspeed[OPS_ACC0(-1,0)];
+
+}
+////
+
+void update_halo_kernel2_xvel_plus_4_a(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = xvel0[OPS_ACC0(0,4)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = xvel1[OPS_ACC1(0,4)];
+}
+void update_halo_kernel2_xvel_plus_2_a(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = xvel0[OPS_ACC0(0,2)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = xvel1[OPS_ACC1(0,2)];
+}
+
+void update_halo_kernel2_xvel_plus_4_b(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = xvel0[OPS_ACC0(0,-4)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = xvel1[OPS_ACC1(0,-4)];
+}
+void update_halo_kernel2_xvel_plus_2_b(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = xvel0[OPS_ACC0(0,-2)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = xvel1[OPS_ACC1(0,-2)];
+}
+
+///
+
+void update_halo_kernel2_xvel_minus_4_a(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = -xvel0[OPS_ACC0(4,0)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = -xvel1[OPS_ACC1(4,0)];
+}
+void update_halo_kernel2_xvel_minus_2_a(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = -xvel0[OPS_ACC0(2,0)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = -xvel1[OPS_ACC1(2,0)];
+}
+
+void update_halo_kernel2_xvel_minus_4_b(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = -xvel0[OPS_ACC0(-4,0)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = -xvel1[OPS_ACC1(-4,0)];
+}
+void update_halo_kernel2_xvel_minus_2_b(double *xvel0, double *xvel1){
+  if(fields[FIELD_XVEL0] == 1) xvel0[OPS_ACC0(0,0)] = -xvel0[OPS_ACC0(-2,0)];
+  if(fields[FIELD_XVEL1] == 1) xvel1[OPS_ACC1(0,0)] = -xvel1[OPS_ACC1(-2,0)];
+}
+
+
+///
+
+void update_halo_kernel2_yvel_plus_4_a(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = yvel0[OPS_ACC0(4,0)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = yvel1[OPS_ACC1(4,0)];
+}
+void update_halo_kernel2_yvel_plus_2_a(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = yvel0[OPS_ACC0(2,0)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = yvel1[OPS_ACC1(2,0)];
+}
+
+void update_halo_kernel2_yvel_plus_4_b(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = yvel0[OPS_ACC0(-4,0)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = yvel1[OPS_ACC1(-4,0)];
+}
+void update_halo_kernel2_yvel_plus_2_b(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = yvel0[OPS_ACC0(-2,0)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = yvel1[OPS_ACC1(-2,0)];
+}
+
+///
+
+void update_halo_kernel2_yvel_minus_4_a(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = -yvel0[OPS_ACC0(0,4)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = -yvel1[OPS_ACC1(0,4)];
+}
+void update_halo_kernel2_yvel_minus_2_a(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = -yvel0[OPS_ACC0(0,2)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = -yvel1[OPS_ACC1(0,2)];
+}
+
+void update_halo_kernel2_yvel_minus_4_b(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = -yvel0[OPS_ACC0(0,-4)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = -yvel1[OPS_ACC1(0,-4)];
+}
+void update_halo_kernel2_yvel_minus_2_b(double *yvel0, double *yvel1){
+  if(fields[FIELD_XVEL0] == 1) yvel0[OPS_ACC0(0,0)] = -yvel0[OPS_ACC0(0,-2)];
+  if(fields[FIELD_XVEL1] == 1) yvel1[OPS_ACC1(0,0)] = -yvel1[OPS_ACC1(0,-2)];
+}
+
+
+///
+
+void update_halo_kernel3_plus_4_a(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = vol_flux_x[OPS_ACC0(0,4)];
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = mass_flux_x[OPS_ACC1(0,4)];
+}
+void update_halo_kernel3_plus_2_a(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = vol_flux_x[OPS_ACC0(0,2)];
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = mass_flux_x[OPS_ACC1(0,2)];
+}
+
+void update_halo_kernel3_plus_4_b(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = vol_flux_x[OPS_ACC0(0,-4)];
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = mass_flux_x[OPS_ACC1(0,-4)];
+}
+void update_halo_kernel3_plus_2_b(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = vol_flux_x[OPS_ACC0(0,-2)];
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = mass_flux_x[OPS_ACC1(0,-2)];
+}
+
+void update_halo_kernel3_minus_4_a(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = -(vol_flux_x[OPS_ACC0(4,0)]);
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = -(mass_flux_x[OPS_ACC1(4,0)]);
+}
+void update_halo_kernel3_minus_2_a(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = -(vol_flux_x[OPS_ACC0(2,0)]);
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = -(mass_flux_x[OPS_ACC1(2,0)]);
+}
+void update_halo_kernel3_minus_4_b(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = -(vol_flux_x[OPS_ACC0(-4,0)]);
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = -(mass_flux_x[OPS_ACC1(-4,0)]);
+}
+void update_halo_kernel3_minus_2_b(double *vol_flux_x, double *mass_flux_x){
+  if(fields[FIELD_VOL_FLUX_X] == 1)  vol_flux_x[OPS_ACC0(0,0)]  = -(vol_flux_x[OPS_ACC0(-2,0)]);
+  if(fields[FIELD_MASS_FLUX_X] == 1) mass_flux_x[OPS_ACC1(0,0)] = -(mass_flux_x[OPS_ACC1(-2,0)]);
+}
+
+
+///
+
+
+void update_halo_kernel4_plus_4_a(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = vol_flux_y[OPS_ACC0(4,0)];
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = mass_flux_y[OPS_ACC1(4,0)];
+}
+void update_halo_kernel4_plus_2_a(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = vol_flux_y[OPS_ACC0(2,0)];
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = mass_flux_y[OPS_ACC1(2,0)];
+}
+void update_halo_kernel4_plus_4_b(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = vol_flux_y[OPS_ACC0(-4,0)];
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = mass_flux_y[OPS_ACC1(-4,0)];
+}
+void update_halo_kernel4_plus_2_b(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = vol_flux_y[OPS_ACC0(-2,0)];
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = mass_flux_y[OPS_ACC1(-2,0)];
+}
+
+void update_halo_kernel4_minus_4_a(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = -(vol_flux_y[OPS_ACC0(0,4)]);
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = -(mass_flux_y[OPS_ACC1(0,4)]);
+}
+void update_halo_kernel4_minus_2_a(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = -(vol_flux_y[OPS_ACC0(0,2)]);
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = -(mass_flux_y[OPS_ACC1(0,2)]);
+}
+void update_halo_kernel4_minus_4_b(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = -(vol_flux_y[OPS_ACC0(0,-4)]);
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = -(mass_flux_y[OPS_ACC1(0,-4)]);
+}
+void update_halo_kernel4_minus_2_b(double *vol_flux_y, double *mass_flux_y){
+  if(fields[FIELD_VOL_FLUX_Y] == 1) vol_flux_y[OPS_ACC0(0,0)] = -(vol_flux_y[OPS_ACC0(0,-2)]);
+  if(fields[FIELD_MASS_FLUX_Y] == 1) mass_flux_y[OPS_ACC1(0,0)] = -(mass_flux_y[OPS_ACC1(0,-2)]);
+}
 #endif
