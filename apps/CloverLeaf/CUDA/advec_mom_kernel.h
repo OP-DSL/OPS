2d559a7740e8efd7c222501e8fbe53f75e74a67f
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-11-26 14:38:33 +0000

    Removing extra argument from one of the advec_mom kernels

--- apps/CloverLeaf/CUDA/advec_mom_kernel.h
+++ apps/CloverLeaf/CUDA/advec_mom_kernel.h
@@ -145,7 +145,7 @@ inline void advec_mom_kernel1_x( const double *node_flux, const double *node_mas
 
 __device__
 inline void advec_mom_kernel1_x_nonvector( const double *node_flux, const double *node_mass_pre,
-                        double *advec_vel, double *mom_flux,
+                        double *mom_flux,
                         const double *celldx, const double *vel1) {
 
   //node_flux accessed with: {0,0}
@@ -174,9 +174,9 @@ inline void advec_mom_kernel1_x_nonvector( const double *node_flux, const double
 
   sigma = fabs(node_flux[OPS_ACC0(0,0)])/node_mass_pre[OPS_ACC1(donor,0)];
 
-  width = celldx[OPS_ACC4(0,0)];
-  vdiffuw = vel1[OPS_ACC5(donor,0)] - vel1[OPS_ACC5(upwind,0)];
-  vdiffdw = vel1[OPS_ACC5(downwind,0)] - vel1[OPS_ACC5(donor,0)];
+  width = celldx[OPS_ACC3(0,0)];
+  vdiffuw = vel1[OPS_ACC4(donor,0)] - vel1[OPS_ACC4(upwind,0)];
+  vdiffdw = vel1[OPS_ACC4(downwind,0)] - vel1[OPS_ACC4(donor,0)];
   limiter=0.0;
 
 
@@ -185,11 +185,11 @@ inline void advec_mom_kernel1_x_nonvector( const double *node_flux, const double
     adw = fabs(vdiffdw);
     wind = 1.0;
     if(vdiffdw <= 0.0) wind = -1.0;
-    limiter=wind*MIN(width*((2.0-sigma)*adw/width+(1.0+sigma)*auw/celldx[OPS_ACC4(dif,0)])/6.0, MIN(auw, adw));
+    limiter=wind*MIN(width*((2.0-sigma)*adw/width+(1.0+sigma)*auw/celldx[OPS_ACC3(dif,0)])/6.0, MIN(auw, adw));
   }
 
-  advec_vel_temp = vel1[OPS_ACC5(donor,0)] + (1.0 - sigma) * limiter;
-  mom_flux[OPS_ACC3(0,0)] = advec_vel_temp * node_flux[OPS_ACC0(0,0)];
+  advec_vel_temp = vel1[OPS_ACC4(donor,0)] + (1.0 - sigma) * limiter;
+  mom_flux[OPS_ACC2(0,0)] = advec_vel_temp * node_flux[OPS_ACC0(0,0)];
 
 }
 
@@ -249,8 +249,8 @@ inline void advec_mom_kernel1_y( const double *node_flux, const double *node_mas
 
 __device__
 inline void advec_mom_kernel1_y_nonvector( const double *node_flux, const double *node_mass_pre,
-                        double *advec_vel, double *mom_flux,
-                        const double *celldy, const double *vel1) {
+                       double *mom_flux,
+                       const double *celldy, const double *vel1) {
 
   //node_flux accessed with: {0,0}
   //node_mass_pre accessed with: {0,0, 0,1}
@@ -275,19 +275,19 @@ inline void advec_mom_kernel1_y_nonvector( const double *node_flux, const double
   }
 
   sigma = fabs(node_flux[OPS_ACC0(0,0)])/node_mass_pre[OPS_ACC1(0,donor)];
-  width = celldy[OPS_ACC4(0,0)];
-  vdiffuw = vel1[OPS_ACC5(0,donor)] - vel1[OPS_ACC5(0,upwind)];
-  vdiffdw = vel1[OPS_ACC5(0,downwind)] - vel1[OPS_ACC5(0,donor)];
+  width = celldy[OPS_ACC3(0,0)];
+  vdiffuw = vel1[OPS_ACC4(0,donor)] - vel1[OPS_ACC4(0,upwind)];
+  vdiffdw = vel1[OPS_ACC4(0,downwind)] - vel1[OPS_ACC4(0,donor)];
   limiter = 0.0;
   if(vdiffuw*vdiffdw > 0.0) {
     auw = fabs(vdiffuw);
     adw = fabs(vdiffdw);
     wind = 1.0;
     if(vdiffdw <= 0.0) wind = -1.0;
-    limiter=wind*MIN(width*((2.0-sigma)*adw/width+(1.0+sigma)*auw/celldy[OPS_ACC4(0,dif)])/6.0,MIN(auw,adw));
+    limiter=wind*MIN(width*((2.0-sigma)*adw/width+(1.0+sigma)*auw/celldy[OPS_ACC3(0,dif)])/6.0,MIN(auw,adw));
   }
-  advec_vel_temp= vel1[OPS_ACC5(0,donor)] + (1.0 - sigma) * limiter;
-  mom_flux[OPS_ACC3(0,0)] = advec_vel_temp * node_flux[OPS_ACC0(0,0)];
+  advec_vel_temp= vel1[OPS_ACC4(0,donor)] + (1.0 - sigma) * limiter;
+  mom_flux[OPS_ACC2(0,0)] = advec_vel_temp * node_flux[OPS_ACC0(0,0)];
 }
 
 
