570889972dc37b3b262c0a9f538b85e6f3cb63c2
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2013-11-29 14:54:29 +0000

    Setting up MPI version of Clover leaf

---
+++ apps/CloverLeaf/MPI/build_field.cpp
@@ -0,0 +1,233 @@
+/* Crown Copyright 2012 AWE.
+
+ This file is part of CloverLeaf.
+
+ CloverLeaf is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or (at your option)
+ any later version.
+
+ CloverLeaf is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License along with
+ CloverLeaf. If not, see http://www.gnu.org/licenses/. */
+
+/** @brief Allocates the data for each mesh chunk
+ *  @author Wayne Gaudin, converted to OPS by Gihan Mudalige
+ *  @details The data fields for the mesh chunk are allocated based on the mesh size
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// OPS header file
+#include "ops_seq.h"
+
+
+#include "data.h"
+#include "definitions.h"
+
+void build_field()
+{
+  //initialize sizes using global values
+  int x_cells = grid->x_cells;
+  int y_cells = grid->y_cells;
+  int x_min = field->x_min;
+  int x_max = field->x_max;
+  int y_min = field->y_min;
+  int y_max = field->y_max;
+
+  printf("x_min = %d, y_min = %d\n",x_min,y_min);
+  printf("x_max = %d, y_max = %d\n",x_max,y_max);
+
+  /**----------------------------OPS Declarations----------------------------**/
+
+  int dims[2] = {x_cells, y_cells};  //cloverleaf 2D block dimensions
+  ops_block clover_grid = ops_decl_block(2, dims, "grid");
+
+  //declare edges of block
+  dims[0] = x_cells; dims[1] = 1;
+  clover_xedge = ops_decl_block(2, dims, "xedge");
+
+  dims[0] = 1; dims[1] = y_cells;
+  clover_yedge = ops_decl_block(2, dims, "yedge");
+
+  //
+  //declare data on blocks
+  //
+  int offset[2] = {-2,-2};
+  int size[2] = {(x_max+2)-(x_min-2), (y_max+2)-(y_min-2)};
+  double* temp = NULL;
+
+  density0    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "density0");
+  density1    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "density1");
+  energy0     = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "energy0");
+  energy1     = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "energy1");
+  pressure    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "pressure");
+  viscosity   = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "viscosity");
+  soundspeed  = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "soundspeed");
+  volume      = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "volume");
+
+  size[0] = (x_max+3)-(x_min-2); size[1] = (y_max+3)-(y_min-2);
+  xvel0    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "xvel0");
+  xvel1    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "xvel1");
+  yvel0    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "yvel0");
+  yvel1    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "yvel1");
+
+  size[0] = (x_max+3)-(x_min-2); size[1] = (y_max+2)-(y_min-2);
+  vol_flux_x  = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "vol_flux_x");
+  mass_flux_x = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "mass_flux_x");
+  xarea       = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "xarea");
+
+  size[0] = (x_max+2)-(x_min-2); size[1] = (y_max+3)-(y_min-2);
+  vol_flux_y  = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "vol_flux_y");
+  mass_flux_y = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "mass_flux_y");
+  yarea       = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "yarea");
+
+  size[0] = (x_max+3)-(x_min-2); size[1] = (y_max+3)-(y_min-2);
+  work_array1    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array1");
+  work_array2    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array2");
+  work_array3    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array3");
+  work_array4    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array4");
+  work_array5    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array5");
+  work_array6    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array6");
+  work_array7    = ops_decl_dat(clover_grid, 1, size, offset, temp, "double", "work_array7");
+
+  int size2[2] = {(x_max+2)-(x_min-2),1};
+  int size3[2] = {1, (y_max+2)-(y_min-2)};
+  int size4[2] = {(x_max+3)-(x_min-2),1};
+  int size5[2] = {1,(y_max+3)-(y_min-2)};
+  int offsetx[2] = {-2,0};
+  int offsety[2] = {0,-2};
+  cellx    = ops_decl_dat(clover_xedge, 1, size2, offsetx, temp, "double", "cellx");
+  celly    = ops_decl_dat(clover_yedge, 1, size3, offsety, temp, "double", "celly");
+  vertexx  = ops_decl_dat(clover_xedge, 1, size4, offsetx, temp, "double", "vertexx");
+  vertexy  = ops_decl_dat(clover_yedge, 1, size5, offsety, temp, "double", "vertexy");
+  celldx   = ops_decl_dat(clover_xedge, 1, size2, offsetx, temp, "double", "celldx");
+  celldy   = ops_decl_dat(clover_yedge, 1, size3, offsety, temp, "double", "celldy");
+  vertexdx = ops_decl_dat(clover_xedge, 1, size4, offsetx, temp, "double", "vertexdx");
+  vertexdy = ops_decl_dat(clover_yedge, 1, size5, offsety, temp, "double", "vertexdy");
+
+  //contains x indicies from 0 to xmax+3 -- needed for initialization
+  int* xindex = (int *)xmalloc(sizeof(int)*size4[0]);
+  for(int i=x_min-2; i<x_max+3; i++) xindex[i-offsetx[0]] = i - x_min;
+  xx  = ops_decl_dat(clover_xedge, 1, size4, offsetx, xindex, "int", "xx");
+
+  //contains y indicies from 0 to ymax+3 -- needed for initialization
+  int* yindex = (int *)xmalloc(sizeof(int)*size5[1]);
+  for(int i=y_min-2; i<y_max+3; i++) yindex[i-offsety[1]] = i - y_min;
+  yy  = ops_decl_dat(clover_yedge, 1, size5, offsety, yindex, "int", "yy");
+
+  //
+  //Declare commonly used stencils
+  //
+  int s2D_00[]            = {0,0};
+  int s2D_00_P10[]     = {0,0, 1,0};
+  int s2D_00_0P1[]     = {0,0, 0,1};
+  int s2D_00_M10[]     = {0,0, -1,0};
+  int s2D_00_0M1[]     = {0,0, 0,-1};
+  int s2D_00_P10_M10[] = {0,0, 1,0, -1,0};
+  int s2D_00_0P1_0M1[] = {0,0, 0,1, 0,-1};
+  int s2D_00_M10_M20[] = {0,0, -1,0, -2,0};
+  int s2D_00_0M1_0M2[] = {0,0, 0,-1, 0,-2};
+  int s2D_00_P20[]     = {0,0, 2,0};
+  int s2D_00_0P2[]     = {0,0, 0,2};
+  int s2D_00_M20[]     = {0,0, -2,0};
+  int s2D_00_0M2[]     = {0,0, 0,-2};
+  int s2D_00_P30[]     = {0,0, 3,0};
+  int s2D_00_0P3[]     = {0,0, 0,3};
+  int s2D_00_M30[]     = {0,0, -3,0};
+  int s2D_00_0M3[]     = {0,0, 0,-3};
+  int s2D_00_P40[]     = {0,0, 4,0};
+  int s2D_00_0P4[]     = {0,0, 0,4};
+  int s2D_00_M40[]     = {0,0, -4,0};
+  int s2D_00_0M4[]     = {0,0, 0,-4};
+
+  int s2D_00_P10_0P1_P1P1[]  = {0,0, 1,0, 0,1, 1,1};
+  int s2D_00_M10_0M1_M1M1[]  = {0,0, -1,0, 0,-1, -1,-1};
+  int s2D_00_P10_0M1_P1M1[] = {0,0, 1,0, 0,-1, 1,-1};
+  int s2D_00_0P1_M10_M1P1[] = {0,0, 0,1, -1,0, -1,1};
+
+  int s2D_10_M10_01_0M1[]  = {1,0, -1,0, 0,1, 0,-1};
+
+  int s2D_00_P10_M10_M20[] = {0,0, 1,0, -1,0, -2,0};
+  int s2D_00_0P1_0M1_0M2[] = {0,0, 0,1, 0,-1, 0,-2};
+  int s2D_00_P10_P20_M10[] = {0,0, 1,0, 2,0, -1,0};
+  int s2D_00_0P1_0P2_0M1[] = {0,0, 0,1, 0,2, 0,-1};
+
+
+  int stride2D_x[] = {1,0};
+  int stride2D_y[] = {0,1};
+
+  int stride2D_null[] = {0,0};
+
+  int xmax2D[] = {x_max+2,0};
+  int ymax2D[] = {0,y_max+2};
+
+  S2D_00         = ops_decl_stencil( 2, 1, s2D_00, "00");
+
+  S2D_00_P10     = ops_decl_stencil( 2, 2, s2D_00_P10, "0,0:1,0");
+  S2D_00_0P1     = ops_decl_stencil( 2, 2, s2D_00_0P1, "0,0:0,1");
+  S2D_00_M10     = ops_decl_stencil( 2, 2, s2D_00_M10, "0,0:-1,0");
+  S2D_00_0M1     = ops_decl_stencil( 2, 2, s2D_00_0M1, "0,0:0,-1");
+
+  S2D_00_P10_M10 = ops_decl_stencil( 2, 3, s2D_00_P10_M10, "0,0:1,0:1,0");
+  S2D_00_0P1_0M1 = ops_decl_stencil( 2, 3, s2D_00_0P1_0M1, "0,0:0,1:0,-1");
+
+  S2D_00_M10_M20 = ops_decl_stencil( 2, 3, s2D_00_M10_M20, "0,0:-1,0:-2,0");
+  S2D_00_0M1_0M2 = ops_decl_stencil( 2, 3, s2D_00_0M1_0M2, "0,0:0,-1:0,-2");
+
+  S2D_00_P20     = ops_decl_stencil( 2, 2, s2D_00_P20, "0,0:2,0");
+  S2D_00_0P2     = ops_decl_stencil( 2, 2, s2D_00_0P2, "0,0:0,2");
+
+  S2D_00_M20     = ops_decl_stencil( 2, 2, s2D_00_M20, "0,0:-2,0");
+  S2D_00_0M2     = ops_decl_stencil( 2, 2, s2D_00_0M2, "0,0:0,-2");
+
+  S2D_00_P30     = ops_decl_stencil( 2, 2, s2D_00_P30, "0,0:3,0");
+  S2D_00_0P3     = ops_decl_stencil( 2, 2, s2D_00_0P3, "0,0:0,3");
+
+  S2D_00_M30     = ops_decl_stencil( 2, 2, s2D_00_M30, "0,0:-3,0");
+  S2D_00_0M3     = ops_decl_stencil( 2, 2, s2D_00_0M3, "0,0:0,-3");
+
+  S2D_00_P40     = ops_decl_stencil( 2, 2, s2D_00_P40, "0,0:4,0");
+  S2D_00_0P4     = ops_decl_stencil( 2, 2, s2D_00_0P4, "0,0,0,-4");
+
+  S2D_00_M40     = ops_decl_stencil( 2, 2, s2D_00_M40, "0,0:-4,0");
+  S2D_00_0M4     = ops_decl_stencil( 2, 2, s2D_00_0M4, "0,0:0,-4");
+
+  S2D_00_P10_0P1_P1P1 = ops_decl_stencil( 2, 4, s2D_00_P10_0P1_P1P1, "0,0:1,0:0,1:1,1");
+  S2D_00_M10_0M1_M1M1 = ops_decl_stencil( 2, 4, s2D_00_M10_0M1_M1M1, "0,0:-1,0:0,-1:-1,-1");
+  S2D_00_P10_0M1_P1M1= ops_decl_stencil( 2, 4, s2D_00_P10_0M1_P1M1, "0,0:1,0:0,-1:1,-1");
+  S2D_00_0P1_M10_M1P1= ops_decl_stencil( 2, 4, s2D_00_0P1_M10_M1P1, "0,0:0,1:-1,0:-1,1");
+
+  S2D_10_M10_01_0M1 = ops_decl_stencil( 2, 4, s2D_10_M10_01_0M1, "1,0:-1,0:0,1:0,-1");
+
+  S2D_00_P10_M10_M20 = ops_decl_stencil( 2, 4, s2D_00_P10_M10_M20, "0,0:1,0:-1,0:-2,0");
+  S2D_00_P10_M10_M20_STRID2D_X = ops_decl_strided_stencil( 2, 4, s2D_00_P10_M10_M20, stride2D_x, "self_stride2D_x");
+
+  S2D_00_0P1_0M1_0M2 = ops_decl_stencil( 2, 4, s2D_00_0P1_0M1_0M2, "0,0:0,1:0-1:0,-2");
+  S2D_00_0P1_0M1_0M2_STRID2D_Y = ops_decl_strided_stencil( 2, 4, s2D_00_0P1_0M1_0M2, stride2D_y, "self_stride2D_y");
+
+  S2D_00_P10_P20_M10 = ops_decl_stencil( 2, 4, s2D_00_P10_P20_M10, "0,0:1,0:2,0:-1,0}");
+  S2D_00_0P1_0P2_0M1 = ops_decl_stencil( 2, 4, s2D_00_0P1_0P2_0M1, "0,0,:0,1,:0,2,:0,-1");
+
+
+  S2D_00_STRID2D_X = ops_decl_strided_stencil( 2, 1, s2D_00, stride2D_x, "s2D_00_stride2D_x");
+  S2D_00_STRID2D_Y = ops_decl_strided_stencil( 2, 1, s2D_00, stride2D_y, "s2D_00_stride2D_y");
+
+  s2D_00_P10_STRID2D_X = ops_decl_strided_stencil( 2, 2, s2D_00_P10, stride2D_x, "s2D_00_P10_stride2D_x");
+  S2D_00_0P1_STRID2D_Y = ops_decl_strided_stencil( 2, 2, s2D_00_0P1, stride2D_y, "s2D_00_0P1_stride2D_y");
+
+  S2D_00_M10_STRID2D_X = ops_decl_strided_stencil( 2, 2, s2D_00_M10, stride2D_x, "s2D_00_M10_stride2D_x");
+  S2D_00_0M1_STRID2D_Y = ops_decl_strided_stencil( 2, 2, s2D_00_0M1, stride2D_y, "s2D_00_0M1_stride2D_y");
+
+
+  //print ops blocks and dats details
+  ops_diagnostic_output();
+
+}
---
+++ ops/src/mpi/ops_mpi_decl.c
@@ -0,0 +1,97 @@
+/*
+* Open source copyright declaration based on BSD open source template:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* This file is part of the OPS distribution.
+*
+* Copyright (c) 2013, Mike Giles and others. Please see the AUTHORS file in
+* the main source directory for a full list of copyright holders.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* * The name of Mike Giles may not be used to endorse or promote products
+* derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY Mike Giles ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Mike Giles BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/** @brief ops mpi declaration
+  * @author Gihan Mudalige
+  * @details Implements the OPS API calls for the mpi backend
+  */
+
+#include <ops_lib_cpp.h>
+
+
+void
+ops_init ( int argc, char ** argv, int diags )
+{
+  ops_init_core ( argc, argv, diags );
+}
+
+
+ops_dat ops_decl_dat_char (ops_block block, int size, int *block_size,
+                           int* offset,  char* data, int type_size,
+                           char const * type, char const * name )
+{
+  ops_dat dat;
+
+  if(data != NULL) {
+    dat = ops_decl_dat_core(block, size, block_size, offset, data, type_size, type, name);
+  }
+  else {
+    dat = ops_decl_dat_temp_core (block, size, block_size, offset,
+                                           data, type_size, type, name );
+    int bytes = size*type_size;
+    for (int i=0; i<block->dims; i++) bytes = bytes*block_size[i];
+    dat->data = (char*) calloc(bytes, 1); //initialize data bits to 0
+    //dat->data = (char*) malloc(bytes); //initialize data bits to 0
+    dat->user_managed = 0;
+  }
+
+  return dat;
+}
+
+ops_arg ops_arg_dat( ops_dat dat, ops_stencil stencil, char const * type, ops_access acc )
+{
+  return ops_arg_dat_core( dat, stencil, acc );
+}
+
+ops_arg ops_arg_gbl_char( char * data, int dim, int size, ops_access acc )
+{
+  return ops_arg_gbl_core( data, dim, size, acc );
+}
+
+void ops_print_dat_to_txtfile(ops_dat dat, const char *file_name)
+{
+  ops_print_dat_to_txtfile_core(dat, file_name);
+}
+
+void ops_decl_const_char( int dim, char const * type, int typeSize, char * data, char const * name )
+{
+  (void)dim;
+  (void)type;
+  (void)typeSize;
+  (void)data;
+  (void)name;
+}
+
+void ops_timers(double * cpu, double * et)
+{
+    ops_timers_core(cpu,et);
+}
