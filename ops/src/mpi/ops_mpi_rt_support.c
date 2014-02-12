96775ac40b22a1731712fe88e601f049bb07022e
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2014-02-12 12:01:46 +0000

    Removing other halo exchange routines

--- ops/src/mpi/ops_mpi_rt_support.c
+++ ops/src/mpi/ops_mpi_rt_support.c
@@ -87,69 +87,6 @@ void ops_exchange_halo(ops_arg* arg, int d /*depth*/)
   }
 }
 
-void ops_exchange_halo_edge2(ops_arg* arg, int d /*depth*/, int dir)
-{
-  ops_dat dat = arg->dat;
-  //printf("Exchanging halos for %s\n",dat->name);
-
-  sub_block_list sb = OPS_sub_block_list[dat->block->index];
-  sub_dat_list sd = OPS_sub_dat_list[dat->index];
-
-  int i1,i2,i3,i4; //indicies for halo and boundary of the dat
-  int* d_m = sd->d_m;
-  int* d_p = sd->d_p;
-  int* prod = sd->prod;
-  MPI_Status status;
-  int size = dat->size;
-
-  //send in positive direction, receive from negative direction
-  MPI_Sendrecv(&dat->data[(-d_m[dir]+sb->sizes[dir]-d)*size],d,MPI_DOUBLE_PRECISION,sb->id_p[dir],0,
-               &dat->data[(-d_m[dir]-d)*size],d,MPI_DOUBLE_PRECISION,sb->id_m[dir],0,
-               OPS_CART_COMM, &status);
-
-  //send in negative direction, receive from positive direction
-  MPI_Sendrecv(&dat->data[-d_m[dir]*size],d,MPI_DOUBLE_PRECISION,sb->id_m[dir],1,
-               &dat->data[(-d_m[dir]+sb->sizes[dir])*size],d,MPI_DOUBLE_PRECISION,sb->id_p[dir],1,
-               OPS_CART_COMM, &status);
-}
-
-
-void ops_exchange_halo_edge(ops_arg* arg, int d /*depth*/, int n)
-{
-  ops_dat dat = arg->dat;
-  //printf("Exchanging halos for %s\n",dat->name);
-
-  sub_block_list sb = OPS_sub_block_list[dat->block->index];
-  sub_dat_list sd = OPS_sub_dat_list[dat->index];
-
-  int i1,i2,i3,i4; //indicies for halo and boundary of the dat
-  int* d_m = sd->d_m;
-  int* d_p = sd->d_p;
-  int* prod = sd->prod;
-
-  int size = dat->size;
-  MPI_Status status;
-
-  i1 = (-d_m[n] - d) * prod[n-1];
-  i2 = (-d_m[n]    ) * prod[n-1];
-  i3 = (prod[n]/prod[n-1] - (-d_p[n]) - d) * prod[n-1];
-  i4 = (prod[n]/prod[n-1] - (-d_p[n])    ) * prod[n-1];
-
-  //send in positive direction, receive from negative direction
-  //printf("Exchaning 1 From:%d To: %d\n", i3, i1);
-  MPI_Sendrecv(&dat->data[i3*size],1,sd->mpidat[MAX_DEPTH*n+d],sb->id_p[n],0,
-               &dat->data[i1*size],1,sd->mpidat[MAX_DEPTH*n+d],sb->id_m[n],0,
-               OPS_CART_COMM, &status);
-
-  //send in negative direction, receive from positive direction
-  //printf("Exchaning 2 From:%d To: %d\n", i2, i4);
-  MPI_Sendrecv(&dat->data[i2*size],1,sd->mpidat[MAX_DEPTH*n+d],sb->id_m[n],1,
-               &dat->data[i4*size],1,sd->mpidat[MAX_DEPTH*n+d],sb->id_p[n],1,
-               OPS_CART_COMM, &status);
-
-
-}
-
 void ops_mpi_reduce_double(ops_arg* arg, double* data)
 {
   (void)data;
