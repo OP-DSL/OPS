3aa3c99bbfcced3de814c623e995e1e90b8ecb9e
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2014-02-18 13:30:05 +0000

    code generating generate.cpp

--- apps/CloverLeaf/MPI_Gen/generate_chunk_kernel_seq_kernel.cpp
+++ apps/CloverLeaf/MPI_Gen/generate_chunk_kernel_seq_kernel.cpp
@@ -5,23 +5,23 @@
 #include "ops_mpi_core.h"
 #include "lib.h"
 //user function
-#include "update_halo_kernel.h"
+#include "generate_chunk_kernel.h"
 
 // host stub function
-void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int dim, int* range,
+void ops_par_loop_generate_chunk_kernel(char const *name, ops_block block, int dim, int* range,
  ops_arg arg0, ops_arg arg1, ops_arg arg2, ops_arg arg3,
- ops_arg arg4, ops_arg arg5, ops_arg arg6) {
+ ops_arg arg4, ops_arg arg5, ops_arg arg6, ops_arg arg7) {
 
-  char *p_a[7];
-  int  offs[7][2];
-  ops_arg args[7] = { arg0, arg1, arg2, arg3, arg4, arg5, arg6};
+  char *p_a[8];
+  int  offs[8][2];
+  ops_arg args[8] = { arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7};
 
 
   sub_block_list sb = OPS_sub_block_list[block->index];
   //compute localy allocated range for the sub-block
   int ndim = sb->ndim;
-  int* start = (int*) xmalloc(sizeof(int)*ndim*7);
-  int* end = (int*) xmalloc(sizeof(int)*ndim*7);
+  int* start = (int*) xmalloc(sizeof(int)*ndim*8);
+  int* end = (int*) xmalloc(sizeof(int)*ndim*8);
 
   int s[ndim];
   int e[ndim];
@@ -42,7 +42,7 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
     }
   }
 
-  for ( int i=0; i<7; i++ ){
+  for ( int i=0; i<8; i++ ){
     for ( int n=0; n<ndim; n++ ){
       start[i*ndim+n] = s[n];
       end[i*ndim+n]   = e[n];
@@ -50,7 +50,7 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
   }
 
   #ifdef OPS_DEBUG
-  ops_register_args(args, "update_halo_kernel1_l1");
+  ops_register_args(args, "generate_chunk_kernel");
   #endif
 
   offs[0][0] = args[0].stencil->stride[0]*1;  //unit step in x dimension
@@ -130,6 +130,17 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
   args[6].dat->block_size, args[6].stencil->stride, args[6].dat->offset);
   ops_exchange_halo(&args[6],2);
 
+  offs[7][0] = args[7].stencil->stride[0]*1;  //unit step in x dimension
+  for ( int n=1; n<ndim; n++ ){
+    offs[7][n] = off2(ndim, n, &start[7*ndim],
+    &end[7*ndim],args[7].dat->block_size, args[7].stencil->stride);
+  }
+  //set up initial pointers
+  p_a[7] = (char *)args[7].data
+  + address2(ndim, args[7].dat->size, &start[7*ndim],
+  args[7].dat->block_size, args[7].stencil->stride, args[7].dat->offset);
+  ops_exchange_halo(&args[7],2);
+
   free(start);free(end);
 
   int off0_1 = offs[0][0];
@@ -153,6 +164,9 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
   int off6_1 = offs[6][0];
   int off6_2 = offs[6][1];
   int dat6 = args[6].dat->size;
+  int off7_1 = offs[7][0];
+  int off7_2 = offs[7][1];
+  int dat7 = args[7].dat->size;
 
   xdim0 = args[0].dat->block_size[0];
   xdim1 = args[1].dat->block_size[0];
@@ -161,14 +175,16 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
   xdim4 = args[4].dat->block_size[0];
   xdim5 = args[5].dat->block_size[0];
   xdim6 = args[6].dat->block_size[0];
+  xdim7 = args[7].dat->block_size[0];
 
   for ( int n_y=s[1]; n_y<e[1]; n_y++ ){
     for ( int n_x=s[0]; n_x<s[0]+(e[0]-s[0])/4; n_x++ ){
       //call kernel function, passing in pointers to data -vectorised
       #pragma simd
       for ( int i=0; i<4; i++ ){
-        update_halo_kernel1_l1(  (double *)p_a[0]+ i*1, (double *)p_a[1]+ i*1, (double *)p_a[2]+ i*1,
-           (double *)p_a[3]+ i*1, (double *)p_a[4]+ i*1, (double *)p_a[5]+ i*1, (double *)p_a[6]+ i*1 );
+        generate_chunk_kernel(  (double *)p_a[0]+ i*1, (double *)p_a[1]+ i*0, (double *)p_a[2]+ i*1,
+           (double *)p_a[3]+ i*1, (double *)p_a[4]+ i*1, (double *)p_a[5]+ i*1, (double *)p_a[6]+ i*1,
+           (double *)p_a[7]+ i*0 );
 
       }
 
@@ -180,12 +196,14 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
       p_a[4]= p_a[4] + (dat4 * off4_1)*4;
       p_a[5]= p_a[5] + (dat5 * off5_1)*4;
       p_a[6]= p_a[6] + (dat6 * off6_1)*4;
+      p_a[7]= p_a[7] + (dat7 * off7_1)*4;
     }
 
     for ( int n_x=s[0]+((e[0]-s[0])/4)*4; n_x<e[0]; n_x++ ){
       //call kernel function, passing in pointers to data - remainder
-      update_halo_kernel1_l1(  (double *)p_a[0], (double *)p_a[1], (double *)p_a[2],
-           (double *)p_a[3], (double *)p_a[4], (double *)p_a[5], (double *)p_a[6] );
+      generate_chunk_kernel(  (double *)p_a[0], (double *)p_a[1], (double *)p_a[2],
+           (double *)p_a[3], (double *)p_a[4], (double *)p_a[5], (double *)p_a[6],
+           (double *)p_a[7] );
 
 
       //shift pointers to data x direction
@@ -196,6 +214,7 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
       p_a[4]= p_a[4] + (dat4 * off4_1);
       p_a[5]= p_a[5] + (dat5 * off5_1);
       p_a[6]= p_a[6] + (dat6 * off6_1);
+      p_a[7]= p_a[7] + (dat7 * off7_1);
     }
 
     //shift pointers to data y direction
@@ -206,6 +225,7 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
     p_a[4]= p_a[4] + (dat4 * off4_2);
     p_a[5]= p_a[5] + (dat5 * off5_2);
     p_a[6]= p_a[6] + (dat6 * off6_2);
+    p_a[7]= p_a[7] + (dat7 * off7_2);
   }
   ops_mpi_reduce(&arg0,(double *)p_a[0]);
   ops_mpi_reduce(&arg1,(double *)p_a[1]);
@@ -214,5 +234,6 @@ void ops_par_loop_update_halo_kernel1_l1(char const *name, ops_block block, int 
   ops_mpi_reduce(&arg4,(double *)p_a[4]);
   ops_mpi_reduce(&arg5,(double *)p_a[5]);
   ops_mpi_reduce(&arg6,(double *)p_a[6]);
+  ops_mpi_reduce(&arg7,(double *)p_a[7]);
 
 }
