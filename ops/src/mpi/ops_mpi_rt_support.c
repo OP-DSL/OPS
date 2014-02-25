f33dc453d8588eb2be70bef0d2d40ca51d0ee54b
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2014-02-25 16:59:40 +0000

    validating application

--- ops/src/mpi/ops_mpi_rt_support.c
+++ ops/src/mpi/ops_mpi_rt_support.c
@@ -54,9 +54,9 @@ void ops_exchange_halo(ops_arg* arg, int d /*depth*/)
 {
   ops_dat dat = arg->dat;
 
-  if(arg->opt == 1 && dat->dirtybit == 1) { //need to check OPS accs
-  //if( dat->dirtybit == 1) { //need to check OPS accs
-    //ops_printf("exchanging %s\n",arg->dat->name);
+  //if(arg->opt == 1 && dat->dirtybit == 1) { //need to check OPS accs
+  if( dat->dirtybit == 1) { //need to check OPS accs
+  // ops_printf("exchanging %s\n",arg->dat->name);
 
     sub_block_list sb = OPS_sub_block_list[dat->block->index];
     sub_dat_list sd = OPS_sub_dat_list[dat->index];
