76d958f3bbe07215ee723515777e0304952dfe2d
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2014-03-10 13:11:31 +0000

    Consolidating MPI_OpenMP application level files

--- apps/CloverLeaf/MPI_OpenMP/update_halo_kernel1_t2_omp_kernel.cpp
+++ apps/CloverLeaf/MPI_OpenMP/update_halo_kernel1_t2_omp_kernel.cpp
@@ -6,10 +6,10 @@
 #include <omp.h>
 #endif
 //user function
-#include "advec_cell_kernel.h"
+#include "update_halo_kernel.h"
 
 // host stub function
-void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int dim, int* range,
+void ops_par_loop_update_halo_kernel1_t2(char const *name, ops_block block, int dim, int* range,
  ops_arg arg0, ops_arg arg1, ops_arg arg2, ops_arg arg3,
  ops_arg arg4, ops_arg arg5, ops_arg arg6, ops_arg arg7) {
 
@@ -50,7 +50,7 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
   }
 
   #ifdef OPS_DEBUG
-  ops_register_args(args, "advec_cell_kernel3_xdir");
+  ops_register_args(args, "update_halo_kernel1_t2");
   #endif
 
   offs[0][0] = args[0].stencil->stride[0]*1;  //unit step in x dimension
@@ -81,10 +81,6 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
   offs[6][1] = off2D(1, &start_add[6*2],
   &end_add[6*2],args[6].dat->block_size, args[6].stencil->stride);
 
-  offs[7][0] = args[7].stencil->stride[0]*1;  //unit step in x dimension
-  offs[7][1] = off2D(1, &start_add[7*2],
-  &end_add[7*2],args[7].dat->block_size, args[7].stencil->stride);
-
 
 
   int off0_1 = offs[0][0];
@@ -108,16 +104,22 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
   int off6_1 = offs[6][0];
   int off6_2 = offs[6][1];
   int dat6 = args[6].dat->size;
-  int off7_1 = offs[7][0];
-  int off7_2 = offs[7][1];
-  int dat7 = args[7].dat->size;
 
+  int*arg7h = (int *)arg7.data;
 
   #ifdef _OPENMP
   int nthreads = omp_get_max_threads( );
   #else
   int nthreads = 1;
   #endif
+  //allocate and initialise arrays for global reduction
+  //assumes a max of 64 threads with a cacche line size of 64 bytes
+  int arg_gbl7[NUM_FIELDS * 64 * 64];
+  for ( int thr=0; thr<nthreads; thr++ ){
+    for ( int d=0; d<NUM_FIELDS; d++ ){
+      arg_gbl7[d+64*thr] = arg7h[d];
+    }
+  }
   xdim0 = args[0].dat->block_size[0];
   xdim1 = args[1].dat->block_size[0];
   xdim2 = args[2].dat->block_size[0];
@@ -125,7 +127,6 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
   xdim4 = args[4].dat->block_size[0];
   xdim5 = args[5].dat->block_size[0];
   xdim6 = args[6].dat->block_size[0];
-  xdim7 = args[7].dat->block_size[0];
 
   //Halo Exchanges
   ops_exchange_halo(&args[0],2);
@@ -134,13 +135,14 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
   ops_exchange_halo(&args[3],2);
   ops_exchange_halo(&args[4],2);
   ops_exchange_halo(&args[5],2);
+  ops_exchange_halo(&args[6],2);
 
   ops_H_D_exchanges(args, 8);
 
 
   //Timing
   double t1,t2,c1,c2;
-  ops_timing_realloc(10,"advec_cell_kernel3_xdir");
+  ops_timing_realloc(38,"update_halo_kernel1_t2");
   ops_timers_core(&c1,&t1);
 
   int start_thread_add[ndim*nthreads*64];
@@ -207,22 +209,16 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
     (start_thread_add[64*ndim*thr+1] * args[6].stencil->stride[1] - args[6].dat->offset[1]);
     p_a[6] = (char *)args[6].data + base6;
 
-    //set up initial pointers 
-    int base7 = dat7 * 1 * 
-    (start_thread_add[64*ndim*thr+0] * args[7].stencil->stride[0] - args[7].dat->offset[0]);
-    base7 = base7  + dat7 * args[7].dat->block_size[0] * 
-    (start_thread_add[64*ndim*thr+1] * args[7].stencil->stride[1] - args[7].dat->offset[1]);
-    p_a[7] = (char *)args[7].data + base7;
+    p_a[7] = (char *)args[7].data;
 
 
     for ( int n_y=start; n_y<finish; n_y++ ){
       for ( int n_x=s[0]; n_x<s[0]+(e[0]-s[0])/SIMD_VEC; n_x++ ){
         //call kernel function, passing in pointers to data -vectorised
-        #pragma simd
         for ( int i=0; i<SIMD_VEC; i++ ){
-          advec_cell_kernel3_xdir(  (double *)p_a[0]+ i*1, (double *)p_a[1]+ i*1, (int *)p_a[2]+ i*1,
+          update_halo_kernel1_t2(  (double *)p_a[0]+ i*1, (double *)p_a[1]+ i*1, (double *)p_a[2]+ i*1,
            (double *)p_a[3]+ i*1, (double *)p_a[4]+ i*1, (double *)p_a[5]+ i*1, (double *)p_a[6]+ i*1,
-           (double *)p_a[7]+ i*1 );
+           &arg_gbl7[64*thr] );
 
         }
 
@@ -234,14 +230,13 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
         p_a[4]= p_a[4] + (dat4 * off4_1)*SIMD_VEC;
         p_a[5]= p_a[5] + (dat5 * off5_1)*SIMD_VEC;
         p_a[6]= p_a[6] + (dat6 * off6_1)*SIMD_VEC;
-        p_a[7]= p_a[7] + (dat7 * off7_1)*SIMD_VEC;
       }
 
       for ( int n_x=s[0]+((e[0]-s[0])/SIMD_VEC)*SIMD_VEC; n_x<e[0]; n_x++ ){
         //call kernel function, passing in pointers to data - remainder
-        advec_cell_kernel3_xdir(  (double *)p_a[0], (double *)p_a[1], (int *)p_a[2],
+        update_halo_kernel1_t2(  (double *)p_a[0], (double *)p_a[1], (double *)p_a[2],
            (double *)p_a[3], (double *)p_a[4], (double *)p_a[5], (double *)p_a[6],
-           (double *)p_a[7] );
+           &arg_gbl7[64*thr] );
 
 
         //shift pointers to data x direction
@@ -252,7 +247,6 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
         p_a[4]= p_a[4] + (dat4 * off4_1);
         p_a[5]= p_a[5] + (dat5 * off5_1);
         p_a[6]= p_a[6] + (dat6 * off6_1);
-        p_a[7]= p_a[7] + (dat7 * off7_1);
       }
 
       //shift pointers to data y direction
@@ -263,24 +257,33 @@ void ops_par_loop_advec_cell_kernel3_xdir(char const *name, ops_block block, int
       p_a[4]= p_a[4] + (dat4 * off4_2);
       p_a[5]= p_a[5] + (dat5 * off5_2);
       p_a[6]= p_a[6] + (dat6 * off6_2);
-      p_a[7]= p_a[7] + (dat7 * off7_2);
+    }
+  }
+
+  // combine reduction data
+  for ( int thr=0; thr<nthreads; thr++ ){
+    for ( int d=0; d<NUM_FIELDS; d++ ){
     }
   }
   ops_set_dirtybit_host(args, 8);
 
+  ops_set_halo_dirtybit(&args[0]);
+  ops_set_halo_dirtybit(&args[1]);
+  ops_set_halo_dirtybit(&args[2]);
+  ops_set_halo_dirtybit(&args[3]);
+  ops_set_halo_dirtybit(&args[4]);
+  ops_set_halo_dirtybit(&args[5]);
   ops_set_halo_dirtybit(&args[6]);
-  ops_set_halo_dirtybit(&args[7]);
 
   //Update kernel record
   ops_timers_core(&c2,&t2);
-  OPS_kernels[10].count++;
-  OPS_kernels[10].time += t2-t1;
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg0);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg1);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg2);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg3);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg4);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg5);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg6);
-  OPS_kernels[10].transfer += ops_compute_transfer(dim, range, &arg7);
+  OPS_kernels[38].count++;
+  OPS_kernels[38].time += t2-t1;
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg0);
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg1);
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg2);
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg3);
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg4);
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg5);
+  OPS_kernels[38].transfer += ops_compute_transfer(dim, range, &arg6);
 }
