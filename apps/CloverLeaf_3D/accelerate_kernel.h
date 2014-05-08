b0c7990bef492ed9f767405a49b03f157805f454
Istvan Reguly <oerc0060@broomway.oerc.ox.ac.uk>
2014-05-08 11:34:08 +0100

    No NaNs

---
+++ apps/CloverLeaf_3D/accelerate_kernel.h
@@ -0,0 +1,69 @@
+#ifndef ACCELERATE_KERNEL_H
+#define ACCELERATE_KERNEL_H
+
+#include "data.h"
+#include "definitions.h"
+
+
+void accelerate_kernel( const double *density0, const double *volume,
+                double *stepbymass, const double *xvel0, double *xvel1,
+                const double *xarea, const double *pressure,
+                const double *yvel0, double *yvel1,
+                const double *yarea, const double *viscosity,
+                const double *zvel0, double *zvel1,
+                const double *zarea) {
+
+  double nodal_mass = 0.0;
+  nodal_mass =(density0[OPS_ACC0(-1,-1, 0)] * volume[OPS_ACC0(-1,-1, 0)] +
+               density0[OPS_ACC0( 0,-1, 0)] * volume[OPS_ACC0( 0,-1, 0)] +
+               density0[OPS_ACC0( 0, 0, 0)] * volume[OPS_ACC0( 0, 0, 0)] +
+               density0[OPS_ACC0(-1, 0, 0)] * volume[OPS_ACC0(-1, 0, 0)] +
+               density0[OPS_ACC0(-1,-1,-1)] * volume[OPS_ACC0(-1,-1,-1)] +
+               density0[OPS_ACC0( 0,-1,-1)] * volume[OPS_ACC0( 0,-1,-1)] +
+               density0[OPS_ACC0( 0, 0,-1)] * volume[OPS_ACC0( 0, 0,-1)] +
+               density0[OPS_ACC0(-1, 0,-1)] * volume[OPS_ACC0(-1, 0,-1)]) * 0.125;
+  
+  stepbymass[OPS_ACC2(0,0,0)] = 0.25*dt / nodal_mass;
+
+  xvel1[OPS_ACC4(0,0,0)] = xvel0[OPS_ACC3(0,0,0)] - stepbymass[OPS_ACC2(0,0,0)] *
+            ( xarea[OPS_ACC5(0,0,0)]  * ( pressure[OPS_ACC6(0,0,0)] - pressure[OPS_ACC6(-1,0,0)] ) +
+              xarea[OPS_ACC5(0,-1,0)] * ( pressure[OPS_ACC6(0,-1,0)] - pressure[OPS_ACC6(-1,-1,0)] ) +
+              xarea[OPS_ACC5(0,0,-1)] * ( pressure[OPS_ACC6(0,0,-1)] - pressure[OPS_ACC6(-1,0,-1)] ) +
+              xarea[OPS_ACC5(0,-1,-1)] * ( pressure[OPS_ACC6(0,-1,-1)] - pressure[OPS_ACC6(-1,-1,-1)] ) );
+
+  yvel1[OPS_ACC8(0,0,0)] = yvel0[OPS_ACC7(0,0,0)] - stepbymass[OPS_ACC2(0,0,0)] *
+            ( yarea[OPS_ACC9(0,0,0)]  * ( pressure[OPS_ACC6(0,0,0)] - pressure[OPS_ACC6(0,-1,0)] ) +
+              yarea[OPS_ACC9(-1,0,0)] * ( pressure[OPS_ACC6(-1,0,0)] - pressure[OPS_ACC6(-1,-1,0)] ) +
+              yarea[OPS_ACC9(0,0,-1)] * ( pressure[OPS_ACC6(0,0,-1)] - pressure[OPS_ACC6(0,-1,-1)] ) +
+              yarea[OPS_ACC9(-1,0,-1)]* ( pressure[OPS_ACC6(-1,0,-1)] - pressure[OPS_ACC6(-1,-1,-1)] ) );
+              
+  zvel1[OPS_ACC12(0,0,0)] = zvel0[OPS_ACC11(0,0,0)] - stepbymass[OPS_ACC2(0,0,0)] *
+            ( zarea[OPS_ACC13(0,0,0)]  * ( pressure[OPS_ACC6(0,0,0)] - pressure[OPS_ACC6(0,0,-1)] ) +
+              zarea[OPS_ACC13(0,-1,0)] * ( pressure[OPS_ACC6(0,-1,0)] - pressure[OPS_ACC6(0,-1,-1)] ) +
+              zarea[OPS_ACC13(-1,0,0)] * ( pressure[OPS_ACC6(-1,0,0)] - pressure[OPS_ACC6(-1,0,-1)] ) +
+              zarea[OPS_ACC13(-1,-1,0)]* ( pressure[OPS_ACC6(-1,-1,0)] - pressure[OPS_ACC6(-1,-1,-1)] ) );
+
+
+  xvel1[OPS_ACC4(0,0,0)] = xvel1[OPS_ACC4(0,0,0)] - stepbymass[OPS_ACC2(0,0,0)] *
+            ( xarea[OPS_ACC5(0,0,0)]  * ( viscosity[OPS_ACC10(0,0,0)] - viscosity[OPS_ACC10(-1,0,0)] ) +
+              xarea[OPS_ACC5(0,-1,0)] * ( viscosity[OPS_ACC10(0,-1,0)] - viscosity[OPS_ACC10(-1,-1,0)] ) +
+              xarea[OPS_ACC5(0,0,-1)] * ( viscosity[OPS_ACC10(0,0,-1)] - viscosity[OPS_ACC10(-1,0,-1)] ) +
+              xarea[OPS_ACC5(0,-1,-1)]* ( viscosity[OPS_ACC10(0,-1,-1)] - viscosity[OPS_ACC10(-1,-1,-1)] ) );
+
+  yvel1[OPS_ACC8(0,0,0)] = yvel1[OPS_ACC8(0,0,0)] - stepbymass[OPS_ACC2(0,0,0)] *
+            ( yarea[OPS_ACC9(0,0,0)]  * ( viscosity[OPS_ACC10(0,0,0)]  - viscosity[OPS_ACC10(0,-1,0)] ) +
+              yarea[OPS_ACC9(-1,0,0)] * ( viscosity[OPS_ACC10(-1,0,0)] - viscosity[OPS_ACC10(-1,-1,0)] ) +
+              yarea[OPS_ACC9(0,0,-1)] * ( viscosity[OPS_ACC10(0,0,-1)] - viscosity[OPS_ACC10(0,-1,-1)] ) +
+              yarea[OPS_ACC9(-1,0,-1)]* ( viscosity[OPS_ACC10(-1,0,-1)]- viscosity[OPS_ACC10(-1,-1,-1)] ) );
+  
+  zvel1[OPS_ACC12(0,0,0)] = zvel1[OPS_ACC12(0,0,0)] - stepbymass[OPS_ACC2(0,0,0)] *
+            ( zarea[OPS_ACC13(0,0,0)]  * ( viscosity[OPS_ACC10(0,0,0)]  - viscosity[OPS_ACC10(0,0,-1)] ) +
+              zarea[OPS_ACC13(0,-1,0)] * ( viscosity[OPS_ACC10(0,-1,0)] - viscosity[OPS_ACC10(0,-1,-1)] ) +
+              zarea[OPS_ACC13(-1,0,0)] * ( viscosity[OPS_ACC10(-1,0,0)] - viscosity[OPS_ACC10(-1,0,-1)] ) +
+              zarea[OPS_ACC13(-1,-1,0)]* ( viscosity[OPS_ACC10(-1,-1,0)]- viscosity[OPS_ACC10(-1,-1,-1)] ) );
+
+
+}
+
+
+#endif
---
+++ apps/CloverLeaf_3D/advec_mom.cpp
@@ -0,0 +1,222 @@
+/* Crown Copyright 2012 AWE.
+
+ This file is part of CloverLeaf.
+
+ CloverLeaf is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or (at your option)
+ any later version.
+
+ CloverLeaf is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License along with
+ CloverLeaf. If not, see http://www.gnu.org/licenses/. */
+
+/** @brief momentum advection
+ *  @author Wayne Gaudin, converted to OPS by Gihan Mudalige
+ *  @details Performs a second order advective remap on the vertex momentum
+ *  using van-Leer limiting and diral splitting.
+ *  Note that although pre_vol is only set and not used in the update, please
+ *  leave it in the method.
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// OPS header file
+#include "ops_seq.h"
+
+#include "data.h"
+#include "definitions.h"
+#include "advec_mom_kernel.h"
+
+
+void advec_mom(int which_vel, int sweep_number, int dir)
+{
+  //initialize sizes using global values
+  int x_cells = grid.x_cells;
+  int y_cells = grid.y_cells;
+  int x_min = field.x_min;
+  int x_max = field.x_max;
+  int y_min = field.y_min;
+  int y_max = field.y_max;
+  int z_min = field.z_min;
+  int z_max = field.z_max;
+  
+  int rangexyz[] = {x_min-2,x_max+2,y_min-2,y_max+2,z_min-2,z_max+2}; // full range over grid
+
+  int mom_sweep;
+  ops_dat vel1;
+
+  if( which_vel == 1) {
+    vel1 = xvel1;
+  }
+  else if( which_vel == 2) {
+    vel1 = yvel1;
+  }
+  else if( which_vel == 3) {
+    vel1 = zvel1;
+  }
+
+  if(sweep_number==1 && dir == 1) {
+      ops_par_loop(advec_mom_kernel_x1, "advec_mom_kernel_x1", clover_grid, 3, rangexyz,
+        ops_arg_dat(work_array6, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(work_array7, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(volume, S3D_000, "double", OPS_READ),
+        ops_arg_dat(vol_flux_x, S3D_000_P100, "double", OPS_READ),
+        ops_arg_dat(vol_flux_y, S3D_000_0P10, "double", OPS_READ),
+        ops_arg_dat(vol_flux_z, S3D_000_00P1, "double", OPS_READ));
+  }
+  else if(sweep_number==1 && dir == 3) {
+    ops_par_loop(advec_mom_kernel_z1, "advec_mom_kernel_z1", clover_grid, 3, rangexyz,
+        ops_arg_dat(work_array6, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(work_array7, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(volume, S3D_000, "double", OPS_READ),
+        ops_arg_dat(vol_flux_x, S3D_000_P100, "double", OPS_READ),
+        ops_arg_dat(vol_flux_y, S3D_000_0P10, "double", OPS_READ),
+        ops_arg_dat(vol_flux_z, S3D_000_00P1, "double", OPS_READ));
+  }
+  else if (sweep_number==2 && advect_x) {
+    ops_par_loop(advec_mom_kernel_x2, "advec_mom_kernel_x2", clover_grid, 3, rangexyz,
+        ops_arg_dat(work_array6, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(work_array7, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(volume, S3D_000, "double", OPS_READ),
+        ops_arg_dat(vol_flux_y, S3D_000_0P10, "double", OPS_READ),
+        ops_arg_dat(vol_flux_z, S3D_000_00P1, "double", OPS_READ));
+  }
+  else if (sweep_number==2 && !advect_x) {
+    ops_par_loop(advec_mom_kernel_y2, "advec_mom_kernel_y2", clover_grid, 3, rangexyz,
+        ops_arg_dat(work_array6, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(work_array7, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(volume, S3D_000, "double", OPS_READ),
+        ops_arg_dat(vol_flux_x, S3D_000_P100, "double", OPS_READ),
+        ops_arg_dat(vol_flux_y, S3D_000_0P10, "double", OPS_READ));
+  }
+  else if (sweep_number==3 && dir == 1) {
+    ops_par_loop(advec_mom_kernel_x3, "advec_mom_kernel_x3", clover_grid, 3, rangexyz,
+        ops_arg_dat(work_array6, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(work_array7, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(volume, S3D_000, "double", OPS_READ),
+        ops_arg_dat(vol_flux_x, S3D_000_P100, "double", OPS_READ));
+  }
+  else if (sweep_number==3 && dir == 3) {
+    ops_par_loop(advec_mom_kernel_z3, "advec_mom_kernel_z3", clover_grid, 3, rangexyz,
+        ops_arg_dat(work_array6, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(work_array7, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(volume, S3D_000, "double", OPS_READ),
+        ops_arg_dat(vol_flux_z, S3D_000_00P1, "double", OPS_READ));
+  }
+
+  
+  int range_partx_party_1[] = {x_min-1,x_max+2,y_min,y_max+1}; // partial x range partial y range
+
+  int range_fully_party_1[] = {x_min,x_max+1,y_min-2,y_max+2}; // full y range partial x range
+  int range_partx_party_2[] = {x_min,x_max+1,y_min-1,y_max+2}; // partial x range partial y range
+
+  if (dir == 1) {
+    if (which_vel == 1) {
+      //Find staggered mesh mass fluxes, nodal masses and volumes.
+      int range_fullx_party_partz_1[] = {x_min-2,x_max+2,y_min,y_max+1,z_min,z_max+1}; // full x range partial y,z range
+      ops_par_loop(advec_mom_kernel_mass_flux_x, "advec_mom_kernel_mass_flux_x", clover_grid, 3, range_fullx_party_partz_1,
+          ops_arg_dat(work_array1, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(mass_flux_x, S3D_000_fP1M1M1, "double", OPS_READ));
+
+      //Staggered cell mass post and pre advection
+      int range_partx_party_partz_1[] = {x_min-1,x_max+2,y_min,y_max+1,z_min,z_max+1}; // partial x,y,z range
+      ops_par_loop(advec_mom_kernel_post_pre_advec_x, "advec_mom_kernel_post_pre_advec_x", clover_grid, 3, range_partx_party_partz_1,
+          ops_arg_dat(work_array2/*node_mass_post*/, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(work_array7/*post_vol*/, S3D_000_fM1M1M1, "double", OPS_READ),
+          ops_arg_dat(density1, S3D_000_fM1M1M1, "double", OPS_READ),
+          ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(work_array1/*node_flux*/, S3D_000_M100, "double", OPS_READ));
+    }
+    
+    int range_innder_plus1xyz_minus1x[] = {x_min-1,x_max+1,y_min,y_max+1,z_min,z_max+1}; // partial x range partial y range
+    ops_par_loop(advec_mom_kernel1_x_nonvector, "advec_mom_kernel1_x", clover_grid, 3, range_innder_plus1xyz_minus1x,
+        ops_arg_dat(work_array1/*node_flux*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000_P100, "double", OPS_READ),
+        ops_arg_dat(work_array5/*mom_flux*/, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(celldx, S3D_000_P100_M100_STRID3D_X, "double", OPS_READ),
+        ops_arg_dat(vel1, S3D_000_P100_P200_M100, "double", OPS_READ));
+
+    int range_partx_party_partz_2[] = {x_min,x_max+1,y_min,y_max+1,z_min,z_max+1};
+    ops_par_loop(advec_mom_kernel2_x, "advec_mom_kernel2_x", clover_grid, 3, range_partx_party_partz_2,
+        ops_arg_dat(vel1, S3D_000, "double", OPS_RW),
+        ops_arg_dat(work_array2/*node_mass_post*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array5/*mom_flux*/, S3D_000_M100, "double", OPS_READ)
+        );
+  }
+  else if (dir == 2) {
+    if (which_vel == 1) {
+      //Find staggered mesh mass fluxes, nodal masses and volumes.
+      int range_fully_partx_partz_1[] = {x_min,x_max+1,y_min-2,y_max+2,z_min,z_max+1}; // full x range partial y,z range
+      ops_par_loop(advec_mom_kernel_mass_flux_y, "advec_mom_kernel_mass_flux_y", clover_grid, 3, range_fully_partx_partz_1,
+          ops_arg_dat(work_array1, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(mass_flux_y, S3D_000_fM1P1M1, "double", OPS_READ));
+
+      //Staggered cell mass post and pre advection
+      int range_party_partx_partz_1[] = {x_min,x_max+1,y_min-1,y_max+2,z_min,z_max+1}; // full x range partial y,z range
+      ops_par_loop(advec_mom_kernel_post_pre_advec_y, "advec_mom_kernel_post_pre_advec_y", clover_grid, 3, range_party_partx_partz_1,
+          ops_arg_dat(work_array2/*node_mass_post*/, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(work_array7, S3D_000_fM1M1M1, "double", OPS_READ),
+          ops_arg_dat(density1, S3D_000_fM1M1M1, "double", OPS_READ),
+          ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(work_array1/*node_flux*/, S3D_000_0M10, "double", OPS_READ));
+    }
+    int range_plus1xyz_minus1y[] = {x_min,x_max+1,y_min-1,y_max+1,z_min,z_max+1}; // partial x range partial y range
+    ops_par_loop(advec_mom_kernel1_y_nonvector, "advec_mom_kernel1_y", clover_grid, 3, range_plus1xyz_minus1y,
+        ops_arg_dat(work_array1/*node_flux*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000_0P10, "double", OPS_READ),
+        ops_arg_dat(work_array5/*mom_flux*/, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(celldy, S3D_000_0P10_0M10_STRID3D_Y, "double", OPS_READ),
+        ops_arg_dat(vel1, S3D_000_0P10_0P20_0M10, "double", OPS_READ));
+
+    int range_partx_party_partz_2[] = {x_min,x_max+1,y_min,y_max+1,z_min,z_max+1};
+    ops_par_loop(advec_mom_kernel2_y, "advec_mom_kernel2_y", clover_grid, 3, range_partx_party_partz_2,
+        ops_arg_dat(vel1, S3D_000, "double", OPS_RW),
+        ops_arg_dat(work_array2/*node_mass_post*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array5/*mom_flux*/, S3D_000_0M10, "double", OPS_READ));
+
+  }
+  else if (dir == 3) {
+    if (which_vel == 1) {
+      //Find staggered mesh mass fluxes, nodal masses and volumes.
+      int range_fullz_partx_party_1[] = {x_min,x_max+1,y_min,y_max+1,z_min-2,z_max+2}; // full x range partial y,z range
+      ops_par_loop(advec_mom_kernel_mass_flux_z, "advec_mom_kernel_mass_flux_z", clover_grid, 3, range_fullz_partx_party_1,
+          ops_arg_dat(work_array1, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(mass_flux_z, S3D_000_fM1M1P1, "double", OPS_READ));
+
+      //Staggered cell mass post and pre advection
+      int range_party_partx_partz_1[] = {x_min,x_max+1,y_min,y_max+1,z_min-1,z_max+2}; // full x range partial y,z range
+      ops_par_loop(advec_mom_kernel_post_pre_advec_z, "advec_mom_kernel_post_pre_advec_z", clover_grid, 3, range_party_partx_partz_1,
+          ops_arg_dat(work_array2/*node_mass_post*/, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(work_array7, S3D_000_fM1M1M1, "double", OPS_READ),
+          ops_arg_dat(density1, S3D_000_fM1M1M1, "double", OPS_READ),
+          ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000, "double", OPS_WRITE),
+          ops_arg_dat(work_array1/*node_flux*/, S3D_000_00M1, "double", OPS_READ));
+    }
+    int range_plus1xyz_minus1z[] = {x_min,x_max+1,y_min,y_max+1,z_min-1,z_max+1}; // partial x range partial y range
+    ops_par_loop(advec_mom_kernel1_z_nonvector, "advec_mom_kernel1_z", clover_grid, 3, range_plus1xyz_minus1z,
+        ops_arg_dat(work_array1/*node_flux*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000_00P1, "double", OPS_READ),
+        ops_arg_dat(work_array5/*mom_flux*/, S3D_000, "double", OPS_WRITE),
+        ops_arg_dat(celldz, S3D_000_00P1_00M1_STRID3D_Z, "double", OPS_READ),
+        ops_arg_dat(vel1, S3D_000_00P1_00P2_00M1, "double", OPS_READ));
+
+    int range_partx_party_partz_2[] = {x_min,x_max+1,y_min,y_max+1,z_min,z_max+1}; // full x range partial y range
+    ops_par_loop(advec_mom_kernel2_z, "advec_mom_kernel2_z", clover_grid, 3, range_partx_party_partz_2,
+        ops_arg_dat(vel1, S3D_000, "double", OPS_RW),
+        ops_arg_dat(work_array2/*node_mass_post*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array3/*node_mass_pre*/, S3D_000, "double", OPS_READ),
+        ops_arg_dat(work_array5/*mom_flux*/, S3D_000_00M1, "double", OPS_READ));
+
+  }
+
+}
