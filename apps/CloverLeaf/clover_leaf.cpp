5a78b1a28141cec261da92385eabdaf143b72f42
Istvan Zoltan Reguly <ireguly@phileas.oerc.ox.ac.uk>
2014-07-08 13:12:28 +0100

    MIC

--- apps/CloverLeaf/clover_leaf.cpp
+++ apps/CloverLeaf/clover_leaf.cpp
@@ -136,7 +136,7 @@ int main(int argc, char **argv)
   /**-------------------------- OPS Initialisation --------------------------**/
 
   // OPS initialisation
-  ops_init(argc,argv,1);
+  ops_init(argc,argv,2);
   ops_printf(" Clover version %f\n", g_version);
 
 
