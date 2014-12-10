b45f6e8e4b7de10dcc720a3e3cbdbc7ed41ced18
satyajammy <s.p.jammy@soton.ac.uk>
2014-12-10 13:21:58 +0000

    Update visc.cpp

--- apps/SHSGC_dev/visc.cpp
+++ apps/SHSGC_dev/visc.cpp
@@ -21,11 +21,7 @@
 
 #include "xder1_kernel.h"
 #include "xder12_kernel.h"
-#include "visc1_kernel.h"
-#include "visc_kernel.h"
-#include "energy1_kernel.h"
-#include "energy2_kernel.h"
-#include "energy3_kernel.h"
+
 #include "flowvar_kernel.h"
 #include "zerovisc_kernel.h"
 
@@ -54,11 +50,6 @@ ops_par_loop(xder12_kernel, "xder12_kernel" ,shsgc_grid , 1 ,nxp_range_1,
 			ops_arg_dat(u_xx ,1 ,S1D_0 ,"double" ,OPS_WRITE));
 
 
-// Evaluation of T__x and T__xx
-ops_par_loop(xder12_kernel, "xder12_kernel" ,shsgc_grid , 1 ,nxp_range_1,
-			ops_arg_dat(T ,1 ,S1D_0M1M2P1P2 ,"double" ,OPS_READ),
-			ops_arg_dat(T_x ,1 ,S1D_0 ,"double" ,OPS_WRITE),
-			ops_arg_dat(T_xx ,1 ,S1D_0 ,"double" ,OPS_WRITE));
 	// Evaluation of mu__x
 ops_par_loop(xder1_kernel, "xder1_kernel" ,shsgc_grid , 1 ,nxp_range_1,
 			ops_arg_dat(mu ,1 ,S1D_0M1M2P1P2, "double" ,OPS_READ),
@@ -73,33 +64,5 @@ ops_par_loop(zerovisc_kernel, "zerovisc_kernel" ,shsgc_grid , 1 ,nxp_range_1,
 //
 //Here is the calculation of viscous trems that involves derivatives
 //
-		
-ops_par_loop(visc_kernel1, "visc_kernel1" ,shsgc_grid , 1 ,nxp_range,
-			ops_arg_dat(u_x ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(mu_x ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(viscu_res ,1 ,S1D_0 ,"double" ,OPS_WRITE));
-//
-				
-ops_par_loop(visc_kernel, "visc_kernel" ,shsgc_grid , 1 ,nxp_range,
-			ops_arg_dat(mu ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(u_xx ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(viscu_res ,1 ,S1D_0 ,"double" ,OPS_WRITE));
-				
-	// energy equation
-ops_par_loop(energy1_kernel, "energy1_kernel" ,shsgc_grid , 1 ,nxp_range,
-			ops_arg_dat(mu_x ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(T_x ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(viscE_res ,1 ,S1D_0 ,"double" ,OPS_WRITE));
-	
-ops_par_loop(energy2_kernel, "energy2_kernel" ,shsgc_grid , 1 ,nxp_range,
-			ops_arg_dat(mu ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(T_xx ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(viscE_res ,1 ,S1D_0 ,"double" ,OPS_WRITE));
-	
-ops_par_loop(energy3_kernel, "energy3_kernel" ,shsgc_grid , 1 ,nxp_range,
-			ops_arg_dat(u ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(u_x ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(mu ,1 ,S1D_0 ,"double" ,OPS_READ),
-			ops_arg_dat(viscE_res ,1 ,S1D_0 ,"double" ,OPS_WRITE));
 
 }
