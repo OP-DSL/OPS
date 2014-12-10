ee715b1250cbdcbabb4ff16b18db5762c96c1dba
satyajammy <s.p.jammy@soton.ac.uk>
2014-12-10 13:27:50 +0000

    Update visc.cpp

--- apps/SHSGC_dev/visc.cpp
+++ apps/SHSGC_dev/visc.cpp
@@ -36,14 +36,6 @@ double ct = gam1 * gam * pow(Mach,2);
 double cq = 1.0 / (gam1 * Pr * pow(Mach,2));
 
 
-ops_par_loop(flowvar_kernel, "flowvar_kernel", shsgc_grid , 1 , nxp_range,
-			 ops_arg_dat(rho_new ,1 ,S1D_0 ,"double" ,OPS_READ),
-			 ops_arg_dat(rhou_new ,1 ,S1D_0 ,"double" ,OPS_READ),
-			 ops_arg_dat(rhoE_new ,1 ,S1D_0 ,"double" ,OPS_READ),
-			 ops_arg_dat(u ,1 ,S1D_0 ,"double" ,OPS_WRITE),
-			 ops_arg_dat(T ,1 ,S1D_0 ,"double" ,OPS_WRITE),
-			 ops_arg_dat(mu ,1 ,S1D_0 ,"double" ,OPS_WRITE));
-	
 ops_par_loop(xder12_kernel, "xder12_kernel" ,shsgc_grid , 1 ,nxp_range_1,
 			ops_arg_dat(u ,1 ,S1D_0M1M2P1P2 ,"double" ,OPS_READ),
 			ops_arg_dat(u_x ,1 ,S1D_0 ,"double" ,OPS_WRITE),
@@ -55,12 +47,7 @@ ops_par_loop(xder1_kernel, "xder1_kernel" ,shsgc_grid , 1 ,nxp_range_1,
 			ops_arg_dat(mu ,1 ,S1D_0M1M2P1P2, "double" ,OPS_READ),
 			ops_arg_dat(mu_x ,1 ,S1D_0, "double" ,OPS_WRITE));
 		
-	
-//IMPORTANT Make sure the viscous residues are equated to zero
-ops_par_loop(zerovisc_kernel, "zerovisc_kernel" ,shsgc_grid , 1 ,nxp_range_1,
-			ops_arg_dat(viscu_res ,1 , S1D_0,"double" ,OPS_WRITE),
-			ops_arg_dat(viscE_res ,1 , S1D_0,"double" ,OPS_WRITE));
-			 
+
 //
 //Here is the calculation of viscous trems that involves derivatives
 //
