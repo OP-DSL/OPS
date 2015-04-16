91d31b452374bf3aba74ca732dfedd9a8698b0eb
Gihan R. Mudalige <gihan.mudalige@oerc.ox.ac.uk>
2015-04-16 13:33:23 +0100

    setting up different module locations for openacc

--- apps/c/shsgc/test.sh
+++ apps/c/shsgc/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#cd ../../ops/
-#source ./source_intel
-#make
-#cd -
-#make
+cd ../../../ops/c
+source ../rubly_intel_source
+make
+cd -
+make
 #============================ Test SHSGC ==========================================================
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=12 ./shsgc_openmp > perf_out
@@ -56,6 +56,24 @@ rm perf_out
 echo '============> Running MPI+OpenCL on GPU'
 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./shsgc_mpi_opencl OPS_CL_DEVICE=1 OPS_BLOCK_SIZE_X=32 OPS_BLOCK_SIZE_Y=4 > perf_out
 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./shsgc_mpi_opencl OPS_CL_DEVICE=1 OPS_BLOCK_SIZE_X=32 OPS_BLOCK_SIZE_Y=4 > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+
+cd -
+source ../rubly_pgi_source
+make clean
+make
+cd -
+make shsgc_openacc
+make shsgc_mpi_openacc
+echo '============> Running OpenACC'
+./shsgc_openacc OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+echo '============> Running MPI+OpenACC'
+$MPI_INSTALL_PATH/bin/mpirun -np 2 ./shsgc_mpi_openacc OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "RMS =" perf_out
 grep "Total Wall time" perf_out
 rm perf_out
--- apps/fortran/shsgc/test.sh
+++ apps/fortran/shsgc/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#cd ../../ops/
-#source ./source_intel
-#make
-#cd -
-#make
+cd ../../../ops/fortran
+source ../rubly_intel_source
+make
+cd -
+make
 #============================ Test SHSGC ==========================================================
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=12 ./shsgc_openmp > perf_out
@@ -33,6 +33,25 @@ grep "Total Wall time" perf_out
 rm perf_out
 echo '============> Running MPI+CUDA with GPU-Direct'
 MV2_USE_CUDA=1 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./shsgc_mpi_cuda -gpudirect OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+
+
+cd -
+source ../rubly_pgi_source
+make clean
+make
+cd -
+make shsgc_openacc
+make shsgc_mpi_openacc
+echo '============> Running OpenACC'
+./shsgc_openacc OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+echo '============> Running MPI+OpenACC'
+$MPI_INSTALL_PATH/bin/mpirun -np 2 ./shsgc_mpi_openacc OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "RMS =" perf_out
 grep "Total Wall time" perf_out
 rm perf_out
