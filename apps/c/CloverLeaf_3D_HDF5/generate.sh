4be4f44d394c94cf596f5c2aa319ccb28b109100
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2015-09-09 11:31:44 +0100

    Addign new test application CLoverLeaf_3D_HDF5 to test out HDF5 file I/O functionality

---
+++ apps/c/CloverLeaf_3D_HDF5/generate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+../../../translator/python/c/ops.py clover_leaf.cpp revert.cpp reset_field.cpp ideal_gas.cpp PdV.cpp accelerate.cpp advec_cell.cpp accelerate.cpp advec_mom.cpp calc_dt.cpp field_summary.cpp flux_calc.cpp viscosity.cpp initialise_chunk.cpp generate.cpp update_halo.cpp
