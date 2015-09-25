01bdafc31c998cd5455183a4b8ca4d2dbccee0b1
Istvan Reguly <regulyistvan@gmail.com>
2015-09-25 16:54:30 +0200

    Some runtime checks

--- ops/c/src/externlib/ops_checkpointing.c
+++ ops/c/src/externlib/ops_checkpointing.c
@@ -879,6 +879,7 @@ void ops_checkpointing_save_control(hid_t file_out) {
 }
 
 void ops_checkpointing_manual_datlist(int ndats, ops_dat *datlist) {
+  if (!OPS_enable_checkpointing) return;
 //  if (!((ops_checkpointing_options & OPS_CHECKPOINT_MANUAL_DATLIST) && !(ops_checkpointing_options & OPS_CHECKPOINT_FASTFW || ops_checkpointing_options & OPS_CHECKPOINT_MANUAL))) {
 //    if (OPS_diags>1) ops_printf("Warning: ops_checkpointing_manual_datlist called, but checkpointing options do not match\n");
 //    return;
@@ -947,6 +948,7 @@ void ops_checkpointing_manual_datlist(int ndats, ops_dat *datlist) {
 }
 
 bool ops_checkpointing_fastfw(int nbytes, char *payload) {
+  if (!OPS_enable_checkpointing) return false;
 //  if (!((ops_checkpointing_options & OPS_CHECKPOINT_FASTFW) && !(ops_checkpointing_options & OPS_CHECKPOINT_MANUAL_DATLIST || ops_checkpointing_options & OPS_CHECKPOINT_MANUAL))) {
 //    if (OPS_diags>1) ops_printf("Warning: ops_checkpointing_fastfw called, but checkpointing options do not match\n");
 //    return false;
@@ -980,6 +982,7 @@ bool ops_checkpointing_fastfw(int nbytes, char *payload) {
 }
 
 bool ops_checkpointing_manual_datlist_fastfw(int ndats, ops_dat *datlist, int nbytes, char *payload) {
+  if (!OPS_enable_checkpointing) return false;
 //  if (!((ops_checkpointing_options & OPS_CHECKPOINT_FASTFW && ops_checkpointing_options & OPS_CHECKPOINT_MANUAL_DATLIST) && !(ops_checkpointing_options & OPS_CHECKPOINT_MANUAL))) {
 //    if (OPS_diags>1) ops_printf("Warning: ops_checkpointing_manual_datlist_fastfw called, but checkpointing options do not match\n");
 //    return false;
@@ -998,6 +1001,7 @@ bool ops_checkpointing_manual_datlist_fastfw(int ndats, ops_dat *datlist, int nb
 }
 
 bool ops_checkpointing_manual_datlist_fastfw_trigger(int ndats, ops_dat *datlist, int nbytes, char *payload) {
+  if (!OPS_enable_checkpointing) return false;
   if (!(ops_checkpointing_options & OPS_CHECKPOINT_FASTFW && ops_checkpointing_options & OPS_CHECKPOINT_MANUAL_DATLIST && ops_checkpointing_options & OPS_CHECKPOINT_MANUAL)) {
     if (OPS_diags>1) ops_printf("Warning: ops_checkpointing_manual_datlist_fastfw_trigger called, but checkpointing options do not match\n");
     return false;
