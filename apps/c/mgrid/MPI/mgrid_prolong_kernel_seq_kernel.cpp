eb37929c0e287a8b0503a4958ea2f28ecafa9fef
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2015-12-03 17:47:45 +0000

    Updating code generator for mgrid

--- apps/c/mgrid/MPI/mgrid_prolong_kernel_seq_kernel.cpp
+++ apps/c/mgrid/MPI/mgrid_prolong_kernel_seq_kernel.cpp
@@ -35,10 +35,12 @@ void ops_par_loop_mgrid_prolong_kernel(char const *name, ops_block block, int di
   }
 
   //compute locally allocated range for the sub-block
-
   int start[2];
   int end[2];
 
+  #ifdef OPS_MPI
+  sub_block_list sb = OPS_sub_block_list[block->index];
+  #endif
   #ifdef OPS_DEBUG
   ops_register_args(args, "mgrid_prolong_kernel");
   #endif
@@ -46,8 +48,9 @@ void ops_par_loop_mgrid_prolong_kernel(char const *name, ops_block block, int di
 
   int arg_idx[2];
   #ifdef OPS_MPI
-  sub_block_list sb = OPS_sub_block_list[block->index];
   sub_dat *sd0 = OPS_sub_dat_list[args[0].dat->index];
+  sub_dat *sd1 = OPS_sub_dat_list[args[1].dat->index];
+  sub_dat *sd2 = OPS_sub_dat_list[args[2].dat->index];
   arg_idx[0] = sb->decomp_disp[0]+start[0];
   arg_idx[1] = sb->decomp_disp[1]+start[1];
   #else //OPS_MPI
@@ -78,19 +81,15 @@ void ops_par_loop_mgrid_prolong_kernel(char const *name, ops_block block, int di
     d_size_0[n] = args[0].dat->d_m[n] + sd0->decomp_size[n] - args[0].dat->d_p[n];
     start_0[n] = global_idx[n]/stride_0[n] - sd0->decomp_disp[n] + args[0].dat->d_m[n];
     end_0[n] = start_0[n] + d_size_0[n];
-    printf("start_0[n] = %d, end_0[n] = %d, d_size_0[n] = %d \n",start_0[n],end_0[n],d_size_0[n]);
   }
-  #else //OPS_MPI
+  #else
   for ( int n=0; n<2; n++ ){
     stride_0[n] = args[0].stencil->mgrid_stride[n];
     d_size_0[n] = args[0].dat->d_m[n] + args[0].dat->size[n] - args[0].dat->d_p[n];
-    start_0[n] = global_idx[n]/stride_0[n] ;//+ args[0].dat->d_m[n];
+    start_0[n] = global_idx[n]/stride_0[n];
     end_0[n] = start_0[n] + d_size_0[n];
-    printf("start_0[n] = %d, end_0[n] = %d, d_size_0[n] = %d \n",start_0[n],end_0[n],d_size_0[n]);
   }
   #endif
-
-
   offs[0][0] = args[0].stencil->stride[0]*1;  //unit step in x dimension
   offs[0][1] = off2D(1, &start_0[0],
       &end_0[0],args[0].dat->size, args[0].stencil->stride) - offs[0][0];
