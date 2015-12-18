64e65bfcf753a8d83717001e43c3ade2a4da4fa4
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2015-12-18 12:50:51 +0000

    Testing MPI with stride 2

--- apps/c/mgrid/mgrid.cpp
+++ apps/c/mgrid/mgrid.cpp
@@ -78,15 +78,15 @@ int main(int argc, char **argv)
   //declare restrict and prolong stencils
   ops_stencil S2D_RESTRICT_00 = ops_decl_restrict_stencil( 2, 1, s2D_00, stride1, "RESTRICT_00");
   //ops_stencil S2D_RESTRICT_00 = ops_decl_restrict_stencil( 2, 1, s2D_00, stride2, "RESTRICT_00");
-  //ops_stencil S2D_PROLONG_00 = ops_decl_prolong_stencil( 2, 1, s2D_00, stride1, "PROLONG_00");
-  ops_stencil S2D_PROLONG_00 = ops_decl_prolong_stencil( 2, 1, s2D_00, stride2, "PROLONG_00");
+  ops_stencil S2D_PROLONG_00 = ops_decl_prolong_stencil( 2, 1, s2D_00, stride1, "PROLONG_00");
+  //ops_stencil S2D_PROLONG_00 = ops_decl_prolong_stencil( 2, 1, s2D_00, stride2, "PROLONG_00");
 
   int base[2] = {0,0};
   double* temp = NULL;
 
   ops_dat data0 = ops_decl_dat(grid0, 1, size0, base, d_m, d_p, stride0 , temp, "double", "data0");
-  //ops_dat data1 = ops_decl_dat(grid0, 1, size1, base, d_m, d_p, stride1 , temp, "double", "data1");
-  ops_dat data2 = ops_decl_dat(grid0, 1, size2, base, d_m, d_p, stride2 , temp, "double", "data2");
+  ops_dat data1 = ops_decl_dat(grid0, 1, size1, base, d_m, d_p, stride1 , temp, "double", "data1");
+  //ops_dat data2 = ops_decl_dat(grid0, 1, size2, base, d_m, d_p, stride2 , temp, "double", "data2");
 
   //ops_dat data3 = ops_decl_dat(grid0, 1, size3, base, d_m, d_p, stride1 , temp, "double", "data3");
   //ops_dat data4 = ops_decl_dat(grid0, 1, size3, base, d_m, d_p, stride2 , temp, "double", "data3");
@@ -103,22 +103,22 @@ int main(int argc, char **argv)
   int iter_range_small[] = {0,6,0,6};
   int iter_range_tiny[] = {0,4,0,4};
 
-  /*ops_par_loop(mgrid_populate_kernel_1, "mgrid_populate_kernel_1", grid0, 2, iter_range_small,
-               ops_arg_dat(data1, S2D_00, "double", OPS_WRITE),
-               ops_arg_idx());*/
-
-  ops_par_loop(mgrid_populate_kernel_2, "mgrid_populate_kernel_2", grid0, 2, iter_range_tiny,
-               ops_arg_dat(data2, 1, S2D_00, "double", OPS_WRITE),
+  ops_par_loop(mgrid_populate_kernel_1, "mgrid_populate_kernel_1", grid0, 2, iter_range_small,
+               ops_arg_dat(data1, 1, S2D_00, "double", OPS_WRITE),
                ops_arg_idx());
 
+  /*ops_par_loop(mgrid_populate_kernel_2, "mgrid_populate_kernel_2", grid0, 2, iter_range_tiny,
+               ops_arg_dat(data2, 1, S2D_00, "double", OPS_WRITE),
+               ops_arg_idx());*/
+
   ops_par_loop(mgrid_prolong_kernel, "mgrid_prolong_kernel", grid0, 2, iter_range,
-               //ops_arg_dat(data1, S2D_PROLONG_00, "double", OPS_READ),
-               ops_arg_dat(data2, 1, S2D_PROLONG_00, "double", OPS_READ),
+               //ops_arg_dat(data2, 1, S2D_PROLONG_00, "double", OPS_READ),
+               ops_arg_dat(data1, 1, S2D_PROLONG_00, "double", OPS_READ),
                ops_arg_dat(data0, 1, S2D_00, "double", OPS_WRITE),
                ops_arg_idx());
 
-  //ops_print_dat_to_txtfile(data1, "data.txt");
-  ops_print_dat_to_txtfile(data2, "data.txt");
+  ops_print_dat_to_txtfile(data1, "data.txt");
+  //ops_print_dat_to_txtfile(data2, "data.txt");
   ops_print_dat_to_txtfile(data0, "data.txt");
 
   /*ops_par_loop(mgrid_restrict_kernel, "mgrid_restrict_kernel", grid0, 2, iter_range_small,
