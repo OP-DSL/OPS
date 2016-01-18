0f73d781af95f1057c7f4e09dd8bb59e7fb25853
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-01-18 10:27:56 +0000

    Getting the fortran version of possion working with PGI compilers

--- apps/fortran/poisson/test.sh
+++ apps/fortran/poisson/test.sh
@@ -5,7 +5,7 @@ source ../source_intel
 make
 cd -
 make
-echo '============================ Test Poisson =========================================================='
+echo '============================ Test Poisson Intel Compilers=========================================================='
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=20 ./poisson_openmp > perf_out
 grep "Total error:" perf_out
@@ -23,10 +23,26 @@ grep "Total Wall time" perf_out
 rm perf_out
 
 cd $OPS_INSTALL_PATH/fortran
-source ../source_pgi
+source ../source_pgi_15.1
 make
 cd -
 make
+echo '============================ Test Poisson PGI Compilers=========================================================='
+echo '============> Running OpenMP'
+KMP_AFFINITY=compact OMP_NUM_THREADS=20 ./poisson_openmp > perf_out
+grep "Total error:" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+echo '============> Running MPI+OpenMP'
+export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./poisson_mpi_openmp > perf_out
+grep "Total error:" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+echo '============> Running MPI'
+$MPI_INSTALL_PATH/bin/mpirun -np 20 ./poisson_mpi > perf_out
+grep "Total error:" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
 echo '============> Running CUDA'
 ./poisson_cuda OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Total error:" perf_out
--- apps/fortran/shsgc/test.sh
+++ apps/fortran/shsgc/test.sh
@@ -7,7 +7,7 @@ cd -
 make
 
 
-#============================ Test SHSGC ==========================================================
+#============================ Test SHSGC Intel Compilers ==========================================================
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=10 ./shsgc_openmp > perf_out
 grep "RMS =" perf_out
@@ -26,11 +26,27 @@ rm perf_out
 
 
 cd $OPS_INSTALL_PATH/fortran
-source ../source_pgi
+source ../source_pgi_15.1
 make
 cd -
 make clean
 make
+#============================ Test SHSGC PGI Compilers ==========================================================
+echo '============> Running OpenMP'
+KMP_AFFINITY=compact OMP_NUM_THREADS=10 ./shsgc_openmp > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+echo '============> Running MPI+OpenMP'
+export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./shsgc_mpi_openmp > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
+echo '============> Running MPI'
+$MPI_INSTALL_PATH/bin/mpirun -np 20 ./shsgc_mpi > perf_out
+grep "RMS =" perf_out
+grep "Total Wall time" perf_out
+rm perf_out
 echo '============> Running CUDA'
 ./shsgc_cuda OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "RMS =" perf_out
