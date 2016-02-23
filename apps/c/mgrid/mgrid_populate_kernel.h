2759487ff40d7fddec3121cc43e1ee182b3d1667
Istvan Reguly <regulyistvan@gmail.com>
2016-02-23 18:58:49 +0100

    Fixed prolongation exchange

--- apps/c/mgrid/mgrid_populate_kernel.h
+++ apps/c/mgrid/mgrid_populate_kernel.h
@@ -9,5 +9,8 @@ void mgrid_populate_kernel_2(double *val, int *idx) {
   val[OPS_ACC0(0,0)] = (double)(idx[0]+4*idx[1]);
 }
 
+void mgrid_populate_kernel_3(double *val, int *idx) {
+  val[OPS_ACC0(0,0)] = (double)(idx[0]+24*idx[1]);
+}
 
 #endif //MGRID_POPULATE_KERNELS_H
