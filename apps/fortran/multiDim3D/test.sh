776d97faf3657258ce536bb5adede675683cae83
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-04-04 16:39:43 +0100

    Updating tests

--- apps/fortran/multiDim3D/test.sh
+++ apps/fortran/multiDim3D/test.sh
@@ -5,21 +5,21 @@ source ../source_intel
 make
 cd -
 make
-echo '============================ Test Poisson Intel Compilers=========================================================='
+echo '============================ Test MultiDim3D Intel Compilers=========================================================='
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=20 ./multidim_openmp > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+OpenMP'
 export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./multidim_mpi_openmp > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI'
 $MPI_INSTALL_PATH/bin/mpirun -np 20 ./multidim_mpi > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 
 
@@ -28,34 +28,34 @@ source ../source_pgi_15.10
 make
 cd -
 make
-echo '============================ Test Poisson PGI Compilers=========================================================='
+echo '============================ Test MultiDim3D PGI Compilers=========================================================='
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=20 ./multidim_openmp > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+OpenMP'
 export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./multidim_mpi_openmp > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI'
 $MPI_INSTALL_PATH/bin/mpirun -np 20 ./multidim_mpi > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running CUDA'
 ./multidim_cuda OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+CUDA'
 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./multidim_mpi_cuda OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+CUDA with GPU-Direct'
 MV2_USE_CUDA=1 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./multidim_mpi_cuda -gpudirect OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Reduction result" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
--- apps/fortran/poisson/test.sh
+++ apps/fortran/poisson/test.sh
@@ -9,17 +9,17 @@ echo '============================ Test Poisson Intel Compilers=================
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=20 ./poisson_openmp > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+OpenMP'
 export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./poisson_mpi_openmp > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI'
 $MPI_INSTALL_PATH/bin/mpirun -np 20 ./poisson_mpi > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 
 cd $OPS_INSTALL_PATH/fortran
@@ -31,36 +31,36 @@ echo '============================ Test Poisson PGI Compilers===================
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=20 ./poisson_openmp > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+OpenMP'
 export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./poisson_mpi_openmp > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI'
 $MPI_INSTALL_PATH/bin/mpirun -np 20 ./poisson_mpi > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running CUDA'
 ./poisson_cuda OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+CUDA'
 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./poisson_mpi_cuda OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 echo '============> Running MPI+CUDA with GPU-Direct'
 MV2_USE_CUDA=1 $MPI_INSTALL_PATH/bin/mpirun -np 2 ./poisson_mpi_cuda -gpudirect OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
 make poisson_openacc
 echo '============> Running OpenACC'
 ./poisson_openacc OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
 grep "Total error:" perf_out
-grep "Total Wall time" perf_out
+grep "Max total runtime" perf_out
 rm perf_out
