3ad2ea5b87d89852789c5926ec7fd0b77e5fd755
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-05-23 15:02:10 +0100

    Adding adi_openmp - still skiwer than the original adi_cpu

---
+++ ops/c/include/ops_tridiag.h
@@ -0,0 +1,53 @@
+/*
+* Open source copyright declaration based on BSD open source template:
+* http://www.opensource.org/licenses/bsd-license.php
+*
+* This file is part of the OPS distribution.
+*
+* Copyright (c) 2013, Mike Giles and others. Please see the AUTHORS file in
+* the main source directory for a full list of copyright holders.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* * Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer.
+* * Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution.
+* * The name of Mike Giles may not be used to endorse or promote products
+* derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY Mike Giles ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Mike Giles BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/** @brief ops tridiagonal API header file
+  * @author Gihan Mudalige
+  * @details This header file should be included by all C++
+  * OPS applications using the OPS Tridiagonal API
+  */
+
+#ifndef __OPS_TRIDIAG_H
+#define __OPS_TRIDIAG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ops_tridMultiDimBatch( int ndim, int solvedim, int* dims,
+  ops_dat a, ops_dat b, ops_dat c, ops_dat d, ops_dat u );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
