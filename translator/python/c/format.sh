6d300ce88ae8d3b8484053a889f97a306fa93f2c
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-07-15 15:51:46 +0100

    Adding formatting scripts

---
+++ translator/python/c/format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Uses clang-format to format code to conform to the OPS coding guidelines
+# ... currently only applies to files within the current directory
+# also only format the files producded by the code generator (i.e. files with kernel in their name)
+
+#for file in ./*.cu ./*.cpp ./*.h ./*.hpp; do clang-format "$file" > "$file"_temp; mv "$file"_temp "$file"; done
+
+ls ./*kernel* 2> /dev/null
+if [ $? -eq 0 ]
+then
+  for file in ./*kernel*; do clang-format -i "$file"; done
+fi
+ls ./*_ops.cpp 2> /dev/null
+if [ $? -eq 0 ]
+then
+  for file in ./*_ops.cpp; do clang-format -i "$file"; done
+fi
