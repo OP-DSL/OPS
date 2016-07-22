5e7ad876ac12b2228736a1085d7b761a1b110d72
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-07-22 10:29:43 +0100

    Code formatting mblock and mb_shscg

--- apps/c/mb_shsgc/Max_datatransfer/test.sh
+++ apps/c/mb_shsgc/Max_datatransfer/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -e
 cd ../../../../ops/c
-source ../source_intel
+source ../../scripts/source_intel
 make
 cd -
 ../../../../translator/python/c/ops.py shsgc.cpp
@@ -122,7 +122,11 @@ rc=$?; if [[ $rc != 0 ]]; then echo "TEST FAILED";exit $rc; fi
 rm perf_out
 
 cd -
-source ../source_pgi_15.10
+source ../../scripts/source_pgi_15.10
+
+#tobe removed
+export LD_LIBRARY_PATH+=:/opt/compilers/intel/intelPS-2015/composer_xe_2015.2.164/compiler/lib/intel64/
+
 make clean
 make
 cd -
@@ -239,14 +243,6 @@ grep "Post shock Error is" perf_out
 grep "Total Wall time" perf_out
 grep -e "acceptable" -e "correct"  perf_out
 rc=$?; if [[ $rc != 0 ]]; then echo "TEST FAILED";exit $rc; fi
-
-cd -
-source ../source_pgi_15.10
-make clean
-make
-cd -
-make shsgc_openacc
-make shsgc_mpi_openacc
 
 echo '============> Running OpenACC'
 ./shsgc_openacc OPS_BLOCK_SIZE_X=64 OPS_BLOCK_SIZE_Y=4 > perf_out
