1455c2acce31cdb567b5c848eb2b75e74db06d43
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-08-18 08:51:31 +0100

    Adding the full MPI tridiagonal x-dim algorithm

--- apps/c/adi/adi.cpp
+++ apps/c/adi/adi.cpp
@@ -171,20 +171,19 @@ int main(int argc, char **argv)
   ops_timers(&ct3, &et3);
   ops_printf("Elapsed preproc (sec): %lf (s)\n",et3-et2);
 
-
-  double rms_value = 0.0;
-  ops_par_loop(rms_kernel, "rms_kernel", heat3D, 3, iter_range,
-               ops_arg_dat(h_du, 1, S3D_000, "double", OPS_READ),
-               ops_arg_reduce(rms, 1, "double", OPS_INC));
-
   /**----------------- perform tri-diagonal solves in x-direction ---------------**/
   ops_timers(&ct2, &et2);
   ops_tridMultiDimBatch( 3, 0 , size, h_ax, h_bx, h_cx, h_du, h_u, opts);
   ops_timers(&ct3, &et3);
   ops_printf("Elapsed trid_x (sec): %lf (s)\n",et3-et2);
 
+  double rms_value = 0.0;
+  ops_par_loop(rms_kernel, "rms_kernel", heat3D, 3, iter_range,
+               ops_arg_dat(h_u, 1, S3D_000, "double", OPS_READ),
+               ops_arg_reduce(rms, 1, "double", OPS_INC));
+
   ops_reduction_result(rms,&rms_value);
-  ops_printf("Value %lg\n",rms_value);
+  ops_printf("h_u Value %lg\n", rms_value);
 
   exit(-2);
 
