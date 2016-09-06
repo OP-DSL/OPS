406b770b46620e9df98fed3b45e118b06b4dc9af
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-09-06 12:09:48 +0100

    Intermediate values correct for mpi backend x_dimension

--- apps/c/adi/adi_ops.cpp
+++ apps/c/adi/adi_ops.cpp
@@ -148,32 +148,6 @@ int main(int argc, char **argv) {
     ops_printf("Elapsed preproc (sec): %lf (s)\n", et3 - et2);
 
     double rms_value = 0.0;
-    ops_par_loop_rms_kernel("rms_kernel", heat3D, 3, iter_range,
-                            ops_arg_dat(h_ax, 1, S3D_000, "double", OPS_READ),
-                            ops_arg_reduce(rms, 1, "double", OPS_INC));
-    ops_reduction_result(rms, &rms_value);
-    ops_printf("h_ax Value %lg\n", rms_value);
-
-    rms_value = 0.0;
-    ops_par_loop_rms_kernel("rms_kernel", heat3D, 3, iter_range,
-                            ops_arg_dat(h_bx, 1, S3D_000, "double", OPS_READ),
-                            ops_arg_reduce(rms, 1, "double", OPS_INC));
-    ops_reduction_result(rms, &rms_value);
-    ops_printf("h_bx Value %lg\n", rms_value);
-
-    rms_value = 0.0;
-    ops_par_loop_rms_kernel("rms_kernel", heat3D, 3, iter_range,
-                            ops_arg_dat(h_cx, 1, S3D_000, "double", OPS_READ),
-                            ops_arg_reduce(rms, 1, "double", OPS_INC));
-    ops_reduction_result(rms, &rms_value);
-    ops_printf("h_cx Value %lg\n", rms_value);
-
-    rms_value = 0.0;
-    ops_par_loop_rms_kernel("rms_kernel", heat3D, 3, iter_range,
-                            ops_arg_dat(h_du, 1, S3D_000, "double", OPS_READ),
-                            ops_arg_reduce(rms, 1, "double", OPS_INC));
-    ops_reduction_result(rms, &rms_value);
-    ops_printf("h_du Value %lg\n", rms_value);
 
     rms_value = 0.0;
     ops_par_loop_rms_kernel("rms_kernel", heat3D, 3, iter_range,
@@ -181,12 +155,13 @@ int main(int argc, char **argv) {
                             ops_arg_reduce(rms, 1, "double", OPS_INC));
     ops_reduction_result(rms, &rms_value);
     ops_printf("h_u Value %lg\n", rms_value);
-    exit(-2);
 
     ops_timers(&ct2, &et2);
     ops_tridMultiDimBatch(3, 0, size, h_ax, h_bx, h_cx, h_du, h_u, opts);
     ops_timers(&ct3, &et3);
     ops_printf("Elapsed trid_x (sec): %lf (s)\n", et3 - et2);
+
+    exit(-2);
 
     ops_timers(&ct2, &et2);
     ops_tridMultiDimBatch(3, 1, size, h_ay, h_by, h_cy, h_du, h_u, opts);
