ca62f579108639f1180360cda0b20a730c86245d
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2016-09-30 11:38:09 +0100

    Code formating for Poisson

--- apps/c/poisson/CUDA/poisson_kernel_stencil_cuda_kernel.cu
+++ apps/c/poisson/CUDA/poisson_kernel_stencil_cuda_kernel.cu
@@ -11,171 +11,174 @@ int ydim1_poisson_kernel_stencil_h = -1;
 #undef OPS_ACC0
 #undef OPS_ACC1
 
+#define OPS_ACC0(x, y) (x + xdim0_poisson_kernel_stencil * (y))
+#define OPS_ACC1(x, y) (x + xdim1_poisson_kernel_stencil * (y))
 
-#define OPS_ACC0(x,y) (x+xdim0_poisson_kernel_stencil*(y))
-#define OPS_ACC1(x,y) (x+xdim1_poisson_kernel_stencil*(y))
-
-//user function
+// user function
 __device__
 
-void poisson_kernel_stencil(const double *u, double *u2) {
-  u2[OPS_ACC1(0,0)] = ((u[OPS_ACC0(-1,0)]-2.0f*u[OPS_ACC0(0,0)]+u[OPS_ACC0(1,0)])*0.125f
-                     + (u[OPS_ACC0(0,-1)]-2.0f*u[OPS_ACC0(0,0)]+u[OPS_ACC0(0,1)])*0.125f
-                     + u[OPS_ACC0(0,0)]);
+    void
+    poisson_kernel_stencil(const double *u, double *u2) {
+  u2[OPS_ACC1(0, 0)] =
+      ((u[OPS_ACC0(-1, 0)] - 2.0f * u[OPS_ACC0(0, 0)] + u[OPS_ACC0(1, 0)]) *
+           0.125f +
+       (u[OPS_ACC0(0, -1)] - 2.0f * u[OPS_ACC0(0, 0)] + u[OPS_ACC0(0, 1)]) *
+           0.125f +
+       u[OPS_ACC0(0, 0)]);
 }
-
-
 
 #undef OPS_ACC0
 #undef OPS_ACC1
 
-
-__global__ void ops_poisson_kernel_stencil(
-const double* __restrict arg0,
-double* __restrict arg1,
-int size0,
-int size1 ){
-
+__global__ void ops_poisson_kernel_stencil(const double *__restrict arg0,
+                                           double *__restrict arg1, int size0,
+                                           int size1) {
 
   int idx_y = blockDim.y * blockIdx.y + threadIdx.y;
   int idx_x = blockDim.x * blockIdx.x + threadIdx.x;
 
-  arg0 += idx_x * 1*1 + idx_y * 1*1 * xdim0_poisson_kernel_stencil;
-  arg1 += idx_x * 1*1 + idx_y * 1*1 * xdim1_poisson_kernel_stencil;
+  arg0 += idx_x * 1 * 1 + idx_y * 1 * 1 * xdim0_poisson_kernel_stencil;
+  arg1 += idx_x * 1 * 1 + idx_y * 1 * 1 * xdim1_poisson_kernel_stencil;
 
   if (idx_x < size0 && idx_y < size1) {
     poisson_kernel_stencil(arg0, arg1);
   }
-
 }
 
 // host stub function
-void ops_par_loop_poisson_kernel_stencil(char const *name, ops_block block, int dim, int* range,
- ops_arg arg0, ops_arg arg1) {
+void ops_par_loop_poisson_kernel_stencil(char const *name, ops_block block,
+                                         int dim, int *range, ops_arg arg0,
+                                         ops_arg arg1) {
 
-  //Timing
-  double t1,t2,c1,c2;
+  // Timing
+  double t1, t2, c1, c2;
 
-  ops_arg args[2] = { arg0, arg1};
+  ops_arg args[2] = {arg0, arg1};
 
-
-  #ifdef CHECKPOINTING
-  if (!ops_checkpointing_before(args,2,range,3)) return;
-  #endif
+#ifdef CHECKPOINTING
+  if (!ops_checkpointing_before(args, 2, range, 3))
+    return;
+#endif
 
   if (OPS_diags > 1) {
-    ops_timing_realloc(3,"poisson_kernel_stencil");
+    ops_timing_realloc(3, "poisson_kernel_stencil");
     OPS_kernels[3].count++;
-    ops_timers_core(&c1,&t1);
+    ops_timers_core(&c1, &t1);
   }
 
-  //compute locally allocated range for the sub-block
+  // compute locally allocated range for the sub-block
   int start[2];
   int end[2];
-  #ifdef OPS_MPI
+#ifdef OPS_MPI
   sub_block_list sb = OPS_sub_block_list[block->index];
-  if (!sb->owned) return;
-  for ( int n=0; n<2; n++ ){
-    start[n] = sb->decomp_disp[n];end[n] = sb->decomp_disp[n]+sb->decomp_size[n];
-    if (start[n] >= range[2*n]) {
+  if (!sb->owned)
+    return;
+  for (int n = 0; n < 2; n++) {
+    start[n] = sb->decomp_disp[n];
+    end[n] = sb->decomp_disp[n] + sb->decomp_size[n];
+    if (start[n] >= range[2 * n]) {
       start[n] = 0;
+    } else {
+      start[n] = range[2 * n] - start[n];
     }
-    else {
-      start[n] = range[2*n] - start[n];
-    }
-    if (sb->id_m[n]==MPI_PROC_NULL && range[2*n] < 0) start[n] = range[2*n];
-    if (end[n] >= range[2*n+1]) {
-      end[n] = range[2*n+1] - sb->decomp_disp[n];
-    }
-    else {
+    if (sb->id_m[n] == MPI_PROC_NULL && range[2 * n] < 0)
+      start[n] = range[2 * n];
+    if (end[n] >= range[2 * n + 1]) {
+      end[n] = range[2 * n + 1] - sb->decomp_disp[n];
+    } else {
       end[n] = sb->decomp_size[n];
     }
-    if (sb->id_p[n]==MPI_PROC_NULL && (range[2*n+1] > sb->decomp_disp[n]+sb->decomp_size[n]))
-      end[n] += (range[2*n+1]-sb->decomp_disp[n]-sb->decomp_size[n]);
+    if (sb->id_p[n] == MPI_PROC_NULL &&
+        (range[2 * n + 1] > sb->decomp_disp[n] + sb->decomp_size[n]))
+      end[n] += (range[2 * n + 1] - sb->decomp_disp[n] - sb->decomp_size[n]);
   }
-  #else
-  for ( int n=0; n<2; n++ ){
-    start[n] = range[2*n];end[n] = range[2*n+1];
+#else
+  for (int n = 0; n < 2; n++) {
+    start[n] = range[2 * n];
+    end[n] = range[2 * n + 1];
   }
-  #endif
+#endif
 
-  int x_size = MAX(0,end[0]-start[0]);
-  int y_size = MAX(0,end[1]-start[1]);
+  int x_size = MAX(0, end[0] - start[0]);
+  int y_size = MAX(0, end[1] - start[1]);
 
   int xdim0 = args[0].dat->size[0];
   int xdim1 = args[1].dat->size[0];
 
-  if (xdim0 != xdim0_poisson_kernel_stencil_h || xdim1 != xdim1_poisson_kernel_stencil_h) {
-    cudaMemcpyToSymbol( xdim0_poisson_kernel_stencil, &xdim0, sizeof(int) );
+  if (xdim0 != xdim0_poisson_kernel_stencil_h ||
+      xdim1 != xdim1_poisson_kernel_stencil_h) {
+    cudaMemcpyToSymbol(xdim0_poisson_kernel_stencil, &xdim0, sizeof(int));
     xdim0_poisson_kernel_stencil_h = xdim0;
-    cudaMemcpyToSymbol( xdim1_poisson_kernel_stencil, &xdim1, sizeof(int) );
+    cudaMemcpyToSymbol(xdim1_poisson_kernel_stencil, &xdim1, sizeof(int));
     xdim1_poisson_kernel_stencil_h = xdim1;
   }
 
-
-
-  dim3 grid( (x_size-1)/OPS_block_size_x+ 1, (y_size-1)/OPS_block_size_y + 1, 1);
-  dim3 tblock(OPS_block_size_x,OPS_block_size_y,1);
-
-
+  dim3 grid((x_size - 1) / OPS_block_size_x + 1,
+            (y_size - 1) / OPS_block_size_y + 1, 1);
+  dim3 tblock(OPS_block_size_x, OPS_block_size_y, 1);
 
   int dat0 = args[0].dat->elem_size;
   int dat1 = args[1].dat->elem_size;
 
   char *p_a[2];
 
-  //set up initial pointers
+  // set up initial pointers
   int d_m[OPS_MAX_DIM];
-  #ifdef OPS_MPI
-  for (int d = 0; d < dim; d++) d_m[d] = args[0].dat->d_m[d] + OPS_sub_dat_list[args[0].dat->index]->d_im[d];
-  #else
-  for (int d = 0; d < dim; d++) d_m[d] = args[0].dat->d_m[d];
-  #endif
-  int base0 = dat0 * 1 *
-  (start[0] * args[0].stencil->stride[0] - args[0].dat->base[0] - d_m[0]);
-  base0 = base0+ dat0 *
-    args[0].dat->size[0] *
-    (start[1] * args[0].stencil->stride[1] - args[0].dat->base[1] - d_m[1]);
+#ifdef OPS_MPI
+  for (int d = 0; d < dim; d++)
+    d_m[d] =
+        args[0].dat->d_m[d] + OPS_sub_dat_list[args[0].dat->index]->d_im[d];
+#else
+  for (int d = 0; d < dim; d++)
+    d_m[d] = args[0].dat->d_m[d];
+#endif
+  int base0 = dat0 * 1 * (start[0] * args[0].stencil->stride[0] -
+                          args[0].dat->base[0] - d_m[0]);
+  base0 = base0 +
+          dat0 * args[0].dat->size[0] * (start[1] * args[0].stencil->stride[1] -
+                                         args[0].dat->base[1] - d_m[1]);
   p_a[0] = (char *)args[0].data_d + base0;
 
-  #ifdef OPS_MPI
-  for (int d = 0; d < dim; d++) d_m[d] = args[1].dat->d_m[d] + OPS_sub_dat_list[args[1].dat->index]->d_im[d];
-  #else
-  for (int d = 0; d < dim; d++) d_m[d] = args[1].dat->d_m[d];
-  #endif
-  int base1 = dat1 * 1 *
-  (start[0] * args[1].stencil->stride[0] - args[1].dat->base[0] - d_m[0]);
-  base1 = base1+ dat1 *
-    args[1].dat->size[0] *
-    (start[1] * args[1].stencil->stride[1] - args[1].dat->base[1] - d_m[1]);
+#ifdef OPS_MPI
+  for (int d = 0; d < dim; d++)
+    d_m[d] =
+        args[1].dat->d_m[d] + OPS_sub_dat_list[args[1].dat->index]->d_im[d];
+#else
+  for (int d = 0; d < dim; d++)
+    d_m[d] = args[1].dat->d_m[d];
+#endif
+  int base1 = dat1 * 1 * (start[0] * args[1].stencil->stride[0] -
+                          args[1].dat->base[0] - d_m[0]);
+  base1 = base1 +
+          dat1 * args[1].dat->size[0] * (start[1] * args[1].stencil->stride[1] -
+                                         args[1].dat->base[1] - d_m[1]);
   p_a[1] = (char *)args[1].data_d + base1;
 
-
   ops_H_D_exchanges_device(args, 2);
-  ops_halo_exchanges(args,2,range);
+  ops_halo_exchanges(args, 2, range);
 
   if (OPS_diags > 1) {
-    ops_timers_core(&c2,&t2);
-    OPS_kernels[3].mpi_time += t2-t1;
+    ops_timers_core(&c2, &t2);
+    OPS_kernels[3].mpi_time += t2 - t1;
   }
 
+  // call kernel wrapper function, passing in pointers to data
+  ops_poisson_kernel_stencil<<<grid, tblock>>>(
+      (double *)p_a[0], (double *)p_a[1], x_size, y_size);
 
-  //call kernel wrapper function, passing in pointers to data
-  ops_poisson_kernel_stencil<<<grid, tblock >>> (  (double *)p_a[0], (double *)p_a[1],x_size, y_size);
-
-  if (OPS_diags>1) {
+  if (OPS_diags > 1) {
     cutilSafeCall(cudaDeviceSynchronize());
-    ops_timers_core(&c1,&t1);
-    OPS_kernels[3].time += t1-t2;
+    ops_timers_core(&c1, &t1);
+    OPS_kernels[3].time += t1 - t2;
   }
 
   ops_set_dirtybit_device(args, 2);
-  ops_set_halo_dirtybit3(&args[1],range);
+  ops_set_halo_dirtybit3(&args[1], range);
 
   if (OPS_diags > 1) {
-    //Update kernel record
-    ops_timers_core(&c2,&t2);
-    OPS_kernels[3].mpi_time += t2-t1;
+    // Update kernel record
+    ops_timers_core(&c2, &t2);
+    OPS_kernels[3].mpi_time += t2 - t1;
     OPS_kernels[3].transfer += ops_compute_transfer(dim, start, end, &arg0);
     OPS_kernels[3].transfer += ops_compute_transfer(dim, start, end, &arg1);
   }
