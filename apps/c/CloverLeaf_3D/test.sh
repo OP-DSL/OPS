7facf97ed8f53285875c341b526d94c110d74bbf
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2017-01-18 16:03:58 +0000

    Adding tests for MPI+Tiling to CloverLeaf

--- apps/c/CloverLeaf_3D/test.sh
+++ apps/c/CloverLeaf_3D/test.sh
@@ -26,6 +26,14 @@ grep "PASSED" clover.out
 rc=$?; if [[ $rc != 0 ]]; then echo "TEST FAILED";exit $rc; fi
 rm -f clover.out
 
+echo '============> Running MPI_Tiled'
+export OMP_NUM_THREADS=1;$MPI_INSTALL_PATH/bin/mpirun -np 20 ./cloverleaf_mpi_tiled OPS_TILING_MAXDEPTH=6 > perf_out
+grep "Total Wall time" clover.out
+grep -e "step:   2952" -e "step:   2953" -e "step:   2954" -e "step:   2955" clover.out
+grep "PASSED" clover.out
+rc=$?; if [[ $rc != 0 ]]; then echo "TEST FAILED";exit $rc; fi
+rm -f clover.out
+
 
 echo '============> Running MPI+OpenMP'
 export OMP_NUM_THREADS=2;$MPI_INSTALL_PATH/bin/mpirun -np 10 ./cloverleaf_mpi_openmp > perf_out
