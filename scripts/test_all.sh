efb4d71c91e2e93f7538ef1645859d06ab4e547b
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2017-10-18 11:57:54 +0100

    Updating test script

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -9,30 +9,30 @@ cd $OPS_INSTALL_PATH
 echo "************Testing C Applications *****************"
 cd ../apps/c/CloverLeaf/
 cd ../CloverLeaf/
-#./test.sh
+./test.sh
 cd ../CloverLeaf_3D/
-#./test.sh
+./test.sh
 cd ../CloverLeaf_3D_HDF5/
-#./test.sh
+./test.sh
 cd ../TeaLeaf/
-#./test.sh
+./test.sh
 cd ../poisson/
-#./test.sh
+./test.sh
 cd ../multiDim/
-#./test.sh
+./test.sh
 cd ../multiDim3D/
-#./test.sh
+./test.sh
 cd ../shsgc/
-#./test.sh
+./test.sh
 cd ../mb_shsgc/Max_datatransfer
-#./test.sh
+./test.sh
 cd ../../multiDim_HDF5
-#./test.sh
+./test.sh
 
 echo "************Testing Fortran Applications *****************"
 cd $OPS_INSTALL_PATH
 cd ../apps/fortran/shsgc
-#./test.sh
+./test.sh
 cd ../poisson
 ./test.sh
 cd ../multiDim
