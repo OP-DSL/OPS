850cd968807cff57964d9ceee63d12eb93b5ba2c
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2018-07-11 12:36:25 +0100

    Passes shscg

--- apps/c/shsgc/test.sh
+++ apps/c/shsgc/test.sh
@@ -5,7 +5,8 @@ source ../../scripts/source_intel
 make
 cd -
 ../../../ops_translator/c/ops.py shsgc.cpp
-make
+make clean
+make 
 #============================ Test SHSGC With Intel Compilers==========================================================
 echo '============> Running OpenMP'
 KMP_AFFINITY=compact OMP_NUM_THREADS=10 ./shsgc_openmp > perf_out
@@ -99,6 +100,7 @@ rc=$?; if [[ $rc != 0 ]]; then echo "TEST FAILED";exit $rc; fi
 rm perf_out
 
 
+echo "All Intel complied applications PASSED : Moving no to PGI Compiler Tests "
 
 cd -
 source ../../scripts/source_pgi_15.10
@@ -214,3 +216,6 @@ grep "Total Wall time" perf_out
 grep "PASSED" perf_out
 rc=$?; if [[ $rc != 0 ]]; then echo "TEST FAILED";exit $rc; fi
 rm perf_out
+
+echo "All PGI complied applications PASSED : Exiting Test Script "
+
