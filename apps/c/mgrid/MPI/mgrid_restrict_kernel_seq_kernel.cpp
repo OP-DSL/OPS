ab199ce5d9862ffc79e61d1395b9f947293f683d
Istvan Reguly <regulyistvan@gmail.com>
2018-09-16 21:02:29 +0200

    Fixed restriction issue with misalignment

--- apps/c/mgrid/MPI/mgrid_restrict_kernel_seq_kernel.cpp
+++ apps/c/mgrid/MPI/mgrid_restrict_kernel_seq_kernel.cpp
@@ -69,12 +69,24 @@ void ops_par_loop_mgrid_restrict_kernel(char const *name, ops_block block, int d
   global_idx[1] = start[1];
   #endif //OPS_MPI
 
-  int start_0[2]; int end_0[2]; int stride_0[2];
+  //This arg has a prolong stencil - so create different ranges
+  int start_0[2]; int end_0[2]; int stride_0[2];int d_size_0[2];
+  #ifdef OPS_MPI
+  for ( int n=0; n<2; n++ ){
+    sub_dat *sd0 = OPS_sub_dat_list[args[0].dat->index];
+    stride_0[n] = args[0].stencil->mgrid_stride[n];
+    d_size_0[n] = args[0].dat->d_m[n] + sd0->decomp_size[n] - args[0].dat->d_p[n];
+    start_0[n] = global_idx[n]*stride_0[n] - sd0->decomp_disp[n] + args[0].dat->d_m[n];
+    end_0[n] = start_0[n] + d_size_0[n];
+  }
+  #else
   for ( int n=0; n<2; n++ ){
     stride_0[n] = args[0].stencil->mgrid_stride[n];
-    start_0[n]  = start[n]*stride_0[n];
-    end_0[n]    = end[n];
+    d_size_0[n] = args[0].dat->d_m[n] + args[0].dat->size[n] - args[0].dat->d_p[n];
+    start_0[n] = global_idx[n]*stride_0[n];
+    end_0[n] = start_0[n] + d_size_0[n];
   }
+  #endif
   offs[0][0] = args[0].stencil->stride[0]*1;  //unit step in x dimension
   offs[0][1] = off2D(1, &start_0[0],
       &end_0[0],args[0].dat->size, args[0].stencil->stride) - offs[0][0];
