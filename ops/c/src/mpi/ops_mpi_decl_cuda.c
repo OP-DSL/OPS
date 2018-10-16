8f05fff5c1f9a0adf75868ff071c046db860c146
Istvan Reguly <regulyistvan@gmail.com>
2018-10-16 18:20:06 +0200

    Removing MPI dependency from .cu file

--- ops/c/src/mpi/ops_mpi_decl_cuda.c
+++ ops/c/src/mpi/ops_mpi_decl_cuda.c
@@ -199,3 +199,19 @@ void ops_NaNcheck(ops_dat dat) {
 int getOPS_block_size_x() { return OPS_block_size_x; }
 int getOPS_block_size_y() { return OPS_block_size_y; }
 int getOPS_block_size_z() { return OPS_block_size_z; }
+
+void ops_pack_cuda_internal(ops_dat dat, const int src_offset, char *__restrict dest,
+              const int halo_blocklength, const int halo_stride, const int halo_count);
+
+void ops_unpack_cuda_internal(ops_dat dat, const int dest_offset, const char *__restrict src,
+                const int halo_blocklength, const int halo_stride, const int halo_count);
+
+
+void ops_pack(ops_dat dat, const int src_offset, char *__restrict dest,
+              const ops_int_halo *__restrict halo) {
+  ops_pack_cuda_internal(dat,  src_offset, dest, halo->blocklength, halo->stride, halo->count);
+}
+void ops_unpack(ops_dat dat, const int dest_offset, const char *__restrict src,
+                const ops_int_halo *__restrict halo) {
+  ops_unpack_cuda_internal(dat,  dest_offset, src, halo->blocklength, halo->stride, halo->count);
+}
