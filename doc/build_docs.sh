5e2929e41e2eae75879661c9a76a38ba7937327a
Tim Schmielau <tim.schmielau@nag.co.uk>
2018-10-23 14:46:34 +0100

    doc Makefile

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -6,25 +6,4 @@
 #
 #sudo apt-get install doxygen graphviz
 
-pdflatex --shell-escape user.tex
-pdflatex --shell-escape user.tex
-bibtex user
-pdflatex --shell-escape user.tex
-
-#pdflatex --shell-escape mpidev.tex
-#pdflatex --shell-escape mpidev.tex
-#bibtex mpidev
-#pdflatex --shell-escape mpidev.tex
-
-doxygen ops/Doxyfile
-pushd ops/latex
-make refman.pdf
-popd
-
-doxygen ops_translator/Doxyfile
-pushd ops_translator/latex
-make refman.pdf
-popd
-
-rm -f *.out *.aux *.blg *.pyg.* *.log *.backup *.toc *~ *.bbl
-
+make all clean
