c6a548c31af9525f04ea60bfbb801253b6002a33
reguly <regulyistvan@gmail.com>
2018-10-25 11:04:25 +0100

    Resolves make issues with OpenACC and MPI_inline

--- apps/c/multiDim_HDF5/OpenACC/write_kernels.cpp
+++ apps/c/multiDim_HDF5/OpenACC/write_kernels.cpp
@@ -4,15 +4,15 @@
 #include "./OpenACC/write_common.h"
 
 #include <openacc.h>
-void ops_init_backend() {
-  acc_set_device_num(ops_get_proc() % acc_get_num_devices(acc_device_nvidia),
-                     acc_device_nvidia);
-}
 
-void ops_decl_const_char(int dim, char const *type, int size, char *dat,
-                         char const *name) {
+void ops_init_backend() {acc_set_device_num(ops_get_proc()%acc_get_num_devices(acc_device_nvidia),acc_device_nvidia);}
+
+void ops_decl_const_char(int dim, char const *type,
+int size, char *dat, char const *name){
   {
-    printf("error: unknown const name\n");
-    exit(1);
+    printf("error: unknown const name\n"); exit(1);
   }
 }
+
+//user kernel files
+#include "write_kernel_openacc_kernel.cpp"
