c408db7b0b538ab34368058624c5a4c0d37d07fd
Tim Schmielau <tim.schmielau@nag.co.uk>
2018-11-01 00:32:56 +0000

    add documentation from user.pdf to header files

--- ops/c/include/ops_lib_core.h
+++ ops/c/include/ops_lib_core.h
@@ -337,7 +337,29 @@ extern double OPS_checkpointing_time;
 * Core lib function prototypes
 *******************************************************************************/
 
+/**
+ * This routine must be called before all other OPS routines.
+ *
+ * @param argc         the usual command line argument
+ * @param argv         the usual command line argument
+ * @param diags_level  an integer which defines the level of debugging
+ *                     diagnostics and reporting to be performed
+ *
+ * Currently, higher `diags_level`s perform the following checks:
+ *
+ * | diags level | checks                                                      |
+ * | ----------- | ----------------------------------------------------------- |
+ * |  = 1        | no diagnostics, default to achieve best runtime performance.|
+ * |  > 1        | print block decomposition and ops par loop timing breakdown.|
+ * |  > 4        | print intra-block halo buffer allocation feedback (for OPS internal development only)                 |
+ * |  > 5        | check if intra-block halo MPI sends depth match MPI receives depth (for OPS internal development only)|
+ *
+ */
 void ops_init(const int argc, const char **argv, const int diags_level);
+
+/**
+ * This routine must be called last to cleanly terminate the OPS computation.
+ */
 void ops_exit();
 
 ops_dat ops_decl_dat_char(ops_block, int, int *, int *, int *, int *, int *, char *,
@@ -348,16 +370,58 @@ ops_dat ops_decl_dat_mpi_char(ops_block block, int size, int *dat_size,
                               int type_size, char const *type,
                               char const *name);
 
+/**
+ * Passes a pointer to the value(s) at the current grid point to the user kernel.
+ *
+ * The ::OPS_ACC macro has to be used for dereferencing the pointer.
+ *
+ * @param dat      dataset
+ * @param dim
+ * @param stencil  stencil for accessing data
+ * @param type     string representing the type of data held in dataset
+ * @param acc      access type
+ * @return
+ */
 ops_arg ops_arg_dat(ops_dat dat, int dim, ops_stencil stencil, char const *type,
                     ops_access acc);
 ops_arg ops_arg_dat_opt(ops_dat dat, int dim, ops_stencil stencil,
                         char const *type, ops_access acc, int flag);
+/**
+ * Returns an array of integers (in the user kernel) that have the index of
+ * the current grid point, i.e. `idx[0]` is the index in x, `idx[1]` is
+ * the index in y, etc.
+ * This is a globally consistent index, so even if the block is distributed
+ * across different MPI partitions, it gives you the same indexes.
+ * Generally used to generate initial geometry.
+ *
+ * @return
+ */
 ops_arg ops_arg_idx();
+
+/**
+ * Passes a pointer to a variable that needs to be incremented
+ * (or swapped for min/max reduction) by the user kernel.
+ *
+ * @param handle  an ::ops_reduction handle
+ * @param dim     array dimension (according to @p type)
+ * @param type    string representing the type of data held in data
+ * @param acc     access type
+ * @return
+ */
 ops_arg ops_arg_reduce(ops_reduction handle, int dim, const char *type,
                        ops_access acc);
 ops_arg ops_arg_reduce_core(ops_reduction handle, int dim, const char *type,
                             ops_access acc);
 
+/**
+ * This routine defines a reduction handle to be used in a parallel loop.
+ *
+ * @param size  size of data in bytes
+ * @param type  the name of type used for output diagnostics
+ *              (e.g. "double", "float")
+ * @param name  name of the dat used for output diagnostics
+ * @return
+ */
 ops_reduction ops_decl_reduction_handle(int size, const char *type,
                                         const char *name);
 ops_reduction ops_decl_reduction_handle_core(int size, const char *type,
@@ -372,6 +436,13 @@ void ops_init_core(const int argc, const char **argv, const int diags_level);
 
 void ops_exit_core(void);
 
+/**
+ * This routine defines a structured grid block.
+ *
+ * @param dims  dimension of the block
+ * @param name  a name used for output diagnostics
+ * @return
+ */
 ops_block ops_decl_block(int dims, const char *name);
 
 ops_dat ops_decl_dat_core(ops_block block, int data_size, int *block_size,
@@ -386,8 +457,41 @@ ops_dat ops_decl_dat_temp_core(ops_block block, int data_size, int *block_size,
 void ops_decl_const_core(int dim, char const *type, int typeSize, char *data,
                          char const *name);
 
+/**
+ * This routine defines a stencil.
+ *
+ * @param dims     dimension of loop iteration
+ * @param points   number of points in the stencil
+ * @param stencil  stencil for accessing data
+ * @param name     string representing the name of the stencil
+ * @return
+ */
 ops_stencil ops_decl_stencil(int dims, int points, int *stencil,
                              char const *name);
+
+/**
+ * This routine defines a strided stencil.
+ *
+ * The semantics for the index of data to be accessed, for stencil point `p`,
+ * in dimension `m` are defined as:
+ * ```
+ *    stride[m]*loop index[m] + stencil[p*dims+m]
+ * ```
+ * where `loop_index[m]` is the iteration index (within the user-defined
+ * iteration space) in the different dimensions.
+ * If, for one or more dimensions, both `stride[m]` and `stencil[p*dims+m]`
+ * are zero, then one of the following must be true;
+ *   - the dataset being referenced has size 1 for these dimensions
+ *   - these dimensions are to be omitted and so the dataset has dimension
+ *     equal to the number of remaining dimensions.
+ *
+ * @param dims     dimension of loop iteration
+ * @param points   number of points in the stencil
+ * @param stencil  stencil for accessing data
+ * @param stride   stride for accessing data
+ * @param name     string representing the name of the stencil
+ * @return
+ */
 ops_stencil ops_decl_strided_stencil(int dims, int points, int *sten,
                                      int *stride, char const *name);
 ops_stencil ops_decl_restrict_stencil( int dims, int points, int *sten,
@@ -395,25 +499,118 @@ ops_stencil ops_decl_restrict_stencil( int dims, int points, int *sten,
 ops_stencil ops_decl_prolong_stencil( int dims, int points, int *sten,
                                       int *stride, char const * name);
 
+/**
+ * This routine defines a halo relationship between two datasets defined on two
+ * different blocks.
+ *
+ * A @p from_dir [1,2] and a @p to_dir [2,1] means that x in the first block
+ * goes to y in the second block, and y in first block goes to x in second
+ * block.
+ * A negative sign indicates that the axis is flipped.
+ *
+ * (Simple example: a transfer from (1:2,0:99,0:99) to (-1:0,0:99,0:99) would
+ * use @p iter_size = [2,100,100], @p from_base = [1,0,0],
+ * @p to_base = [-1,0,0], @p from_dir = [0,1,2], @p to_dir = [0,1,2].
+ * In more complex cases this allows for transfers between blocks with
+ * different orientations.)
+ *
+ * @param from       origin dataset
+ * @param to         destination dataset
+ * @param iter_size  defines an iteration size
+ *                   (number of indices to iterate over in each direction)
+ * @param from_base  indices of starting point in @p from dataset
+ * @param to_base    indices of starting point in @p to dataset
+ * @param from_dir   direction of incrementing for @p from for each dimension
+ *                   of @p iter_size
+ * @param to_dir     direction of incrementing for @p to for each dimension
+ *                   of @p iter_size
+ * @return
+ */
 ops_halo ops_decl_halo(ops_dat from, ops_dat to, int *iter_size, int *from_base,
                        int *to_base, int *from_dir, int *to_dir);
 ops_halo ops_decl_halo_core(ops_dat from, ops_dat to, int *iter_size,
                             int *from_base, int *to_base, int *from_dir,
                             int *to_dir);
+
+/**
+ * This routine defines a collection of halos.
+ * Semantically, when an exchange is triggered for all halos
+ * in a group, there is no order defined in which they are carried out.
+ *
+ * @param nhalos  number of halos in @p halos
+ * @param halos   array of halos
+ * @return
+ */
 ops_halo_group ops_decl_halo_group(int nhalos, ops_halo *halos);
+
+/**
+ * This routine exchanges all halos in a halo group and will block execution
+ * of subsequent computations that depend on the exchanged data.
+ *
+ * @param group  the halo group
+ */
 void ops_halo_transfer(ops_halo_group group);
 
 ops_arg ops_arg_dat_core(ops_dat dat, ops_stencil stencil, ops_access acc);
 ops_arg ops_arg_gbl_core(char *data, int dim, int size, ops_access acc);
 
+/**
+ * This routine simply prints a variable number of arguments;
+ * it is created in place of the standard C printf() function which would
+ * print the same on each MPI process.
+ *
+ * @param format
+ * @param ...
+ */
 void ops_printf(const char *format, ...);
+
+/**
+ * This routine simply prints a variable number of arguments;
+ * it is created is in place of the standard C printf function which would
+ * print the same on each MPI process.
+ *
+ * @param stream
+ * @param format
+ * @param ...
+ */
 void ops_fprintf(FILE *stream, const char *format, ...);
 
+/**
+ * This routine prints out various useful bits of diagnostic info about sets,
+ * mappings and datasets.
+ *
+ * Usually used right after an @p ops partition() call to print out the details
+ * of the decomposition.
+ */
 void ops_diagnostic_output();
+
+/**
+ * Print OPS performance performance details to output stream.
+ *
+ * @param stream  output stream, use stdout to print to standard out
+ */
 void ops_timing_output(FILE *stream);
 void ops_timing_output_stdout();
 
+/**
+ * gettimeofday() based timer to start/end timing blocks of code.
+ *
+ *
+ * @param cpu  variable to hold the CPU time at the time of invocation
+ * @param et   variable to hold the elapsed time at the time of invocation
+ */
 void ops_timers(double *cpu, double *et);
+
+/**
+ * Write the details of an ::ops_block to a named text file.
+ *
+ * When used under an MPI parallelization each MPI process will write its own
+ * data set separately to the text file. As such it does not use MPI I/O.
+ * The data can be viewed using a simple text editor
+
+ * @param dat        ::ops_dat to to be written
+ * @param file_name  text file to write to
+ */
 void ops_print_dat_to_txtfile(ops_dat dat, const char *file_name);
 void ops_print_dat_to_txtfile_core(ops_dat dat, const char *file_name);
 
@@ -459,6 +656,18 @@ void ops_init_arg_idx(int *arg_idx, int ndims, ops_arg *args, int nargs);
 
 int ops_is_root();
 
+/**
+ * Triggers a multi-block partitioning across a distributed memory set of
+ * processes.
+ * (links to a dummy function for single node parallelizations).
+ * This routine should only be called after all the ops_decl_block() and
+ * ops_decl_dat() statements have been declared.
+ *
+ * @param routine  string describing the partitioning method.
+ *                 Currently this string is not used internally, but is simply
+ *                 a place-holder to indicate different partitioning methods
+ *                 in the future.
+ */
 void ops_partition(const char *routine);
 
 void ops_mpi_reduce_float(ops_arg *args, float *data);
@@ -490,12 +699,87 @@ int ops_num_procs();
 /*******************************************************************************
 * External access support
 *******************************************************************************/
+
+/**
+ * This routine returns the number of chunks of the given dataset held by the
+ * current process.
+ *
+ * @param dat  the dataset
+ * @return
+ */
 int ops_dat_get_local_npartitions(ops_dat dat);
+
+/**
+ * This routine returns the MPI displacement and size of a given chunk of the
+ * given dataset on the current process.
+ *
+ * @param dat   the dataset
+ * @param part  the chunk index (has to be 0)
+ * @param disp  an array populated with the displacement of the chunk within the
+ *              "global" distributed array
+ * @param size  an array populated with the spatial extents
+ */
 void ops_dat_get_extents(ops_dat dat, int part, int *disp, int *size);
+
+/**
+ * This routine returns a pointer to the internally stored data, with MPI halo
+ * regions automatically updated as required by the supplied stencil.
+ * The strides required to index into the dataset are also given.
+ *
+ * @param dat      the dataset
+ * @param part     the chunk index (has to be 0)
+ * @param stencil  a stencil used to determine required MPI halo exchange depths
+ * @param stride   an array populated strides in spatial dimensions needed for
+ *                 column-major indexing
+ * @return
+ */
 char* ops_dat_get_raw_pointer(ops_dat dat, int part, ops_stencil stencil, int *stride);
+
+/**
+ * Indicates to OPS that a dataset previously accessed with
+ * ops_dat_get_raw_pointer() is released by the user, and also tells OPS how it
+ * was accessed.
+ *
+ * @param dat   the dataset
+ * @param part  the chunk index (has to be 0
+ * @param acc   the kind of access that was used by the user
+ *              (::OPS_READ if it was read only,
+ *              ::OPS_WRITE if it was overwritten,
+ *              ::OPS_RW if it was read and written)
+ */
 void ops_dat_release_raw_data(ops_dat dat, int part, ops_access acc);
+
+/**
+ * This routine copies the data held by OPS to the user-specified
+ * memory location, which needs to be at least as large as indicated
+ * by the `sizes` parameter of ops_dat_get_extents().
+
+ * @param dat   the dataset
+ * @param part  the chunk index (has to be 0)
+ * @param data  pointer to memory which should be filled by OPS
+ */
 void ops_dat_fetch_data(ops_dat dat, int part, char *data);
+
+/**
+ * This routine copies the data given by the user to the internal data structure
+ * used by OPS.
+ *
+ * User data needs to be laid out in column-major order and strided as indicated
+ * by the `sizes` parameter of ops_dat_get_extents().
+ *
+ * @param dat   the dataset
+ * @param part  the chunk index (has to be 0)
+ * @param data  pointer to memory which should be copied to OPS
+ */
 void ops_dat_set_data(ops_dat dat, int part, char *data);
+
+/**
+ * This routine returns the number of chunks of the given dataset held by all
+ * processes.
+ *
+ * @param dat  the dataset
+ * @return
+ */
 int ops_dat_get_global_npartitions(ops_dat dat);
 
 /*******************************************************************************
