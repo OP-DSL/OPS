5f4aa37d5cdefec9f8f9c318f8722bfcfda21678
Istvan Reguly <regulyistvan@gmail.com>
2018-11-02 12:48:04 +0100

    Works on CloverLeaf too

--- apps/c/CloverLeaf/field_summary_kernel.h
+++ apps/c/CloverLeaf/field_summary_kernel.h
@@ -1,10 +1,10 @@
 #ifndef FIELD_SUMMARY_KERNEL_H
 #define FIELD_SUMMARY_KERNEL_H
 
-void field_summary_kernel( const double *volume, const double *density0,
-                     const double *energy0, const double *pressure,
-                     const double *xvel0,
-                     const double *yvel0,
+void field_summary_kernel( const ACC<double> &volume, const ACC<double> &density0,
+                     const ACC<double> &energy0, const ACC<double> &pressure,
+                     const ACC<double> &xvel0,
+                     const ACC<double> &yvel0,
                      double *vol,
                      double *mass,
                      double *ie,
@@ -17,19 +17,19 @@ void field_summary_kernel( const double *volume, const double *density0,
   //yvel0, 0,0, 1,0, 0,1, 1,1
 
   vsqrd = 0.0;
-  vsqrd = vsqrd + 0.25 * ( xvel0[OPS_ACC4(0,0)] * xvel0[OPS_ACC4(0,0)] + yvel0[OPS_ACC5(0,0)] * yvel0[OPS_ACC5(0,0)]);
-  vsqrd = vsqrd + 0.25 * ( xvel0[OPS_ACC4(1,0)] * xvel0[OPS_ACC4(1,0)] + yvel0[OPS_ACC5(1,0)] * yvel0[OPS_ACC5(1,0)]);
-  vsqrd = vsqrd + 0.25 * ( xvel0[OPS_ACC4(0,1)] * xvel0[OPS_ACC4(0,1)] + yvel0[OPS_ACC5(0,1)] * yvel0[OPS_ACC5(0,1)]);
-  vsqrd = vsqrd + 0.25 * ( xvel0[OPS_ACC4(1,1)] * xvel0[OPS_ACC4(1,1)] + yvel0[OPS_ACC5(1,1)] * yvel0[OPS_ACC5(1,1)]);
+  vsqrd = vsqrd + 0.25 * ( xvel0(0,0) * xvel0(0,0) + yvel0(0,0) * yvel0(0,0));
+  vsqrd = vsqrd + 0.25 * ( xvel0(1,0) * xvel0(1,0) + yvel0(1,0) * yvel0(1,0));
+  vsqrd = vsqrd + 0.25 * ( xvel0(0,1) * xvel0(0,1) + yvel0(0,1) * yvel0(0,1));
+  vsqrd = vsqrd + 0.25 * ( xvel0(1,1) * xvel0(1,1) + yvel0(1,1) * yvel0(1,1));
 
 
-  cell_vol = volume[OPS_ACC0(0,0)];
-  cell_mass = cell_vol * density0[OPS_ACC1(0,0)];
+  cell_vol = volume(0,0);
+  cell_mass = cell_vol * density0(0,0);
   *vol = *vol + cell_vol;
   *mass = *mass + cell_mass;
-  *ie = *ie + cell_mass * energy0[OPS_ACC2(0,0)];
+  *ie = *ie + cell_mass * energy0(0,0);
   *ke = *ke + cell_mass * 0.5 * vsqrd;
-  *press = *press + cell_vol * pressure[OPS_ACC3(0,0)];
+  *press = *press + cell_vol * pressure(0,0);
 
 }
 
--- apps/c/CloverLeaf/reset_field_kernel.h
+++ apps/c/CloverLeaf/reset_field_kernel.h
@@ -2,19 +2,19 @@
 #define RESET_FIELD_KERNEL_H
 
 
-void reset_field_kernel1( double *density0, const double *density1,
-                        double *energy0, const double *energy1) {
+void reset_field_kernel1( ACC<double> &density0, const ACC<double> &density1,
+                        ACC<double> &energy0, const ACC<double> &energy1) {
 
-  density0[OPS_ACC0(0,0)]  = density1[OPS_ACC1(0,0)] ;
-  energy0[OPS_ACC2(0,0)]  = energy1[OPS_ACC3(0,0)] ;
+  density0(0,0)  = density1(0,0) ;
+  energy0(0,0)  = energy1(0,0) ;
 
 }
 
-void reset_field_kernel2( double *xvel0, const double *xvel1,
-                        double *yvel0, const double *yvel1) {
+void reset_field_kernel2( ACC<double> &xvel0, const ACC<double> &xvel1,
+                        ACC<double> &yvel0, const ACC<double> &yvel1) {
 
-  xvel0[OPS_ACC0(0,0)]  = xvel1[OPS_ACC1(0,0)] ;
-  yvel0[OPS_ACC2(0,0)]  = yvel1[OPS_ACC3(0,0)] ;
+  xvel0(0,0)  = xvel1(0,0) ;
+  yvel0(0,0)  = yvel1(0,0) ;
 
 }
 
