10fa2ffd7c997d920d55055c55bc2fd5e6b97404
Gihan R. Mudalige <mudalige@octon.arc.ox.ac.uk>
2018-12-12 13:34:56 +0000

    Fix read write constant issue in multiDim_HDF5 application

--- apps/c/multiDim_HDF5/read_ops.cpp
+++ apps/c/multiDim_HDF5/read_ops.cpp
@@ -41,6 +41,18 @@ int main(int argc, char **argv) {
   ops_fetch_dat_hdf5_file(single, "read_data.h5");
   ops_fetch_dat_hdf5_file(integ, "read_data.h5");
 
+  int my_const;
+  ops_get_const_hdf5("my_const", 1, "int", (char*)&my_const, "write_data.h5");
+  printf("Read const: %d\n", my_const);
+
+  char buffer[50];
+  ops_get_const_hdf5("my_text", 11, "char", buffer, "write_data.h5");
+  printf("Read text: %s\n", buffer);
+
+  ops_write_const_hdf5("my_const", 1, "int", (char*)&my_const, "read_data.h5");
+  ops_write_const_hdf5("my_text", 11, "char", (char*)buffer, "read_data.h5");
+
+
 
 
   ops_timing_output(stdout);
