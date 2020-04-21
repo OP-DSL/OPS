5b5abef5232d4154c29fcb759b209dff8a1202fb
Istvan Reguly <regulyistvan@gmail.com>
2020-04-21 14:08:41 +0200

    char array initialization

--- ops/c/src/opencl/ops_opencl_decl.cpp
+++ ops/c/src/opencl/ops_opencl_decl.cpp
@@ -370,8 +370,7 @@ void ops_print_dat_to_txtfile(ops_dat dat, const char *file_name) {
 }
 
 void ops_NaNcheck(ops_dat dat){
-  char buffer[1];
-  sprintf(buffer, "");
+  char buffer[1]={'\0'};
   // need to get data from GPU
   ops_opencl_get_data(dat);
   ops_NaNcheck_core(dat, buffer);
