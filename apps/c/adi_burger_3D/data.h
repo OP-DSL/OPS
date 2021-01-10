18a5a777b04b3f52eab018fadb3471188f91933e
Jianping Meng <jpmeng@gmail.com>
2021-01-10 22:39:02 +0000

    Add 3D Burgers equation app

---
+++ apps/c/adi_burger_3D/data.h
@@ -0,0 +1,16 @@
+#ifndef __ADI_DATA_H
+#define __ADI_DATA_H
+// declare a few constants for solving the equation
+extern int nx;
+extern int ny;
+extern int nz;
+extern int iter;
+extern double dx;
+extern double dy;
+extern double dz;
+extern double dt;
+extern double Re;
+extern double r1;
+extern double r2;
+extern double h;
+#endif
