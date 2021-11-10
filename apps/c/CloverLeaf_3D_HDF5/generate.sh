0a0adcf18fef3588a9d71e690c2c2acae4d68710
Jianping Meng <jpmeng@gmail.com>
2021-11-10 14:47:47 +0000

    Delete generated files in subfolders such as cuda

--- apps/c/CloverLeaf_3D_HDF5/generate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-../../../ops_translator/c/ops.py clover_leaf.cpp revert.cpp reset_field.cpp ideal_gas.cpp PdV.cpp accelerate.cpp advec_cell.cpp accelerate.cpp advec_mom.cpp calc_dt.cpp field_summary.cpp flux_calc.cpp viscosity.cpp initialise_chunk.cpp generate.cpp update_halo.cpp
