79cb82f1993b8d04871de96d8db3581f7f96389c
Beniel Thileepan <thileepanbeniel@gmail.com>
2023-02-19 00:49:17 +0000

    Added initial argparser and added Target class

---
+++ ops_translator_v2/ops-translator/target.py
@@ -0,0 +1,39 @@
+from typing import Any, Dict
+from util import Findable
+
+class Target(Findable):
+    name: str
+    kernel_translation: bool
+    config: Dict[str, Any]
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def __eq__(self, other) -> bool:
+        return self.name == other.name if type(other) is type(self) else False
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def matches(self, key: str) -> bool:
+        return self.name == key.lower()
+
+class Seq(Target):
+    name = "seq"
+    kernel_translation = False
+    config = {
+        "grouped" : False, 
+        "device" : 1
+        }
+
+class HLS(Target):
+    name = "hls"
+    kernel_translation = True
+    config = {
+        "grouped" : False,
+        "SLR_count" : 1,
+        "device" : 3
+        }
+
+Target.register(Seq)
+Target.register(HLS)
