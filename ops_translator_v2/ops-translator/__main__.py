b7cd9592587657600781015bda851a583feedb0a
Beniel Thileepan <thileepanbeniel@gmail.com>
2023-02-28 20:22:55 +0000

    Initial commit of language.py, ops.py and store.py

--- ops_translator_v2/ops-translator/__main__.py
+++ ops_translator_v2/ops-translator/__main__.py
@@ -1,7 +1,10 @@
 import os
 import json
-
 from argparse import ArgumentParser, ArgumentTypeError, Namespace
+from pathlib import Path
+
+#custom implementation imports
+from language import Lang
 from target import Target
 from util import getVersion
 
@@ -29,8 +32,41 @@ def main(argv=None) -> None:
     target_names = [target.name for target in Target.all()] #TODO: implement Target Findable class
     parser.add_argument("-t", "--target", help="Code-gereration target", type=str, action="append", nargs=1, choices=target_names, default=[])
 
+    #invoking arg parser
     args = parser.parse_args(argv)
 
+    if os.environ.get("OPS_AUTO_SOA") is not None:
+        args.force_soa = True
+
+    file_parents = [Path(file_path).parent for file_path in args.file_paths]
+
+    if args.out is None:
+        args.out = file_parents[0]
+
+    #checking includes of OPS
+    if os.environ.get("OPS_INSTALL_PATH") is not None:
+        ops_install_path = Path(os.environ.get("OPS_INSTALL_PATH"))
+        args.I = [[str(ops_install_path/"include")]] + args.I
+    else:
+        script_parents = list(Path(__file__).resolve().parents)
+        if len(script_parents) >= 3 and script_parents[2].stem == "OPS":
+            args.I = [[str(script_parents[2].joinpath("ops/c/include"))]] + args.I
+
+    args.I = [[str(file_parent)] for file_parent in file_parents] + args.I
+
+    # Collect the set of ile extensions 
+    extentions = {str(Path(file_path).suffix)[1:] for file_path in args.file_paths}
+
+    if not extentions:
+        exit("Missing file extensions, unable to determine target language.")
+    elif len(extentions) > 1:
+        exit("Varying file extensions, unable to determine target language.")
+    else:
+        extention = extentions[0]
+    
+    lang = Lang.find(extention)
+
+    
 def isDirPath(path):
     if os.path.isdir(path):
         return path
