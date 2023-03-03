0eb795e29c3811282134b98f196b70a3192a9b22
Beniel Thileepan <thileepanbeniel@gmail.com>
2023-03-03 23:11:57 +0000

    Completed __main__.py. Need to add templates and schemes

--- ops_translator_v2/ops-translator/__main__.py
+++ ops_translator_v2/ops-translator/__main__.py
@@ -1,13 +1,19 @@
+import dataclasses
 import os
 import json
 from argparse import ArgumentParser, ArgumentTypeError, Namespace
+from datetime import datetime
 from pathlib import Path
 
 #custom implementation imports
 import cpp
+from jinja import env
 from language import Lang
+from ops import OpsError, Type
+from scheme import Scheme
+from store import Application, ParseError
 from target import Target
-from util import getVersion
+from util import getVersion, safeFind
 
 def main(argv=None) -> None:
 
@@ -22,7 +28,7 @@ def main(argv=None) -> None:
     parser.add_argument("-c", "--config", help="Target configuration", type=json.loads, default="{}")
     parser.add_argument("-soa", "--force_soa", help="Force structs of arrays", action="store_true")
 
-    parser.add_argument("--suffix", help="Add a suffix to genreated program translations", default="")
+    parser.add_argument("--suffix", help="Add a suffix to genreated program translations", default="_ops")
 
     parser.add_argument("-I", help="Add to include directories", type=isDirPath, action="append", nargs=1, default=[])
     parser.add_argument("-i", help="Add to include files", type=isFilePath, action="append", nargs=1, default=[])
@@ -56,19 +62,174 @@ def main(argv=None) -> None:
     args.I = [[str(file_parent)] for file_parent in file_parents] + args.I
 
     # Collect the set of ile extensions 
-    extentions = {str(Path(file_path).suffix)[1:] for file_path in args.file_paths}
+    extensions = {str(Path(file_path).suffix)[1:] for file_path in args.file_paths}
 
-    if not extentions:
+    if not extensions:
         exit("Missing file extensions, unable to determine target language.")
-    elif len(extentions) > 1:
+    elif len(extensions) > 1:
         exit("Varying file extensions, unable to determine target language.")
     else:
-        [extention] = extentions
+        [extension] = extensions
 
-    lang = Lang.find(extention)
+    lang = Lang.find(extension)
 
     if lang is None:
-        exit(f"Unknown file extention: {extention}")
+        exit(f"Unknown file extension: {extension}")
+
+    Type.set_formatter(lang.formatType)
+
+    if len(args.target) == 0:
+        args.target = [[target_name] for target_name in target_names]
+
+    try:
+        app = parse(args, lang)
+    except ParseError as e:
+        exit(e)
+
+    # TODO: Make sure SOA is applicable to OPS
+    # if args.force_soa:
+    #     for program in app.programs:
+    #         for loop in program.loops:
+    #             loop.dats = [dataclasses.replace(dat, soa=True) for dat in loop.dats]
+    if args.verbose:
+        print()
+        print(app)
+
+    # Validation phase
+    try: 
+        validate(args, lang, app)
+    except OpsError as e:
+        exit(e)
+
+    # Generating code for targets
+    for [target] in args.target:
+        target = Target.find(target)
+
+        # Applying user defined configs to the target config
+        for key in target.config:
+            if key in args.config:
+                target.config[key] = args.config[key]
+
+        scheme = Scheme.find((lang, target))
+
+        if not scheme:
+            if args.verbose:
+                print(f"No scheme register for {lang}/{target}")
+
+            continue
+
+        if args.verbose:
+            print(f"Translation scheme: {scheme}")
+
+        codegen(args, scheme, app, args.force_soa)
+
+        if args.verbose:
+            print(f"Translation completed: {scheme}")
+
+
+    # Generate program translations
+    for i, program in enumerate(app.programs, 1):
+        include_dirs = set([Path(dir) for [dir] in args.I])
+        defines = [define for [define] in args.D]
+
+        source = lang.translateProgram(program, include_dirs, defines, args.force_soa)
+
+        new_file = os.path.splitext(os.path.basename(program.path))[0]
+        ext = os.path.splitext(os.path.basename(program.path))[1]
+        new_path = Path(args.out, f"{new_file}{args.suffix}{ext}")
+
+        with open(new_path, "w") as new_file:
+            new_file.write(f"\n{lang.com_delim} Auto-generated at {datetime.now()} by ops-translator v2")
+            new_file.write(source)
+
+            if args.verbose:
+                print(f"Translated program {i} of {len(args.file_paths)}: {new_path}")
+        
+
+
+def parse(args: Namespace, lang: Lang) -> Application:
+    app = Application()
+
+    # Collect the include directories
+    include_dirs = set([Path(dir) for [dir] in args.I])
+    defines = [define for [define] in args.D]
+
+    # Parse the input files
+    for i, raw_path in enumerate(args.file_paths, 1):
+        if args.verbose:
+            print(f"Parseing file {i} of {len(args.file_paths)}: {raw_path}")
+
+        # Parase the program
+        program = lang.parseProgram(Path(raw_path), include_dirs, defines)
+        app.programs.append(program)
+        
+    return app
+
+def validate(args: Namespace, lang: Lang, app: Application) -> None:
+    # Run sementic checks on the application
+    app.validate(Lang)
+
+    if args.dump:
+        store_path = Path(args.out, "store.json")
+        serializer = lambda o: getattr(o, "__dict__", "unserializable")
+
+        # Write application dump
+        with open(store_path, "w") as file:
+            file.write(json.dumps(app, default=serializer, indent=4))
+
+        if args.verbose:
+            print("Dumped store: ", store_path.resolve(), end="\n\n")
+
+
+def codegen(args: Namespace, scheme: Scheme, app: Application, force_soa: bool = False) -> None:
+    # Collect the paths of the generated files
+    include_dirs = set([Path(dir) for [dir] in args.I])
+    defines = [define for [define] in args.D]
+
+    # Generate loop hosts
+    for i, (loop, program) in enumerate(app.loops(), 1):
+        # Generate loop host source
+        source, extension = scheme.genLoopHost(include_dirs, defines, env, loop, program, app, i)
+
+        # From output files path
+        path = None
+        if scheme.lang.kernel_dir:
+            Path(args.out, scheme.target.name).mkdir(parents=True, exist_ok=True)
+            path = Path(args.out, scheme.target.name, f"{i}_{loop.kernel}_kernel.{extension}")
+        else:
+            path = Path(args.out,f"{loop.kernel}_{scheme.target.name}_kernel.{extension}")
+
+        # Write the gernerated source file
+        with open(path, "w") as file:
+            file.write(f"{scheme.lang.com_delim} Auto-generated at {datetime.now()} by ops-translator v2")
+            file.write(source)
+
+            if args.verbose:
+                print(f"Generated loop host {i} of {len(app.loops())}: {path}")
+
+    # Gernerate master kernel file
+    if scheme.master_kernel_template is not None:
+        user_types_name = f"user_types.{scheme.lang.include_ext}"
+        user_types_candidates = [Path(dir, user_types_name) for dir in include_dirs]
+        user_types_file = safeFind(user_types_candidates, lambda p: p.is_file())
+
+        source, name = scheme.genMasterKernel(env, app, user_types_file)
+
+        path = None
+
+        if scheme.lang.kernel_dir:
+            Path(args.out, scheme.target.name).mkdir(parents=True, exist_ok=True)
+            path = Path(args.out, scheme.target.name, name)
+
+        else:
+            path = Path(args.out, name)
+
+        with open(path, "w") as file:
+            file.write(f"{scheme.lang.com_delim} Auto-generated at {datetime.now()} by ops-translator v2")
+            file.write(source)
+
+            if args.verbose:
+                print(f"Generated master kernel file: {path}")
 
     
 def isDirPath(path):
