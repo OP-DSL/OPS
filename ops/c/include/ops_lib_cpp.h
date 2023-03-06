7d6c5a5e835566cc8a89707d1793b459423a9f7f
Daniel Balogh <balogh.dani54@gmail.com>
2023-03-06 17:07:44 +0100

    Remove empty file

--- ops/c/include/ops_lib_cpp.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Dummy file:
-//
-// This file is only here to CI on the OPS repo will pass.  Once this branch as merged, 
-// this file can be deleted and pdetk/common/include/dat.h will need to be updated
