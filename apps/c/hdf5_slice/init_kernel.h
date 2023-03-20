fb3190ac6e7e5952e7e0cb229005787705a3fe8b
Jianping Meng <jpmeng@gmail.com>
2023-03-20 18:26:58 +0000

    Add example of double to float

--- apps/c/hdf5_slice/init_kernel.h
+++ apps/c/hdf5_slice/init_kernel.h
@@ -8,9 +8,12 @@ void initKernelU(ACC<double> &u, int *idx) {
   u(0, 0, 0) = sin(x) * sin(2 * y) * cos(3 * z);
 }
 
-void CopyU(const ACC<double> &u, ACC<float> &u_single) {
+void KernelCopy3D(const ACC<double> &u, ACC<float> &u_single) {
+#ifdef OPS_3D
   u_single(0, 0, 0) = float(u(0, 0, 0));
+#endif
 }
+
 
 void initKernelvelo(ACC<double> &velo, int *idx) {
   const double x{h * idx[0]};
@@ -18,7 +21,8 @@ void initKernelvelo(ACC<double> &velo, int *idx) {
   const double z{h * idx[2]};
   velo(0, 0, 0, 0) = sin(x) * sin(y) * sin(z);
   velo(1, 0, 0, 0) = sin(2 * x) * sin(2 * y) * sin(2 * z);
-  velo(2, 0, 0, 0) = sin(3 * x) * sin(3 * y) * sin(3 * z);;
+  velo(2, 0, 0, 0) = sin(3 * x) * sin(3 * y) * sin(3 * z);
+  ;
 }
 
 void initKernelV(ACC<int> &v, int *idx) {
