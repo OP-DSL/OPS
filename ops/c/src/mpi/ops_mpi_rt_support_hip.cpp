58002a9934e298612ad89e1993902295e8370468
Ashutosh Londhe (EXT) <asl@kos.hpsg.dcs.warwick.ac.uk>
2023-06-04 22:28:54 +0100

    finished hip/cpp template, tested with laplace, poisson, cloverleaf_3d and tealeaf application

--- ops/c/src/mpi/ops_mpi_rt_support_hip.cpp
+++ ops/c/src/mpi/ops_mpi_rt_support_hip.cpp
@@ -612,9 +612,9 @@ void ops_dat_fetch_data_slab_memspace(ops_dat dat, int part, char *data, int *ra
       prod *= target->size[d];
     }
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
-    desc->name = "ops_internal_copy_device";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    strcpy(desc->name, "ops_internal_copy_device\0");
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
@@ -653,9 +653,9 @@ void ops_dat_set_data_slab_memspace(ops_dat dat, int part, char *data, int *rang
       prod *= target->size[d];
     }
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
-    desc->name = "ops_internal_copy_device_reverse";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    strcpy(desc->name, "ops_internal_copy_device_reverse\0");
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
@@ -693,9 +693,9 @@ void ops_dat_fetch_data_memspace(ops_dat dat, int part, char *data, ops_memspace
     target->base_offset = 0;
     for (int d = 0; d < OPS_MAX_DIM; d++) target->size[d] = size[d];
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
-    desc->name = "ops_internal_copy_device";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    strcpy(desc->name, "ops_internal_copy_device\0");
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
@@ -728,9 +728,9 @@ void ops_dat_set_data_memspace(ops_dat dat, int part, char *data, ops_memspace m
     target->base_offset = 0;
     for (int d = 0; d < OPS_MAX_DIM; d++) target->size[d] = size[d];
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
-    desc->name = "ops_internal_copy_device_reverse";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    strcpy(desc->name, "ops_internal_copy_device_reverse\0");
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
--- ops_translator_v2/ops-translator/cpp/schemes.py
+++ ops_translator_v2/ops-translator/cpp/schemes.py
@@ -63,3 +63,32 @@ class CppCuda(Scheme):
         return ctk.writeSource(extracted_entities)
 
 Scheme.register(CppCuda)
+
+
+class CppHip(Scheme):
+    lang = Lang.find("cpp")
+    target = Target.find("hip")
+
+    const_template = None
+    loop_host_template = Path("cpp/cuda/loop_host.cpp.j2")
+    master_kernel_template = Path("cpp/cuda/master_kernel.cpp.j2")
+
+    loop_kernel_extension = "cpp"
+    master_kernel_extension = "cpp"
+
+    def translateKernel(
+        self,
+        loop: ops.Loop,
+        program: Program,
+        app: Application,
+        kernel_idx: int
+    ) -> str:
+        kernel_entities = app.findEntities(loop.kernel, program)
+
+        if len(kernel_entities) == 0:
+            raise ParseError(f"Unable to find kernel: {loop.kernel}")
+
+        extracted_entities = ctk.extractDependancies(kernel_entities, app)
+        return ctk.writeSource(extracted_entities)
+
+Scheme.register(CppHip)
