e2797a471d06200b22ba22bdd0a770f19f88a072
Istvan Reguly <beef1056@bask-pg0309u27a.cluster.baskerville.ac.uk>
2023-08-08 09:57:21 +0100

    no cl_int

--- ops/c/src/mpi/ops_mpi_rt_support_sycl.cpp
+++ ops/c/src/mpi/ops_mpi_rt_support_sycl.cpp
@@ -619,8 +619,8 @@ void ops_dat_fetch_data_slab_memspace(ops_dat dat, int part, char *data, int *ra
     }
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
     desc->name = "ops_internal_copy_device";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
@@ -660,8 +660,8 @@ void ops_dat_set_data_slab_memspace(ops_dat dat, int part, char *data, int *rang
     }
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
     desc->name = "ops_internal_copy_device_reverse";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
@@ -700,8 +700,8 @@ void ops_dat_fetch_data_memspace(ops_dat dat, int part, char *data, ops_memspace
     for (int d = 0; d < OPS_MAX_DIM; d++) target->size[d] = size[d];
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
     desc->name = "ops_internal_copy_device";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
@@ -735,8 +735,8 @@ void ops_dat_set_data_memspace(ops_dat dat, int part, char *data, ops_memspace m
     for (int d = 0; d < OPS_MAX_DIM; d++) target->size[d] = size[d];
     ops_kernel_descriptor *desc = ops_dat_deep_copy_core(target, dat, range);
     desc->name = "ops_internal_copy_device_reverse";
-    desc->device = 1;
-    desc->function = ops_internal_copy_device;
+    desc->isdevice = 1;
+    desc->func = ops_internal_copy_device;
     ops_internal_copy_device(desc);
     target->data_d = NULL;
     ops_free(target);
