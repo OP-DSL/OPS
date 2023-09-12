7f92df83eeda685efd1389b695e98293e2339bc9
g.mudalige@warwick.ac.uk <g.mudalige@warwick.ac.uk>
2023-09-12 09:45:53 +0100

    Testing TeaLeaf with test scripts

--- apps/c/CloverLeaf_3D/test.sh
+++ apps/c/CloverLeaf_3D/test.sh
@@ -7,9 +7,9 @@ cd ../../../ops/c
 #export SOURCE_INTEL_SYCL=source_intel_2021.3_sycl_pythonenv
 #export SOURCE_AMD_HIP=source_amd_rocm-5.4.3_pythonenv
 
-export AMOS=TRUE
+#export AMOS=TRUE
 #export DMOS=TRUE
-#export TELOS=TRUE
+export TELOS=TRUE
 #export KOS=TRUE
 
 if [[ -v TELOS || -v KOS ]]; then
