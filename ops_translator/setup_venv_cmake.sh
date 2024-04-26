520bd835b870cb1228add5730dcf05470ebf7e81
Ashutosh Londhe (EXT) <londhe.ashutosh@gmail.com>
2024-04-26 03:40:35 +0100

    installing python dependency for new codegen through cmake, not tested with apps to use new codegen with cmake

---
+++ ops_translator/setup_venv_cmake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Do not run below command if virtualenv is already installed
+python3 -m pip install --user virtualenv
+
+OPS_INSTALL_PATH=$1
+
+mkdir -p $OPS_INSTALL_PATH/ops_venv
+
+python3 -m venv $OPS_INSTALL_PATH/ops_venv
+
+source $OPS_INSTALL_PATH/ops_venv/bin/activate
+
+python3 -m pip install --upgrade pip
+
+python3 -m pip install -r $OPS_INSTALL_PATH/requirements.txt
+
+python3 -m pip install --force-reinstall libclang==16.0.6
+
