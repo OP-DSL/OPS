75de96c7446cb1ffc9bb85f53adcee0a62386a2d
Ashutosh Londhe (EXT) <londhe.ashutosh@gmail.com>
2024-05-13 23:52:23 +0100

    added more tests to perform

--- apps/c/runtests.cmake
+++ apps/c/runtests.cmake
@@ -1,8 +1,18 @@
 macro(TEST CMD ARG OPS_INSTALL_PATH)
     set(ENV{OPS_INSTALL_PATH} ${OPS_INSTALL_PATH})
+
+    string(FIND ${CMD} "_openmp" openmp_index)
+    if(openmp_index EQUAL -1)
+        set(ENV{OMP_NUM_THREADS} 1)
+    else()
+        set(ENV{OMP_NUM_THREADS} 2)
+    endif()
+
     #set(ENV{OMP_NUM_THREADS} 1)
     separate_arguments(args NATIVE_COMMAND ${ARG})
-    execute_process(COMMAND ${CMD} ${args} OUTPUT_FILE perf.out)
+    separate_arguments(cmds NATIVE_COMMAND ${CMD})
+
+    execute_process(COMMAND ${cmds} ${args} OUTPUT_FILE perf.out)
     execute_process(COMMAND grep "PASSED" perf.out RESULT_VARIABLE RES)
     # message("RES=${RES}")
     if(RES)
