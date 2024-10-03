f760b006cb0fab93f22a3a6817bc1f99fe3c3830
benielT <Beniel.Thileepan@warwick.ac.uk>
2024-10-03 21:25:15 +0300

    OPS HLS app heat3d optimised kernel project and config changes added

--- apps/c/heat3d_fpga_experimental/opt_kernel/heat3d_kernels.h
+++ apps/c/heat3d_fpga_experimental/opt_kernel/heat3d_kernels.h
@@ -25,6 +25,15 @@ void ops_krnl_copy(ACC<float> &data, const ACC<float>& data_new)
 
 void ops_krnl_heat3D(ACC<float> & next, const ACC<float> & current, const float * K, const int * idx)
 {
-	next(0,0,0) = (1 - 6 * (*K)) * current(0,0,0) 
-			+ (*K) * (current(1,0,0) + current(-1,0,0) + current(0,1,0) + current(0,-1,0) + current(0,0,1) + current(0,0,-1));
+    const float reg0 = (1 - 6 * (*K));
+    const float reg1 = current(1,0,0) + current(-1,0,0);
+    const float reg2 = current(0,1,0) + current(0,-1,0);
+    const float reg3 = current(0,0,1) + current(0,0,-1);
+    const float reg4 = reg1 + reg2;
+    const float reg5 = reg0 * current(0,0,0);
+    const float reg6 = reg4 + reg3;
+    const float reg7 = (*K) * reg6;
+    next(0,0,0) =  reg7 + reg6;
+	// next(0,0,0) = (1 - 6 * (*K)) * current(0,0,0) 
+	// 		+ (*K) * (current(1,0,0) + current(-1,0,0) + current(0,1,0) + current(0,-1,0) + current(0,0,1) + current(0,0,-1));
 }
