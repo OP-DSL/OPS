97fec90e81bb65caa0388e48b6770c4dcf0ba597
g.mudalige@warwick.ac.uk <g.mudalige@warwick.ac.uk>
2025-07-30 15:12:54 +0100

    Testing on Demos

--- apps/c/poisson/test.sh
+++ apps/c/poisson/test.sh
@@ -2,17 +2,21 @@
 set -e
 cd $OPS_INSTALL_PATH/c
 
-export SOURCE_INTEL=source_intel_2021.3_pythonenv
+#export SOURCE_INTEL=source_intel_2021.3_pythonenv
+export SOURCE_INTEL=source_oneapi_sycl_pythonenv
 export SOURCE_PGI=source_pgi_nvhpc_23_pythonenv
-export SOURCE_INTEL_SYCL=source_intel_2021.3_sycl_pythonenv
+#export SOURCE_INTEL_SYCL=source_intel_2021.3_sycl_pythonenv
+export SOURCE_INTEL_SYCL=source_oneapi_sycl_pythonenv
 export SOURCE_AMD_HIP=source_amd_rocm-5.4.3_pythonenv
 
-#export AMOS=TRUE
+export AMOS=TRUE
 #export DMOS=TRUE
-export TELOS=TRUE
+#export TELOS=TRUE
+#export DMOS=TRUE
 #export KOS=TRUE
 
-if [[ -v TELOS || -v KOS ]]; then
+#<<COMMENT
+if [[ -v TELOS || -v DMOS || -v KOS ]]; then
 
 #============================ Test with Intel Classic Compilers==========================================
 echo "Testing Intel classic complier based applications ---- "
@@ -117,8 +121,9 @@ fi
 
 echo "All Intel classic complier based applications ---- PASSED"
 
+#COMMENT
 
-if [[ -v TELOS ]]; then
+if [[ -v TELOS || -v DMOS ]]; then
 
 #============================ Test with Intel SYCL Compilers==========================================
 echo "Testing Intel SYCL complier based applications ---- "
@@ -126,7 +131,7 @@ cd $OPS_INSTALL_PATH/c
 source ../../scripts/$SOURCE_INTEL_SYCL
 #make -j -B
 make clean
-make
+make sycl mpi_sycl
 cd $OPS_INSTALL_PATH/../apps/c/poisson
 
 make clean
