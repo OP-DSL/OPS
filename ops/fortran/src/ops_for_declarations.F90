d2fec9f1fdf33d9d1407f24ee78c8008beb7e0ea
Ashutosh Londhe (EXT) <londhe.ashutosh@gmail.com>
2025-08-11 11:00:29 +0100

    added random number generation interface in Fortran side to call C functions

--- ops/fortran/src/ops_for_declarations.F90
+++ ops/fortran/src/ops_for_declarations.F90
@@ -529,6 +529,32 @@ module OPS_Fortran_Declarations
 
         end subroutine create_kerneldesc_and_enque 
 
+        subroutine ops_randomgen_init( seed, options ) BIND(C,name='ops_randomgen_init')
+            use, intrinsic :: ISO_C_BINDING
+
+            integer(kind=c_int), value   :: seed
+            integer(kind=c_int), value   :: options
+
+        end subroutine ops_randomgen_init
+
+        subroutine ops_randomgen_exit() BIND(C,name='ops_randomgen_exit')
+            use, intrinsic :: ISO_C_BINDING
+        end subroutine ops_randomgen_exit
+
+        subroutine ops_fill_random_uniform_c ( dat ) BIND(C,name='ops_fill_random_uniform')
+            use, intrinsic :: ISO_C_BINDING
+
+            type(c_ptr), value, intent(in)           :: dat
+
+        end subroutine ops_fill_random_uniform_c
+
+        subroutine ops_fill_random_normal_c ( dat ) BIND(C,name='ops_fill_random_normal')
+            use, intrinsic :: ISO_C_BINDING
+
+            type(c_ptr), value, intent(in)           :: dat
+
+        end subroutine ops_fill_random_normal_c
+
   end interface
 
   !##################################################################
@@ -1388,6 +1414,18 @@ module OPS_Fortran_Declarations
 
         call ops_execute_c(c_null_ptr)
     end subroutine ops_execute
+
+    subroutine ops_fill_random_uniform( dat )
+        use, intrinsic :: ISO_C_BINDING
+        type(ops_dat) :: dat
+        call ops_fill_random_uniform_c( dat%dataCPtr )
+    end subroutine ops_fill_random_uniform
+
+    subroutine ops_fill_random_normal( dat )
+        use, intrinsic :: ISO_C_BINDING
+        type(ops_dat) :: dat
+        call ops_fill_random_normal_c( dat%dataCPtr )
+    end subroutine ops_fill_random_normal
 
  !ops_decl_const -- various versions .. no-ops in ref ?
 
