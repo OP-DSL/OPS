9fd42f2bf288eb5ffa6d69fdb9f43df504975472
rfj82982 <stefano.rolfo@stfc.ac.uk>
2025-09-09 23:51:11 +0100

    Update ENV

--- setup_env_nvhpcscarf_ops.sh
+++ setup_env_nvhpcscarf_ops.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 export OPS_COMPILER=pgi
+export OPS_COMPILER_VERS=24.9
 export OPS_INSTALL_PATH=$(pwd)/ops
 
 export NV_ARCH=Ampere
 
 echo "GPU architecture " $NV_ARCH
 
-module purge
+#module purge
 # PGI and MPI compiler
-module load OpenMPI/5.0.8-NVHPC-25.5-CUDA-12.9.0
+module load OpenMPI/5.0.3-NVHPC-24.9-CUDA-12.6.0
 
 export CUDA_INSTALL_PATH=$CUDA_HOME
-export CUDA_MATH_LIBS=/work4/scd/scarf562/eb/software/NVHPC/25.5-CUDA-12.9.0/Linux_x86_64/25.5/math_libs/lib64
+export CUDA_MATH_LIBS=$NVHPC/Linux_x86_64/$OPS_COMPILER_VERS/math_libs/lib64
 #export LD_LIBRARY_PATH=$CUDA_MATH_LIBS:$LD_LIBRARY_PATH
 
-export MPI_INSTALL_PATH=/work4/scd/scarf562/eb/software/OpenMPI/5.0.8-NVHPC-25.5-CUDA-12.9.0
+export MPI_INSTALL_PATH=$EBROOTOPENMPI
 #export PATH=$MPI_INSTALL_PATH/bin:$PATH
 #export LD_LIBRARY_PATH=$MPI_INSTALL_PATH/lib:$LD_LIBRARY_PATH
 
@@ -29,9 +30,7 @@ export MPICH_FC=mpif90
 # HDF5
 unset HDF5_INSTALL_PATH
 #export HDF5_INSTALL_PATH=/usr/lib64/openmpi 
-export HDF5_INSTALL_PATH=/work4/scd/scarf924/Software/opt_nvhpc2505
-export PATH=$HDF5_INSTALL_PATH/bin:$PATH
-export LD_LIBRARY_PATH=$HDF5_INSTALL_PATH/lib:$LD_LIBRARY_PATH
+export HDF5_INSTALL_PATH=$HDF5_ROOT
 
 # Python
 # Available by defualt >3.8
